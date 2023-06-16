9151125c2974112145ce2beaa53c280aef662022
abhimanyu.chauhan <abhimanyu.chauhan@dpworld.com>
2023-06-16 13:17:44 +0530

    Added FileRepo and ReferenceNumbers Entity

---
+++ src/main/java/com/dpw/runner/shipment/services/entity/FileRepo.java
@@ -0,0 +1,46 @@
+package com.dpw.runner.shipment.services.entity;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.CollectionId;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "reference_numbers")
+@Accessors(chain = true)
+@ToString(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class FileRepo {
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "file_name")
+    private String fileName;
+
+    @Column(name = "path")
+    private String path;
+
+    @Column(name = "doc_type")
+    private String docType;
+
+    @Column(name = "client_enabled")
+    private Boolean clientEnabled;
+
+    @Column(name = "is_posted")
+    private Boolean isPosted;
+
+    @Column(name = "event_code")
+    private String eventCode;
+
+    @Column(name = "shipment_id")
+    private Long shipmentId;
+
+    @Column(name = "consolidation_id")
+    private Long consolidationId;
+}
---
+++ src/main/java/com/dpw/runner/shipment/services/entity/ReferenceNumbers.java
@@ -0,0 +1,46 @@
+package com.dpw.runner.shipment.services.entity;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "reference_numbers")
+@Accessors(chain = true)
+@ToString(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReferenceNumbers {
+
+    @Id
+    @ToString.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "guid")
+    private UUID guid;
+
+    @Column(name = "consolidation_id")
+    private Long consolidationId;
+
+    @Column(name = "country_of_issue")
+    private String countryOfIssue;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "reference_number")
+    private String referenceNumber;
+
+    @Column(name = "tenant_id")
+    private String tenantId;
+
+    @Column(name = "shipment_id")
+    private String shipmentId;
+
+}
