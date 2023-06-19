eaa4e3189fab4620aa38b30b0392b811420cb560
Mayank Gupta <mayankgupta@nsg-corporate-210.87.186.122.airtel.in>
2023-06-19 12:59:20 +0530

    User Factory Pattern

--- src/main/java/com/dpw/runner/shipment/services/service/impl/GetUserServiceFactory.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/GetUserServiceFactory.java
@@ -1,2 +1,34 @@
-package com.dpw.runner.shipment.services.service.impl;public class GetUserServiceFactory {
+package com.dpw.runner.shipment.services.service.impl;
+
+import com.dpw.runner.shipment.services.service.interfaces.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GetUserServiceFactory {
+
+    @Value("${user-service.source}")
+    private String source;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserServiceV1 userServiceV1;
+
+    public IUserService returnUserService()
+    {
+        if(source == null || source.isEmpty())
+            return userServiceV1;  // throw exception
+        switch (source)
+        {
+            case "v1":
+                return userServiceV1;
+            case "mavani":
+                return userService;
+            default:
+                return userServiceV1;  // throw exception
+        }
+    }
 }
