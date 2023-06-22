e72ecbe841e76b69f14947737e1a2af4ef74f26e
Tapan Pareek <tapan.pareek@dpworld.com>
2023-06-22 12:56:57 +0530

    shipment service implementation

---
+++ src/main/java/com/dpw/runner/shipment/services/dto/request/ShipmentServiceRequest.java
@@ -0,0 +1,29 @@
+package com.dpw.runner.shipment.services.dto.request;
+
+import com.dpw.runner.shipment.services.commons.requests.CommonRequest;
+import com.dpw.runner.shipment.services.commons.requests.IRunnerRequest;
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.Duration;
+import java.util.Date;
+
+@Getter
+@ApiModel("Shipment Service Request Model")
+@ToString
+public class ShipmentServiceRequest extends CommonRequest implements IRunnerRequest {
+    private Long id;
+    private Long shipmentId;
+    private Long consolidationId;
+    private String serviceType;
+    private Long contractorId;
+    private Long contractorAddressId;
+    private int srvLocation;
+    private Date bookingDate;
+    private String serviceCount;
+    private Duration serviceDuration;
+    private Date completionDate;
+    private String refNumber;
+    private String serviceNotes;
+}
