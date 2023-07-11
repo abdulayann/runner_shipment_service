75ac39ae4bf111544412de463752fe4c84cff1c9
Tanishq Malhotra <tanishq.malhotra@dpworld.com>
2023-07-11 17:11:33 +0530

    moving updateEntityFromShipment logic to entity dao

--- src/main/java/com/dpw/runner/shipment/services/service/impl/RoutingsService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/RoutingsService.java
@@ -24,15 +24,12 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 import static com.dpw.runner.shipment.services.helpers.DbAccessHelper.fetchData;
-import static com.dpw.runner.shipment.services.utils.CommonUtils.constructListCommonRequest;
 
 @Service
 @Slf4j
@@ -47,9 +44,9 @@ public class RoutingsService implements IRoutingsService {
     @Transactional
     public ResponseEntity<?> create(CommonRequestModel commonRequestModel) {
         RoutingsRequest request = (RoutingsRequest) commonRequestModel.getData();
-        Routings notes = convertRequestToRoutingsEntity(request);
-        notes = routingsDao.save(notes);
-        return ResponseHelper.buildSuccessResponse(convertEntityToDto(notes));
+        Routings routing = convertRequestToRoutingsEntity(request);
+        routing = routingsDao.save(routing);
+        return ResponseHelper.buildSuccessResponse(convertEntityToDto(routing));
     }
 
     @Override
@@ -62,10 +59,10 @@ public class RoutingsService implements IRoutingsService {
             throw new DataRetrievalFailureException(DaoConstants.DAO_DATA_RETRIEVAL_FAILURE);
         }
 
-        Routings notes = convertRequestToRoutingsEntity(request);
-        notes.setId(oldEntity.get().getId());
-        notes = routingsDao.save(notes);
-        return ResponseHelper.buildSuccessResponse(convertEntityToDto(notes));
+        Routings routing = convertRequestToRoutingsEntity(request);
+        routing.setId(oldEntity.get().getId());
+        routing = routingsDao.save(routing);
+        return ResponseHelper.buildSuccessResponse(convertEntityToDto(routing));
     }
 
     @Override
@@ -75,11 +72,11 @@ public class RoutingsService implements IRoutingsService {
             ListCommonRequest request = (ListCommonRequest) commonRequestModel.getData();
 
             Pair<Specification<Routings>, Pageable> tuple = fetchData(request, Routings.class);
-            Page<Routings> notesPage = routingsDao.findAll(tuple.getLeft(), tuple.getRight());
+            Page<Routings> routingsPage = routingsDao.findAll(tuple.getLeft(), tuple.getRight());
             return ResponseHelper.buildListSuccessResponse(
-                    convertEntityListToDtoList(notesPage.getContent()),
-                    notesPage.getTotalPages(),
-                    notesPage.getTotalElements());
+                    convertEntityListToDtoList(routingsPage.getContent()),
+                    routingsPage.getTotalPages(),
+                    routingsPage.getTotalElements());
         } catch (Exception e) {
             responseMsg = e.getMessage() != null ? e.getMessage()
                     : DaoConstants.DAO_GENERIC_LIST_EXCEPTION_MSG;
@@ -96,11 +93,11 @@ public class RoutingsService implements IRoutingsService {
             ListCommonRequest request = (ListCommonRequest) commonRequestModel.getData();
 
             Pair<Specification<Routings>, Pageable> tuple = fetchData(request, Routings.class);
-            Page<Routings> notesPage = routingsDao.findAll(tuple.getLeft(), tuple.getRight());
+            Page<Routings> routingsPage = routingsDao.findAll(tuple.getLeft(), tuple.getRight());
             return CompletableFuture.completedFuture(ResponseHelper.buildListSuccessResponse(
-                    convertEntityListToDtoList(notesPage.getContent()),
-                    notesPage.getTotalPages(),
-                    notesPage.getTotalElements()));
+                    convertEntityListToDtoList(routingsPage.getContent()),
+                    routingsPage.getTotalPages(),
+                    routingsPage.getTotalElements()));
         } catch (Exception e) {
             responseMsg = e.getMessage() != null ? e.getMessage()
                     : DaoConstants.DAO_GENERIC_LIST_EXCEPTION_MSG;
@@ -115,12 +112,12 @@ public class RoutingsService implements IRoutingsService {
         try {
             CommonGetRequest request = (CommonGetRequest) commonRequestModel.getData();
             long id = request.getId();
-            Optional<Routings> note = routingsDao.findById(id);
-            if (note.isEmpty()) {
+            Optional<Routings> routing = routingsDao.findById(id);
+            if (routing.isEmpty()) {
                 log.debug("Routings is null for Id {}", request.getId());
                 throw new DataRetrievalFailureException(DaoConstants.DAO_DATA_RETRIEVAL_FAILURE);
             }
-            routingsDao.delete(note.get());
+            routingsDao.delete(routing.get());
             return ResponseHelper.buildSuccessResponse();
         } catch (Exception e) {
             responseMsg = e.getMessage() != null ? e.getMessage()
@@ -136,85 +133,17 @@ public class RoutingsService implements IRoutingsService {
         try {
             CommonGetRequest request = (CommonGetRequest) commonRequestModel.getData();
             long id = request.getId();
-            Optional<Routings> notes = routingsDao.findById(id);
-            if (notes.isEmpty()) {
+            Optional<Routings> routings = routingsDao.findById(id);
+            if (routings.isEmpty()) {
                 log.debug("Routings is null for Id {}", request.getId());
                 throw new DataRetrievalFailureException(DaoConstants.DAO_DATA_RETRIEVAL_FAILURE);
             }
 
-            RoutingsResponse response = convertEntityToDto(notes.get());
+            RoutingsResponse response = convertEntityToDto(routings.get());
             return ResponseHelper.buildSuccessResponse(response);
         } catch (Exception e) {
             responseMsg = e.getMessage() != null ? e.getMessage()
                     : DaoConstants.DAO_GENERIC_RETRIEVE_EXCEPTION_MSG;
-            log.error(responseMsg, e);
-            return ResponseHelper.buildFailedResponse(responseMsg);
-        }
-    }
-
-    public ResponseEntity<?> updateEntityFromShipment(CommonRequestModel commonRequestModel, Long shipmentId) {
-        String responseMsg;
-        List<Routings> responseRoutings = new ArrayList<>();
-        try {
-            // TODO- Handle Transactions here
-            ListCommonRequest listCommonRequest = constructListCommonRequest("shipmentId", shipmentId, "=");
-            Pair<Specification<Routings>, Pageable> pair = fetchData(listCommonRequest, Routings.class);
-            Page<Routings> routings = routingsDao.findAll(pair.getLeft(), pair.getRight());
-            HashSet<Long> existingIds = new HashSet<>(routings
-                    .stream()
-                    .map(Routings::getId)
-                    .collect(Collectors.toList()));
-            List<RoutingsRequest> routingsRequestList = new ArrayList<>();
-            List<RoutingsRequest> requestList = (List<RoutingsRequest>) commonRequestModel.getDataList();
-            if (requestList != null && requestList.size() != 0) {
-                for (RoutingsRequest request : requestList) {
-                    Long id = request.getId();
-                    if (id != null) {
-                        existingIds.remove(id);
-                    }
-                    routingsRequestList.add(request);
-                }
-                responseRoutings = saveRoutings(routingsRequestList);
-            }
-            deleteRoutings(existingIds);
-            return ResponseHelper.buildListSuccessResponse(convertEntityListToDtoList(responseRoutings));
-        } catch (Exception e) {
-            responseMsg = e.getMessage() != null ? e.getMessage()
-                    : DaoConstants.DAO_FAILED_ENTITY_UPDATE;
-            log.error(responseMsg, e);
-            return ResponseHelper.buildFailedResponse(responseMsg);
-        }
-    }
-
-    private List<Routings> saveRoutings(List<RoutingsRequest> routings) {
-        List<Routings> res = new ArrayList<>();
-        for(RoutingsRequest req : routings){
-            Routings saveEntity = convertRequestToRoutingsEntity(req);
-            if(req.getId() != null){
-                long id = req.getId();
-                Optional<Routings> oldEntity = routingsDao.findById(id);
-                if (!oldEntity.isPresent()) {
-                    log.debug("Routing is null for Id {}", req.getId());
-                    throw new DataRetrievalFailureException(DaoConstants.DAO_DATA_RETRIEVAL_FAILURE);
-                }
-                saveEntity = oldEntity.get();
-            }
-            saveEntity = routingsDao.save(saveEntity);
-            res.add(saveEntity);
-        }
-        return res;
-    }
-
-    private ResponseEntity<?> deleteRoutings(HashSet<Long> existingIds) {
-        String responseMsg;
-        try {
-            for (Long id : existingIds) {
-                delete(CommonRequestModel.buildRequest(CommonGetRequest.builder().id(id).build()));
-            }
-            return ResponseHelper.buildSuccessResponse();
-        } catch (Exception e) {
-            responseMsg = e.getMessage() != null ? e.getMessage()
-                    : DaoConstants.DAO_GENERIC_DELETE_EXCEPTION_MSG;
             log.error(responseMsg, e);
             return ResponseHelper.buildFailedResponse(responseMsg);
         }
