668317f94802506d42bfbd80bfcb299ea0a79a86
Pardeep Malik <pardeep.malik@dpworld.com>
2023-07-11 16:40:16 +0530

    Entity Changes

--- src/main/java/com/dpw/runner/shipment/services/entity/ShipmentDetails.java
+++ src/main/java/com/dpw/runner/shipment/services/entity/ShipmentDetails.java
@@ -248,7 +248,7 @@ public class ShipmentDetails extends MultiTenancy {
 
     @OneToOne(targetEntity = AdditionalDetails.class)
     @JoinColumn(name = "additional_details_id", referencedColumnName = "id")
-    private AdditionalDetails additionalDetails;
+    private AdditionalDetails additionalDetail;
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "entityId")
     @Where(clause = "entity_name = 'shipments'")
