08780c79ae49cc8d542bb0262b19f36a9ec5cfea
abhishekgoyal <abhishek.goyal@dpworld.com>
2023-07-22 14:38:07 +0530

    User Authorization

--- src/main/java/com/dpw/runner/shipment/services/service/interfaces/IUserService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/interfaces/IUserService.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface IUserService {
-    UsersDto getUserByUserName(String userName);
+    UsersDto getUserByToken(String token);
 }
