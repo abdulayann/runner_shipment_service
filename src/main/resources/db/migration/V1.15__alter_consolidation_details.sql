8796238038808bdb420236278987fb7b608e2035
abhishekgoyal <abhishek.goyal@dpworld.com>
2023-08-01 20:18:31 +0530

    Migration corrected

--- src/main/resources/db/migration/V1.15__alter_consolidation_details.sql
+++ src/main/resources/db/migration/V1.15__alter_consolidation_details.sql
@@ -1,9 +1,9 @@
 ALTER TABLE IF EXISTS consolidation_details
     DROP COLUMN arrival_departure_details_id,
-    ADD COLUMN arrival_details_id,
-    ADD COLUMN departure_details_id,
-    ADD COLUMN sending_agent_id,
-    ADD COLUMN receiving_agent_id,
-    ADD COLUMN borrowed_from_id,
-    ADD COLUMN creditor_id,
-    ADD COLUMN co_load_with_id;
+    ADD COLUMN arrival_details_id bigint,
+    ADD COLUMN departure_details_id bigint,
+    ADD COLUMN sending_agent_id bigint,
+    ADD COLUMN receiving_agent_id bigint,
+    ADD COLUMN borrowed_from_id bigint,
+    ADD COLUMN creditor_id bigint,
+    ADD COLUMN co_load_with_id bigint;
