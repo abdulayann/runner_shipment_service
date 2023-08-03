3357783672d09699d69bf46d70f8a1cb8e1aaec7
Wasim Jafar <wasim.jafar@dpworld.com>
2023-08-04 00:23:47 +0530

    Fixes

--- src/main/java/com/dpw/runner/shipment/services/helpers/JsonHelper.java
+++ src/main/java/com/dpw/runner/shipment/services/helpers/JsonHelper.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.json.JsonParseException;
@@ -19,6 +20,8 @@ public class JsonHelper {
     @Autowired
     private ObjectMapper mapper;
 
+    private ObjectMapper mapper1 = new ObjectMapper();
+
     public <T> T readFromJson(String jsonString, Class<T> clazz) {
         try {
             return mapper.readValue(jsonString, clazz);
@@ -33,6 +36,17 @@ public class JsonHelper {
             mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
             mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
             return mapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            log.error("Failed to Parse given Json");
+            throw new JsonParseException(e);
+        }
+    }
+
+    public <T> String convertToJsonIncludeNulls(T object) {
+        try {
+            mapper1.registerModule(new JavaTimeModule());
+            mapper1.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+            return mapper1.writeValueAsString(object);
         } catch (JsonProcessingException e) {
             log.error("Failed to Parse given Json");
             throw new JsonParseException(e);
