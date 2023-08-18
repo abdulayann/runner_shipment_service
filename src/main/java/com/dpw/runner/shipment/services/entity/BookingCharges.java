e7e2c3a48f1d3f1d50ad30b4201bf242a3f3fa35
abhimanyu.chauhan <abhimanyu.chauhan@dpworld.com>
2023-08-18 18:29:05 +0530

    Booking Charges Entity Create

--- src/main/java/com/dpw/runner/shipment/services/entity/BookingCharges.java
+++ src/main/java/com/dpw/runner/shipment/services/entity/BookingCharges.java
@@ -1,67 +1,196 @@
 package com.dpw.runner.shipment.services.entity;
 
+import com.dpw.runner.shipment.services.aspects.MultitenancyAspect.MultiTenancy;
 import com.dpw.runner.shipment.services.dto.request.PartiesRequest;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public class BookingCharges {
-    public Long bookingId;
-    public String seqNo;
-    public String chargeType; //charge type master data
-    public String details;
-    public String chargeCodeAlt;
-    public String hsnMaster; //HSN SAC Master data
-    public String measurementBasis; //enum
-    public String measurementContainerType; //container type master data
-    public BigDecimal totalUnitCount;
-    public String measurementUnit;
-    public String costCurrencyExchangeUpdate; //enum
-    public Boolean reciprocalCurrencyCost;
-    public BigDecimal estimatedCost;
-    public BigDecimal localCostAmount;
-    public BigDecimal overseasCostAmount;
-    public String overseasCostCurrency; //currencies master data
-    public BigDecimal localCostCurrency; //currencies master data
-    public BigDecimal currentCostRate;
-    public String costRateCurrency; //currencies master data
-    public BigDecimal costExchange;
-    public String costAccount;
-    public String costComments;
-    public PartiesRequest creditor;
-    public String costTaxId;
-    public LocalDateTime costTaxDate;
-    public BigDecimal costLocalTax;
-    public BigDecimal costTaxPercentage;
-    public BigDecimal costOverseasTax;
-    public Boolean costNoGST;
-    public BigDecimal costTaxType1;
-    public BigDecimal costTaxType2;
-    public BigDecimal costTaxType3;
-    public BigDecimal costTaxType4;
-    public BigDecimal costLineTotal;
-    public String sellCurrencyExchangeUpdate; //enum
-    public Boolean reciprocalCurrencyRevenue;
-    public BigDecimal estimatedRevenue;
-    public BigDecimal localSellAmount;
-    public BigDecimal overseasSellAmount;
-    public String overseasSellCurrency; //currencies master data
-    public BigDecimal localSellCurrency; //currencies master data
-    public BigDecimal currentSellRate;
-    public String sellRateCurrency; //currencies master data
-    public BigDecimal sellExchange;
-    public String revenueAccount;
-    public String revenueComments;
-    public PartiesRequest debtor;
-    public String revenueTaxId;
-    public LocalDateTime revenueTaxDate;
-    public BigDecimal localTax;
-    public BigDecimal taxPercentage;
-    public BigDecimal overseasTax;
-    public Boolean noGST;
-    public BigDecimal taxType1;
-    public BigDecimal taxType2;
-    public BigDecimal taxType3;
-    public BigDecimal taxType4;
-    public BigDecimal revenueLineTotal;
+@Entity
+@Getter
+@Setter
+@Table(name = "booking_charges")
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookingCharges extends MultiTenancy {
+    @Column(name = "booking_id")
+    private Long bookingId;
+
+    @Column(name = "seq_no")
+    private String seqNo;
+
+    @Column(name = "charges_type")
+    private String chargeType; //charge type master data
+
+    @Column(name = "details")
+    private String details;
+
+    @Column(name = "charges_code_alt")
+    private String chargeCodeAlt;
+
+    @Column(name = "hsn_master")
+    private String hsnMaster; //HSN SAC Master data
+
+    @Column(name = "measurement_basis")
+    private String measurementBasis; //enum
+
+    @Column(name = "measurement_container_type")
+    private String measurementContainerType; //container type master data
+
+    @Column(name = "total_unit_count")
+    private BigDecimal totalUnitCount;
+
+    @Column(name = "measurement_unit")
+    private String measurementUnit;
+
+    @Column(name = "cost_currency_exchange_update")
+    private String costCurrencyExchangeUpdate; //enum
+
+    @Column(name = "reciprocal_currency_cost")
+    private Boolean reciprocalCurrencyCost;
+
+    @Column(name = "estimated_cost")
+    private BigDecimal estimatedCost;
+
+    @Column(name = "local_cost_amount")
+    private BigDecimal localCostAmount;
+
+    @Column(name = "overseas_cost_amount")
+    private BigDecimal overseasCostAmount;
+
+    @Column(name = "overseas_cost_currency")
+    private String overseasCostCurrency; //currencies master data
+
+    @Column(name = "local_cost_currency")
+    private BigDecimal localCostCurrency; //currencies master data
+
+    @Column(name = "current_cost_rate")
+    private BigDecimal currentCostRate;
+
+    @Column(name = "cost_rate_currency")
+    private String costRateCurrency; //currencies master data
+
+    @Column(name = "cost_exchange")
+    private BigDecimal costExchange;
+
+    @Column(name = "cost_account")
+    private String costAccount;
+
+    @Column(name = "cost_comments")
+    private String costComments;
+
+    @OneToOne(targetEntity = Parties.class, cascade = CascadeType.ALL)
+    @JoinColumn(name = "creditor_id", referencedColumnName = "id")
+    private Parties creditor;
+
+    @Column(name = "cost_tax_id")
+    private String costTaxId;
+
+    @Column(name = "cost_tax_date")
+    private LocalDateTime costTaxDate;
+
+    @Column(name = "cost_local_tax")
+    private BigDecimal costLocalTax;
+
+    @Column(name = "cost_tax_percentage")
+    private BigDecimal costTaxPercentage;
+
+    @Column(name = "cost_overseas_tax")
+    private BigDecimal costOverseasTax;
+
+    @Column(name = "cost_no_gst")
+    private Boolean costNoGST;
+
+    @Column(name = "cost_tax_type1")
+    private BigDecimal costTaxType1;
+
+    @Column(name = "cost_tax_type2")
+    private BigDecimal costTaxType2;
+
+    @Column(name = "cost_tax_type3")
+    private BigDecimal costTaxType3;
+
+    @Column(name = "cost_tax_type4")
+    private BigDecimal costTaxType4;
+
+    @Column(name = "cost_line_total")
+    private BigDecimal costLineTotal;
+
+    @Column(name = "sell_currency_exchange_update")
+    private String sellCurrencyExchangeUpdate; //enum
+
+    @Column(name = "reciprocal_currency_revenue")
+    private Boolean reciprocalCurrencyRevenue;
+
+    @Column(name = "estimated_revenue")
+    private BigDecimal estimatedRevenue;
+
+    @Column(name = "local_sell_amount")
+    private BigDecimal localSellAmount;
+
+    @Column(name = "overseas_sell_amount")
+    private BigDecimal overseasSellAmount;
+
+    @Column(name = "overseas_sell_currency")
+    private String overseasSellCurrency; //currencies master data
+
+    @Column(name = "local_sell_currency")
+    private BigDecimal localSellCurrency; //currencies master data
+
+    @Column(name = "current_sell_rate")
+    private BigDecimal currentSellRate;
+
+    @Column(name = "sell_rate_currency")
+    private String sellRateCurrency; //currencies master data
+
+    @Column(name = "sell_exchange")
+    private BigDecimal sellExchange;
+
+    @Column(name = "revenue_account")
+    private String revenueAccount;
+
+    @Column(name = "revenue_comments")
+    private String revenueComments;
+
+    @OneToOne(targetEntity = Parties.class, cascade = CascadeType.ALL)
+    @JoinColumn(name = "debtor_id", referencedColumnName = "id")
+    private PartiesRequest debtor;
+
+    @Column(name = "revenue_tax_id")
+    private String revenueTaxId;
+
+    @Column(name = "revenue_tax_date")
+    private LocalDateTime revenueTaxDate;
+
+    @Column(name = "local_tax")
+    private BigDecimal localTax;
+
+    @Column(name = "tax_percentage")
+    private BigDecimal taxPercentage;
+
+    @Column(name = "overseas_tax")
+    private BigDecimal overseasTax;
+
+    @Column(name = "no_gst")
+    private Boolean noGST;
+
+    @Column(name = "tax_type_1")
+    private BigDecimal taxType1;
+
+    @Column(name = "tax_type_2")
+    private BigDecimal taxType2;
+
+    @Column(name = "tax_type_3")
+    private BigDecimal taxType3;
+
+    @Column(name = "tax_type_4")
+    private BigDecimal taxType4;
+
+    @Column(name = "revenue_line_total")
+    private BigDecimal revenueLineTotal;
 }
