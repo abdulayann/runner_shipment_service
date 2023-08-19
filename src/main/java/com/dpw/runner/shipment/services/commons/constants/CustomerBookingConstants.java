9b2b19e0ec0d272a6be0eca6388b60a295106bea
Pardeep Malik <Pardeep.Malik@dpworld.com>
2023-08-19 13:16:58 +0530

    Controller and constants class for booking

---
+++ src/main/java/com/dpw/runner/shipment/services/commons/constants/CustomerBookingConstants.java
@@ -0,0 +1,5 @@
+package com.dpw.runner.shipment.services.commons.constants;
+
+public class CustomerBookingConstants {
+    public static final String Customer_Booking_API_HANDLE = "/api/v2/customer-booking";
+}
---
+++ src/main/java/com/dpw/runner/shipment/services/controller/CustomerBookingController.java
@@ -0,0 +1,13 @@
+package com.dpw.runner.shipment.services.controller;
+
+import com.dpw.runner.shipment.services.commons.constants.CustomerBookingConstants;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@SuppressWarnings("ALL")
+@RestController
+@RequestMapping(value = CustomerBookingConstants.Customer_Booking_API_HANDLE)
+public class CustomerBookingController {
+}
