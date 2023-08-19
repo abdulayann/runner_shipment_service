2f7316e542fdfe57e9d5840792d69100cf39e8b0
abhimanyu.chauhan <abhimanyu.chauhan@dpworld.com>
2023-08-19 18:17:22 +0530

    script changes3

--- src/main/resources/db/migration/V1.23__alter_table_containers.sql
+++ src/main/resources/db/migration/V1.23__alter_table_containers.sql
@@ -9,12 +9,12 @@ add column if not exists booking_id bigint;
 
 alter table if exists containers
 add constraint fk_containers_booking
-foreign key (booking_id) references booking_charges(id);
+foreign key (booking_id) references customer_booking(id);
 
 alter table if exists packing
 add constraint fk_packing_booking
-foreign key (booking_id) references booking_charges(id);
+foreign key (booking_id) references customer_booking(id);
 
 alter table if exists routings
 add constraint fk_routings_booking
-foreign key (booking_id) references booking_charges(id);
+foreign key (booking_id) references customer_booking(id);
