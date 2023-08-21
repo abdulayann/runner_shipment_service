8fe8178c2bd857393f973666b15aa5e30221e9bd
abhimanyu.chauhan <abhimanyu.chauhan@dpworld.com>
2023-08-21 12:54:23 +0530

    Changed external url

--- src/main/java/com/dpw/runner/shipment/services/commons/constants/CustomerBookingConstants.java
+++ src/main/java/com/dpw/runner/shipment/services/commons/constants/CustomerBookingConstants.java
@@ -4,8 +4,8 @@ public class CustomerBookingConstants {
     public static final String Customer_Booking_API_HANDLE = "/api/v2/customer-booking";
     public static final String PLATFORM_CREATE_BOOKING = "/platform-create";
     public static final String CREATE_SUCCESSFUL = "Successful Customer Booking Data Creation";
-    public static final String CRP_LIST = "/crp-list";
-    public static final String CRP_RETRIEVE = "/crp-retrieve";
+    public static final String CRP_LIST = "/external/crp-list";
+    public static final String CRP_RETRIEVE = "/external/crp-retrieve";
     public static final String LIST_SUCCESSFUL = "Successful CRP List";
     public static final String RETRIEVE_SUCCESSFUL = "Successfully retrieved data";
     public static final String UPDATE_SUCCESSFUL = "Successful Customer Booking Data Update";
