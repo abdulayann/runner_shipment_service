602fc2a4e5a1132b8c00e96cdbbc0b760c3829b5
abhimanyu.chauhan <abhimanyu.chauhan@dpworld.com>
2023-08-24 16:13:29 +0530

    Refactoring + Changes

--- src/main/java/com/dpw/runner/shipment/services/adapters/interfaces/INPMServiceAdapter.java
+++ src/main/java/com/dpw/runner/shipment/services/adapters/interfaces/INPMServiceAdapter.java
@@ -1,11 +1,13 @@
 package com.dpw.runner.shipment.services.adapters.interfaces;
 
 import com.dpw.runner.shipment.services.commons.requests.CommonRequestModel;
-import com.dpw.runner.shipment.services.dto.request.npm.NPMFetchOffersRequest;
+import com.dpw.runner.shipment.services.dto.request.npm.UpdateContractRequest;
 import org.springframework.http.ResponseEntity;
 
 public interface INPMServiceAdapter {
     ResponseEntity<?> fetchContracts(CommonRequestModel commonRequestModel) throws Exception;
+
+    ResponseEntity<?> updateContracts(UpdateContractRequest updateContractRequest) throws Exception;
 
     ResponseEntity<?> fetchOffers(CommonRequestModel commonRequestModel) throws Exception;
 }
