3a2166f4e0863635ee08621147a30afc6eb130fd
Wasim Jafar <wasim.jafar@dpworld.com>
2023-08-25 17:46:39 +0530

    Added logs in CRP

--- src/main/java/com/dpw/runner/shipment/services/adapters/impl/CRPServiceAdapter.java
+++ src/main/java/com/dpw/runner/shipment/services/adapters/impl/CRPServiceAdapter.java
@@ -6,6 +6,7 @@ import com.dpw.runner.shipment.services.dto.request.crp.CRPListRequest;
 import com.dpw.runner.shipment.services.dto.request.crp.CRPRetrieveRequest;
 import com.dpw.runner.shipment.services.helpers.ResponseHelper;
 import com.dpw.runner.shipment.services.utils.StringUtility;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,7 @@ import org.springframework.beans.factory.annotation.Value;
 import java.net.URI;
 
 @Service
+@Slf4j
 public class CRPServiceAdapter implements com.dpw.runner.shipment.services.adapters.interfaces.ICRPServiceAdapter {
 
     private final RestTemplate restTemplate;
@@ -35,25 +37,29 @@ public class CRPServiceAdapter implements com.dpw.runner.shipment.services.adapt
     public ResponseEntity<?> retrieveCRPService(CommonRequestModel requestModel) throws Exception {
         CRPRetrieveRequest request = (CRPRetrieveRequest) requestModel.getData();
         String url = crpServiceRetrieveUrl + request.getSearchString();
+        log.info("List CRP: with request: {}", request.toString());
         ResponseEntity<?> responseEntity;
         try {
             responseEntity = restTemplate.exchange(RequestEntity.get(URI.create(url)).build(), Object.class);
         }  catch (HttpClientErrorException.NotFound ex) {
             responseEntity = ResponseHelper.buildSuccessResponse();
         }
+        log.info("List CRP: with response: {}", responseEntity);
         return responseEntity;
     }
 
     public ResponseEntity<?> listCRPService(CommonRequestModel requestModel) throws Exception {
         CRPListRequest request = (CRPListRequest) requestModel.getData();
+        log.info("List CRP: with request: {}", request.toString());
         String url = crpServiceListUrl + request.getSearchString() + (request.isBillable() ? CustomerBookingConstants.BILLABLE_IDENTIFIER : StringUtility.getEmptyString());
+        log.info("List CRP: To Url: {}", url);
         ResponseEntity<?> responseEntity;
         try {
             responseEntity = restTemplate.exchange(RequestEntity.get(URI.create(url)).build(), Object.class);
         } catch (HttpClientErrorException.NotFound ex) {
             responseEntity = ResponseHelper.buildSuccessResponse();
         }
-
+        log.info("List CRP: with response: {}", responseEntity);
         return responseEntity;
     }
 }
