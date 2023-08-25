0f305f05e2a6e38852e0c71a2a80e8b30978f201
Abhimanyu Chauhan <Abhimanyu.Chauhan@dpworld.com>
2023-08-25 06:10:51 +0000

    Merged PR 129415: NPM Offers API Impl

--- src/main/java/com/dpw/runner/shipment/services/commons/constants/NPMConstants.java
+++ src/main/java/com/dpw/runner/shipment/services/commons/constants/NPMConstants.java
@@ -5,4 +5,6 @@ public class NPMConstants {
     public static final String LIST_CONTRACT = "/contract";
     public static final String CONTRACT_LIST_SUCCESSFUL = "Successful Contract Data List Retrieval";
     public static final String CONTRACT_LIST_FAILED = "Error occurred while trying to list the contracts. Exception raised is: ";
+    public static final String GET_OFFERS = "/runner-offers";
+    public static final String LIST_SUCCESSFUL = "Successfully Fetched Offers";
 }
