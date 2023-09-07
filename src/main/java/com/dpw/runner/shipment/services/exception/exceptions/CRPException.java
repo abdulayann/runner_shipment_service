349fda8d8db7e603706b7ed2197b301edc336c9c
Wasim Jafar <wasim.jafar@dpworld.com>
2023-09-07 13:46:08 +0530

    Exception handle

---
+++ src/main/java/com/dpw/runner/shipment/services/exception/exceptions/CRPException.java
@@ -0,0 +1,7 @@
+package com.dpw.runner.shipment.services.exception.exceptions;
+
+public class CRPException extends RuntimeException {
+    public CRPException(String msg) {
+        super(msg);
+    }
+}
