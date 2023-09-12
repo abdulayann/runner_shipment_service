4af1b47c97ec9e467ee8c073fbaa838379d24df5
Harsh Khandelwal <harshkhandelwal@nsg-corporate-210.87.186.122.airtel.in>
2023-09-12 16:34:00 +0530

    initial commit for descart service

--- src/main/java/com/dpw/runner/shipment/services/config/JacksonConfig.java
+++ src/main/java/com/dpw/runner/shipment/services/config/JacksonConfig.java
@@ -1,7 +1,10 @@
 package com.dpw.runner.shipment.services.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,13 +15,32 @@ import java.time.LocalDateTime;
 @Configuration
 public class JacksonConfig {
 
+//  @Bean
+//  Jackson2ObjectMapperBuilder objectMapperBuilder() {
+//    Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+//    builder.serializationInclusion(JsonInclude.Include.ALWAYS)
+//            .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+//            .modulesToInstall(new JsonNullableModule());
+//    return builder;
+//  }
+
+
   @Bean
   Jackson2ObjectMapperBuilder objectMapperBuilder() {
-    Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-    builder.serializationInclusion(JsonInclude.Include.ALWAYS)
-            .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-            .deserializerByType(LocalDateTime.class,  new CustomLocalDateTimeDeserializer())
-            .modulesToInstall(new JsonNullableModule());
-    return builder;
+      Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+      builder.serializationInclusion(JsonInclude.Include.ALWAYS)
+              .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+              .deserializerByType(LocalDateTime.class, new CustomLocalDateTimeDeserializer())
+              .modulesToInstall(new JsonNullableModule());
+      return builder;
+  }
+
+  @Bean
+  public ObjectMapper objectMapper() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper. configure(DeserializationFeature. FAIL_ON_UNKNOWN_PROPERTIES, false);
+    objectMapper. configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+    objectMapper.registerModule(new JavaTimeModule());
+    return objectMapper;
   }
 }
