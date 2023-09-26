cbd21bee190ccd6fad6050d496ac1c251c29e594
Wasim Jafar <wasim.jafar@dpworld.com>
2023-09-26 18:52:04 +0530

    Grouped index

--- src/main/resources/db/migration/V1.45__added_Index_booking_module.sql
+++ src/main/resources/db/migration/V1.45__added_Index_booking_module.sql
@@ -3,8 +3,5 @@ CREATE INDEX IF NOT EXISTS packing_booking_id ON packing( booking_id );
 CREATE INDEX IF NOT EXISTS routings_booking_id ON routings( booking_id );
 CREATE INDEX IF NOT EXISTS booking_charges_booking_id ON booking_charges( booking_id );
 
-CREATE INDEX IF NOT EXISTS file_repo_entity_id ON file_repo( entity_id );
-CREATE INDEX IF NOT EXISTS file_repo_entity_type ON file_repo( entity_type );
-
-CREATE INDEX IF NOT EXISTS audit_log_entity_id ON audit_log( entity_id );
-CREATE INDEX IF NOT EXISTS audit_log_entity ON audit_log( entity );
+CREATE INDEX IF NOT EXISTS file_repo_join ON file_repo( entity_id, entity_type );
+CREATE INDEX IF NOT EXISTS audit_log_join ON audit_log( entity_id, entity );
