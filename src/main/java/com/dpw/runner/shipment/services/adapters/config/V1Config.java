46f4865dac9e5d64c36c0e303cd0eeb021af828d
Wasim Jafar <wasim.jafar@dpworld.com>
2023-10-09 21:43:59 +0530

    Performance: FIx

--- src/main/java/com/dpw/runner/shipment/services/adapters/config/V1Config.java
+++ src/main/java/com/dpw/runner/shipment/services/adapters/config/V1Config.java
@@ -1,8 +1,11 @@
 package com.dpw.runner.shipment.services.adapters.config;
 
+import org.apache.http.HttpHost;
+import org.apache.http.conn.routing.HttpRoute;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -13,19 +16,16 @@ public class V1Config {
 
 
     @Bean
-    public CloseableHttpClient httpClient() {
+    public RestTemplate restTemplateForV1() {
         PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
         connectionManager.setMaxTotal(20); // Set the maximum total connections
-
+        connectionManager.setDefaultMaxPerRoute(10);
         CloseableHttpClient httpClient = HttpClientBuilder.create()
                 .setConnectionManager(connectionManager)
                 .build();
-
-        return httpClient;
-    }
-
-    @Bean
-    public RestTemplate restTemplateForV1() {
-        return new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient()));
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        requestFactory.setConnectTimeout(60 * 1000);
+        requestFactory.setReadTimeout(30 * 1000);
+        return new RestTemplate(requestFactory);
     }
 }
