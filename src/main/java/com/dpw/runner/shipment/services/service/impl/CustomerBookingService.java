3e996619f65a87545b76236aa0d0d476a9c41927
abhishekgoyal <abhishek.goyal@dpworld.com>
2023-10-17 19:06:44 +0530

    TimeZone and AuditLog changes

--- src/main/java/com/dpw/runner/shipment/services/service/impl/CustomerBookingService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/CustomerBookingService.java
@@ -208,9 +208,9 @@ public class CustomerBookingService implements ICustomerBookingService {
                 }
                 bookingCharge.setContainersList(containerList);
                 bookingCharge.setBookingId(customerBooking.getId());
-                bookingCharge = bookingChargesDao.save(bookingCharge);
                 bookingCharges.add(bookingCharge);
             }
+            bookingCharges = bookingChargesDao.updateEntityFromBooking(bookingCharges, customerBooking.getId());
             customerBooking.setBookingCharges(bookingCharges);
         }
         auditLogService.addAuditLog(
@@ -247,6 +247,7 @@ public class CustomerBookingService implements ICustomerBookingService {
         customerBooking.setCreatedAt(oldEntity.get().getCreatedAt());
         customerBooking.setCreatedBy(oldEntity.get().getCreatedBy());
         customerBooking.setIsPlatformBookingCreated(isCreatedInPlatform);
+        customerBooking.setSource(oldEntity.get().getSource());
 
         // NPM update contract
         if (oldEntity.get().getBookingCharges() != null) {
