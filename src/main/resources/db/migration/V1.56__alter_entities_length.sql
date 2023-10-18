180d70d85d3217a21e211a74bf056feab15ef799
ChiragBansal <chiragbansal@Macs-MacBook-Pro.local>
2023-10-18 22:23:51 +0530

    fix migration

--- src/main/resources/db/migration/V1.56__alter_entities_length.sql
+++ src/main/resources/db/migration/V1.56__alter_entities_length.sql
@@ -223,8 +223,8 @@ ALTER TABLE shipment_details
     ALTER COLUMN transport_mode TYPE VARCHAR(4),
     ALTER COLUMN shipment_type TYPE VARCHAR(3),
     ALTER COLUMN master_bill TYPE VARCHAR(50),
-    ALTER COLUMN additional_terms TYPE VARCHAR(2048),
-    ALTER TABLE shipment_details
+    ALTER COLUMN additional_terms TYPE VARCHAR(2048);
+ALTER TABLE shipment_details
     ALTER COLUMN shipment_id TYPE VARCHAR(50),
     ALTER COLUMN volume_unit TYPE VARCHAR(10),
     ALTER COLUMN entry_detail TYPE VARCHAR(3),
