cd73c13281a8619e3f26e8f76ddda2757e77f68b
Wasim Jafar <wasim.jafar@dpworld.com>
2023-11-09 11:25:36 +0530

    Redis Integration for Master data Dependency

--- src/main/java/com/dpw/runner/shipment/services/entitytransfer/dto/EntityTransferMasterLists.java
+++ src/main/java/com/dpw/runner/shipment/services/entitytransfer/dto/EntityTransferMasterLists.java
@@ -3,12 +3,14 @@ package com.dpw.runner.shipment.services.entitytransfer.dto;
 import com.dpw.runner.shipment.services.entitytransfer.common.request.IEntityTranferBaseEntity;
 import lombok.*;
 
+import java.io.Serializable;
+
 @Data
 @ToString
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class EntityTransferMasterLists implements IEntityTranferBaseEntity {
+public class EntityTransferMasterLists implements IEntityTranferBaseEntity, Serializable {
     public int ItemType;
     public String ItemValue;
     public String ItemDescription;
