aaece6ea1b7703e93cb02715ecc9e5f85a14d2ab
Tanishq <tanishq.malhotra@dpworld.com>
2023-11-29 13:45:12 +0530

    update getHbl endpoint response into object structure

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ShipmentService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ShipmentService.java
@@ -22,10 +22,7 @@ import com.dpw.runner.shipment.services.dto.ContainerAPIsRequest.ShipmentContain
 import com.dpw.runner.shipment.services.dto.TrackingService.UniversalTrackingPayload;
 import com.dpw.runner.shipment.services.dto.patchRequest.ShipmentPatchRequest;
 import com.dpw.runner.shipment.services.dto.request.*;
-import com.dpw.runner.shipment.services.dto.response.AdditionalDetailResponse;
-import com.dpw.runner.shipment.services.dto.response.CarrierDetailResponse;
-import com.dpw.runner.shipment.services.dto.response.ShipmentDetailsResponse;
-import com.dpw.runner.shipment.services.dto.response.ShipmentListResponse;
+import com.dpw.runner.shipment.services.dto.response.*;
 import com.dpw.runner.shipment.services.dto.v1.request.ShipmentBillingListRequest;
 import com.dpw.runner.shipment.services.dto.v1.request.TIListRequest;
 import com.dpw.runner.shipment.services.dto.v1.request.WayBillNumberFilterRequest;
@@ -2263,7 +2260,7 @@ public class ShipmentService implements IShipmentService {
     @Override
     public ResponseEntity<?> generateCustomHouseBLNumber() {
         try {
-            return ResponseHelper.buildSuccessResponse(generateCustomHouseBL());
+            return ResponseHelper.buildSuccessResponse(GenerateCustomHblResponse.builder().hblNumber(generateCustomHouseBL()).build());
         } catch (Exception e) {
             throw new RunnerException(e.getMessage());
         }
