3f14d4832baf378c8d2d14294b9f1f7e3331381b
ChiragBansal <chiragbansal@Macs-MacBook-Pro.local>
2023-12-12 14:30:44 +0530

    Add bol filter

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ConsolidationService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ConsolidationService.java
@@ -252,7 +252,7 @@ public class ConsolidationService implements IConsolidationService {
             Map.entry("terminalCutoff", RunnerEntityMapping.builder().tableName("ConsolidationDetails").dataType(LocalDateTime.class).build()),
             Map.entry("verifiedGrossMassCutoff", RunnerEntityMapping.builder().tableName("ConsolidationDetails").dataType(LocalDateTime.class).build()),
             Map.entry("guid", RunnerEntityMapping.builder().tableName("ConsolidationDetails").dataType(UUID.class).fieldName("guid").build()),
-            Map.entry("bol", RunnerEntityMapping.builder().tableName("ConsolidationDetails").dataType(UUID.class).fieldName("bol").build())
+            Map.entry("bol", RunnerEntityMapping.builder().tableName("ConsolidationDetails").dataType(String.class).fieldName("bol").build())
     );
 
     @Override
