2a4a2f0866b744d4a6f11249ec031381ea8b0140
abhishekgoyal <abhishek.goyal@dpworld.com>
2024-01-17 11:53:42 +0530

    Permissions Changes

--- src/main/java/com/dpw/runner/shipment/services/aspects/PermissionsValidationAspect/CreateValidateAspect.java
+++ src/main/java/com/dpw/runner/shipment/services/aspects/PermissionsValidationAspect/CreateValidateAspect.java
@@ -1,12 +1,17 @@
 package com.dpw.runner.shipment.services.aspects.PermissionsValidationAspect;
 
+import com.dpw.runner.shipment.services.commons.requests.CommonRequestModel;
+import com.dpw.runner.shipment.services.dto.request.ConsolidationDetailsRequest;
+import com.dpw.runner.shipment.services.dto.request.ShipmentRequest;
 import com.dpw.runner.shipment.services.entity.ConsolidationDetails;
 import com.dpw.runner.shipment.services.entity.ShipmentDetails;
 import com.dpw.runner.shipment.services.exception.exceptions.RunnerException;
 import com.dpw.runner.shipment.services.utils.V1PermissionMapUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
@@ -18,34 +23,32 @@ import static com.dpw.runner.shipment.services.utils.V1PermissionMapUtil.getPerm
 
 @Aspect
 @Component
-@Slf4j
-public class RetrieveValidateAspect {
+public class CreateValidateAspect {
 
-    @AfterReturning(pointcut="execution(* com.dpw.runner.shipment.services.dao.impl.ShipmentDao.findById(..)) && args(Long)",
-            returning="shipment")
-    public void validateShipmentRetrieve(Optional<ShipmentDetails> shipment) throws RunnerException {
-        log.info("Validating Retrieve permissions on shipment entity");
-        List<String> userPermissions = PermissionsContext.getPermissions(SHIPMENT_RETRIEVE_PERMISSION);
+    @Before("execution(* com.dpw.runner.shipment.services.service.impl.ShipmentService.create(..)) && args(commonRequestModel)")
+    public void validateShipmentCreate(JoinPoint joinPoint, CommonRequestModel commonRequestModel) throws RunnerException {
+        ShipmentRequest shipment = (ShipmentRequest) commonRequestModel.getData();
+        List<String> userPermissions = PermissionsContext.getPermissions(SHIPMENT_CREATE_PERMISSION);
         int retrieveValidationFields = 4;
         Set<String> validatedFields = new HashSet<>();
-        if(shipment.isPresent()){
+        if(shipment != null){
             String transportMode = null, direction = null, shipmentType = null;
             Boolean domesticType = null;
-            if(shipment.get().getTransportMode() != null)
-                transportMode = shipment.get().getTransportMode().toLowerCase();
-            if(shipment.get().getDirection() != null)
-                direction = shipment.get().getDirection().toLowerCase();
-            if(shipment.get().getShipmentType() != null)
-                shipmentType = shipment.get().getShipmentType().toLowerCase();
-            if(shipment.get().getIsDomestic() != null)
-                domesticType = shipment.get().getIsDomestic();
+            if(shipment.getTransportMode() != null)
+                transportMode = shipment.getTransportMode().toLowerCase();
+            if(shipment.getDirection() != null)
+                direction = shipment.getDirection().toLowerCase();
+            if(shipment.getShipmentType() != null)
+                shipmentType = shipment.getShipmentType().toLowerCase();
+            if(shipment.getIsDomestic() != null)
+                domesticType = shipment.getIsDomestic();
 
             for (String permission : userPermissions){
                 String v1MappedPermission = V1PermissionMapUtil.getPermissionName(permission);
                 if(v1MappedPermission == null)
                     continue;
                 List<String> parameterList = Arrays.stream(getPermissionName(permission).toLowerCase().split(DELIMITER))
-                        .filter(e -> !e.contains("retrieve"))
+                        .filter(e -> !e.contains("create"))
                         .collect(Collectors.toList());
                 String validTransportMode = getParameterFromPermission(TRANSPORT_MODE_INDEX, parameterList);
                 String validDirection = getParameterFromPermission(DIRECTION_INDEX, parameterList);
@@ -69,35 +72,34 @@ public class RetrieveValidateAspect {
             }
 
             if (validatedFields.size() < retrieveValidationFields)
-                throw new RunnerException("Unavailable to retrieve record due to insufficient retrieve permissions");
+                throw new RunnerException("Unable to create record due to insufficient create permissions");
         }
     }
 
-    @AfterReturning(pointcut="execution(* com.dpw.runner.shipment.services.dao.impl.ConsolidationDao.findById(..)) && args(Long)",
-            returning="consolidation")
-    public void validateConsolidationRetrieve(Optional<ConsolidationDetails> consolidation) throws RunnerException {
-        log.info("Validating Retrieve permissions on consolidation entity");
-        List<String> userPermissions = PermissionsContext.getPermissions(CONSOLIDATION_RETRIEVE_PERMISSION);
+    @Before("execution(* com.dpw.runner.shipment.services.service.impl.ConsolidationService.create(..)) && args(commonRequestModel)")
+    public void validateConsolidationCreate(JoinPoint joinPoint, CommonRequestModel commonRequestModel) throws RunnerException {
+        List<String> userPermissions = PermissionsContext.getPermissions(CONSOLIDATION_CREATE_PERMISSION);
         int retrieveValidationFields = 4;
         Set<String> validatedFields = new HashSet<>();
-        if(consolidation.isPresent()){
+        ConsolidationDetailsRequest consolidation = (ConsolidationDetailsRequest) commonRequestModel.getData();
+        if(consolidation != null){
             String transportMode = null, direction = null, shipmentType = null;
             Boolean domesticType = null;
-            if(consolidation.get().getTransportMode() != null)
-                transportMode = consolidation.get().getTransportMode().toLowerCase();
-            if(consolidation.get().getShipmentType() != null)
-                direction = consolidation.get().getShipmentType().toLowerCase();
-            if(consolidation.get().getContainerCategory() != null)
-                shipmentType = consolidation.get().getContainerCategory().toLowerCase();
-            if(consolidation.get().getIsDomestic() != null)
-                domesticType = consolidation.get().getIsDomestic();
+            if(consolidation.getTransportMode() != null)
+                transportMode = consolidation.getTransportMode().toLowerCase();
+            if(consolidation.getShipmentType() != null)
+                direction = consolidation.getShipmentType().toLowerCase();
+            if(consolidation.getContainerCategory() != null)
+                shipmentType = consolidation.getContainerCategory().toLowerCase();
+            if(consolidation.getIsDomestic() != null)
+                domesticType = consolidation.getIsDomestic();
 
             for (String permission : userPermissions){
                 String v1MappedPermission = V1PermissionMapUtil.getPermissionName(permission);
                 if(v1MappedPermission == null)
                     continue;
                 List<String> parameterList = Arrays.stream(getPermissionName(permission).toLowerCase().split(DELIMITER))
-                        .filter(e -> !e.contains("retrieve"))
+                        .filter(e -> !e.contains("create"))
                         .collect(Collectors.toList());
                 String validTransportMode = getParameterFromPermission(TRANSPORT_MODE_INDEX, parameterList);
                 String validDirection = getParameterFromPermission(DIRECTION_INDEX, parameterList);
@@ -121,7 +123,7 @@ public class RetrieveValidateAspect {
             }
 
             if (validatedFields.size() < retrieveValidationFields)
-                throw new RunnerException("Unavailable to retrieve record due to insufficient retrieve permissions");
+                throw new RunnerException("Unable to create record due to insufficient create permissions");
         }
     }
 }
