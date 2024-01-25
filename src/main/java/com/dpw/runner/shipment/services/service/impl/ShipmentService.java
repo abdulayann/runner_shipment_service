5e3cab2eddf3771cb96c2b1b4ab571c7e88c99cf
mayankgupta <mayank.gupta@dpworld.com>
2024-01-25 11:38:38 +0530

    api changes

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ShipmentService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ShipmentService.java
@@ -4072,9 +4072,17 @@ public class ShipmentService implements IShipmentService {
             Long consolidationId = request.getConsolidationId();
             List<ShipmentsContainersMapping> shipmentsContainersMappingList = shipmentsContainersMappingDao.findByShipmentId(shipmentId);
             List<Containers> containers = containerDao.findByConsolidationId(consolidationId);
+            boolean showDialog = false;
             if(shipmentsContainersMappingList != null && containers != null && containers.size() != shipmentsContainersMappingList.size())
-                return ResponseHelper.buildSuccessResponse(true);
-            return ResponseHelper.buildSuccessResponse(false);
+                showDialog = true;
+            int numberOfShipments = 0;
+            List<ConsoleShipmentMapping> consoleShipmentMappings = consoleShipmentMappingDao.findByConsolidationId(consolidationId);
+            if(consoleShipmentMappings != null && consoleShipmentMappings.size() > 0)
+                numberOfShipments = consoleShipmentMappings.size();
+            AssignAllDialogDto response = new AssignAllDialogDto();
+            response.setShowDialog(showDialog);
+            response.setNumberOfShipments(numberOfShipments);
+            return ResponseHelper.buildSuccessResponse(response);
         } catch (Exception e) {
             responseMsg = e.getMessage() != null ? e.getMessage()
                     : DaoConstants.DAO_GENERIC_RETRIEVE_EXCEPTION_MSG;
