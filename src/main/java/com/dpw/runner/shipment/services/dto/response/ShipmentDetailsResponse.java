a93f664ada3b79338ce4045cf51d8b5fa5e506cd
Pardeep Malik <Pardeep.Malik@dpworld.com>
2024-02-05 19:29:19 +0530

    Shipment Count field added for shipment retrieve response

--- src/main/java/com/dpw/runner/shipment/services/dto/response/ShipmentDetailsResponse.java
+++ src/main/java/com/dpw/runner/shipment/services/dto/response/ShipmentDetailsResponse.java
@@ -174,4 +174,5 @@ public class ShipmentDetailsResponse implements IRunnerResponse {
     private Long clientDpsAddressId;
     private Long consignorDpsAddressId;
     private Long notifyPartyDpsAddressId;
+    private Long shipmentCount;
 }
