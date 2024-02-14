bbe353ae6e8c5b88b4ed299c33d6bf710e216e5e
abhishekgoyal <abhishek.goyal@dpworld.com>
2024-02-14 12:22:58 +0530

    added audit logs

--- src/main/java/com/dpw/runner/shipment/services/dao/impl/ServiceDetailsDao.java
+++ src/main/java/com/dpw/runner/shipment/services/dao/impl/ServiceDetailsDao.java
@@ -1,10 +1,16 @@
 package com.dpw.runner.shipment.services.dao.impl;
 
 import com.dpw.runner.shipment.services.commons.constants.DaoConstants;
+import com.dpw.runner.shipment.services.commons.enums.DBOperationType;
+import com.dpw.runner.shipment.services.commons.requests.AuditLogMetaData;
 import com.dpw.runner.shipment.services.commons.requests.ListCommonRequest;
 import com.dpw.runner.shipment.services.dao.interfaces.IServiceDetailsDao;
 import com.dpw.runner.shipment.services.entity.ServiceDetails;
+import com.dpw.runner.shipment.services.entity.ShipmentDetails;
+import com.dpw.runner.shipment.services.helpers.JsonHelper;
 import com.dpw.runner.shipment.services.repository.interfaces.IServiceDetailsRepository;
+import com.dpw.runner.shipment.services.service.interfaces.IAuditLogService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.nimbusds.jose.util.Pair;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +20,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Repository;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -26,6 +33,10 @@ import static com.dpw.runner.shipment.services.utils.CommonUtils.constructListCo
 public class ServiceDetailsDao implements IServiceDetailsDao {
     @Autowired
     private IServiceDetailsRepository serviceDetailsRepository;
+    @Autowired
+    private JsonHelper jsonHelper;
+    @Autowired
+    private IAuditLogService auditLogService;
 
     @Override
     public ServiceDetails save(ServiceDetails serviceDetails) {
@@ -76,7 +87,7 @@ public class ServiceDetailsDao implements IServiceDetailsDao {
                 }
                 responseServiceDetails = saveEntityFromShipment(serviceDetailsRequests, shipmentId, copyHashMap);
             }
-            deleteServiceDetails(hashMap);
+            deleteServiceDetails(hashMap, ShipmentDetails.class.getSimpleName(), shipmentId);
             return responseServiceDetails;
         } catch (Exception e) {
             responseMsg = e.getMessage() != null ? e.getMessage()
@@ -89,6 +100,8 @@ public class ServiceDetailsDao implements IServiceDetailsDao {
     public List<ServiceDetails> saveEntityFromShipment(List<ServiceDetails> serviceDetailsRequests, Long shipmentId) {
         List<ServiceDetails> res = new ArrayList<>();
         for(ServiceDetails req : serviceDetailsRequests){
+            String oldEntityJsonString = null;
+            String operation = DBOperationType.CREATE.name();
             if(req.getId() != null){
                 long id = req.getId();
                 Optional<ServiceDetails> oldEntity = findById(id);
@@ -98,9 +111,23 @@ public class ServiceDetailsDao implements IServiceDetailsDao {
                 }
                 req.setCreatedAt(oldEntity.get().getCreatedAt());
                 req.setCreatedBy(oldEntity.get().getCreatedBy());
+                oldEntityJsonString = jsonHelper.convertToJson(oldEntity.get());
+                operation = DBOperationType.UPDATE.name();
             }
             req.setShipmentId(shipmentId);
             req = save(req);
+            try {
+                auditLogService.addAuditLog(
+                        AuditLogMetaData.builder()
+                                .newData(req)
+                                .prevData(oldEntityJsonString != null ? jsonHelper.readFromJson(oldEntityJsonString, ServiceDetails.class) : null)
+                                .parent(ShipmentDetails.class.getSimpleName())
+                                .parentId(shipmentId)
+                                .operation(operation).build()
+                );
+            } catch (IllegalAccessException | NoSuchFieldException | JsonProcessingException | InvocationTargetException | NoSuchMethodException e) {
+                log.error(e.getMessage());
+            }
             res.add(req);
         }
         return res;
@@ -108,6 +135,7 @@ public class ServiceDetailsDao implements IServiceDetailsDao {
     @Override
     public List<ServiceDetails> saveEntityFromShipment(List<ServiceDetails> serviceDetailsRequests, Long shipmentId, Map<Long, ServiceDetails> oldEntityMap) {
         List<ServiceDetails> res = new ArrayList<>();
+        Map<Long, String> oldEntityJsonStringMap = new HashMap<>();
         for(ServiceDetails req : serviceDetailsRequests){
             if(req.getId() != null){
                 long id = req.getId();
@@ -117,18 +145,58 @@ public class ServiceDetailsDao implements IServiceDetailsDao {
                 }
                 req.setCreatedAt(oldEntityMap.get(id).getCreatedAt());
                 req.setCreatedBy(oldEntityMap.get(id).getCreatedBy());
+                String oldEntityJsonString = jsonHelper.convertToJson(oldEntityMap.get(id));
+                oldEntityJsonStringMap.put(id, oldEntityJsonString);
             }
             req.setShipmentId(shipmentId);
             res.add(req);
         }
         res = saveAll(res);
+        for (ServiceDetails req : res) {
+            String oldEntityJsonString = null;
+            String operation = DBOperationType.CREATE.name();
+            if(oldEntityJsonStringMap.containsKey(req.getId())){
+                oldEntityJsonString = oldEntityJsonStringMap.get(req.getId());
+                operation = DBOperationType.UPDATE.name();
+            }
+            try {
+                auditLogService.addAuditLog(
+                        AuditLogMetaData.builder()
+                                .newData(req)
+                                .prevData(oldEntityJsonString != null ? jsonHelper.readFromJson(oldEntityJsonString, ServiceDetails.class) : null)
+                                .parent(ShipmentDetails.class.getSimpleName())
+                                .parentId(shipmentId)
+                                .operation(operation).build()
+                );
+            } catch (IllegalAccessException | NoSuchFieldException | JsonProcessingException | InvocationTargetException | NoSuchMethodException e) {
+                log.error(e.getMessage());
+            }
+        }
         return res;
     }
 
-    private void deleteServiceDetails(Map<Long, ServiceDetails> hashMap) {
+    private void deleteServiceDetails(Map<Long, ServiceDetails> hashMap, String entity, Long entityId) {
         String responseMsg;
         try {
-            hashMap.values().forEach(this::delete);
+            hashMap.values().forEach(serviceDetail -> {
+                String json = jsonHelper.convertToJson(serviceDetail);
+                delete(serviceDetail);
+                if(entity != null)
+                {
+                    try {
+                        auditLogService.addAuditLog(
+                                AuditLogMetaData.builder()
+                                        .newData(null)
+                                        .prevData(jsonHelper.readFromJson(json, ServiceDetails.class))
+                                        .parent(entity)
+                                        .parentId(entityId)
+                                        .operation(DBOperationType.DELETE.name()).build()
+                        );
+                    } catch (IllegalAccessException | NoSuchFieldException | JsonProcessingException | InvocationTargetException | NoSuchMethodException e) {
+                        log.error(e.getMessage());
+                    }
+                }
+            });
         } catch (Exception e) {
             responseMsg = e.getMessage() != null ? e.getMessage()
                     : DaoConstants.DAO_GENERIC_DELETE_EXCEPTION_MSG;
@@ -163,7 +231,7 @@ public class ServiceDetailsDao implements IServiceDetailsDao {
             }
             Map<Long, ServiceDetails> hashMap = new HashMap<>();
             serviceDetailsMap.forEach((s, serviceDetails) ->  hashMap.put(serviceDetails.getId(), serviceDetails));
-            deleteServiceDetails(hashMap);
+            deleteServiceDetails(hashMap, ShipmentDetails.class.getSimpleName(), shipmentId);
             return responseServiceDetails;
         } catch (Exception e) {
             responseMsg = e.getMessage() != null ? e.getMessage()
