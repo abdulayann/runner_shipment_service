22a61046e431cebc35f2ea4f3577755204279233
Tanishq <tanishq.malhotra@dpworld.com>
2024-02-27 12:22:52 +0530

    replace max shipment id with serial counter value for hbl generation

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ShipmentService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ShipmentService.java
@@ -2672,9 +2672,6 @@ public class ShipmentService implements IShipmentService {
     private String getShipmentsSerialNumber() {
         // Moving this responsibility to v1 sequnce table to avoid syncing overhead
         return v1Service.getShipmentSerialNumber();
-//        SequenceIncrementor sequenceIncrementor = SequenceIncrementor.builder().entityId(1L).build();
-//        sequenceIncrementorDao.save(sequenceIncrementor);
-//        return sequenceIncrementor.getShipmentIncrementId().toString();
     }
 
     private String createShipmentSequence(ShipmentSettingsDetails shipmentSetting) {
@@ -3679,8 +3676,7 @@ public class ShipmentService implements IShipmentService {
                     res += StringUtility.getRandomString(10);
                     break;
                 case "Serial" :
-                    String serialNumber = v1Service.getMaxShipmentId();
-//                    Long serialNumber = shipmentDao.findMaxId() + 1;
+                    String serialNumber = getShipmentsSerialNumber();
                     res += serialNumber;
                     break;
                 default : res = "";
