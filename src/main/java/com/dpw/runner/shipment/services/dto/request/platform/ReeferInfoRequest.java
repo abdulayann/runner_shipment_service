9beaf8e1fa6a212a989d152efb192000a2b3ce44
Pardeep Malik <Pardeep.Malik@dpworld.com>
2024-03-04 16:55:47 +0530

    Sonar fixes

--- src/main/java/com/dpw/runner/shipment/services/dto/request/platform/ReeferInfoRequest.java
+++ src/main/java/com/dpw/runner/shipment/services/dto/request/platform/ReeferInfoRequest.java
@@ -3,9 +3,11 @@ package com.dpw.runner.shipment.services.dto.request.platform;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
 @Builder
-public class ReeferInfoRequest {
+public class ReeferInfoRequest implements Serializable {
     private Integer temperature;
     private Integer ventilation;
     private Integer humidity;
