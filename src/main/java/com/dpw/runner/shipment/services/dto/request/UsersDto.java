ed01796b841149b46951b2623a0e0e5c822084d5
abhimanyu.chauhan <abhimanyu.chauhan@dpworld.com>
2024-03-06 16:04:08 +0530

    ViewsService Unit Tests

--- src/main/java/com/dpw/runner/shipment/services/dto/request/UsersDto.java
+++ src/main/java/com/dpw/runner/shipment/services/dto/request/UsersDto.java
@@ -13,6 +13,8 @@ import java.util.Map;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
+@Builder
 public class UsersDto implements Serializable {
     private static final long serialVersionUID = 1L;
     private Integer Id;
