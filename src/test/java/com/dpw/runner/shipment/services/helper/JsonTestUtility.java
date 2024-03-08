eebd4f7d658b8defbae4734d0f02c8dfcfd101fc
Tanishq <tanishq.malhotra@dpworld.com>
2024-03-08 19:59:15 +0530

    awb service ut

---
+++ src/test/java/com/dpw/runner/shipment/services/helper/JsonTestUtility.java
@@ -0,0 +1,63 @@
+package com.dpw.runner.shipment.services.helper;
+
+import com.dpw.runner.shipment.services.entity.Awb;
+import com.dpw.runner.shipment.services.entity.ShipmentDetails;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+
+public class JsonTestUtility {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private String path = "src/test/java/com/dpw/runner/shipment/services/helper/payload.json";
+    private Map<String, Object> payload;
+
+    static {
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.configure(SerializationFeature.FAIL_ON_SELF_REFERENCES, false);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES, false);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+    public static ObjectMapper getMapper() {
+        return objectMapper;
+    }
+
+    public JsonTestUtility() throws IOException {
+        payload = objectMapper.readValue(new File(path), Map.class);
+    }
+
+    public ShipmentDetails getTestShipment() {
+        ShipmentDetails shipmentDetails = objectMapper.convertValue(payload.get("SHIPMENT"), ShipmentDetails.class);
+        return shipmentDetails;
+    }
+
+    public Awb getTestHawb() {
+        Awb awb = objectMapper.convertValue(payload.get("HAWB"), Awb.class);
+        return awb;
+    }
+
+    public Awb getTestDmawb() {
+        Awb awb = objectMapper.convertValue(payload.get("DMAWB"), Awb.class);
+        return awb;
+    }
+
+    public Awb getTestMawb() {
+        Awb awb = objectMapper.convertValue(payload.get("MAWB"), Awb.class);
+        return awb;
+    }
+
+    public <T,F> List<F> convertValueToList(T object, Class<F> clazz) {
+        return objectMapper.convertValue(object, objectMapper.getTypeFactory().constructCollectionType(List.class, clazz));
+    }
+
+}
