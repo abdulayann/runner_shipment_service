56da2417172717a20999dc8117ea4f1c30927316
abhimanyu.chauhan <abhimanyu.chauhan@dpworld.com>
2024-03-11 17:10:25 +0530

    Dao Layer Tests POC

--- src/main/java/com/dpw/runner/shipment/services/adapters/interfaces/INPMServiceAdapter.java
+++ src/main/java/com/dpw/runner/shipment/services/adapters/interfaces/INPMServiceAdapter.java
@@ -1,14 +1,10 @@
 package com.dpw.runner.shipment.services.adapters.interfaces;
 
-import com.dpw.runner.shipment.services.commons.requests.CommonRequest;
 import com.dpw.runner.shipment.services.commons.requests.CommonRequestModel;
-import com.dpw.runner.shipment.services.commons.responses.DependentServiceResponse;
 import com.dpw.runner.shipment.services.commons.responses.IRunnerResponse;
-import com.dpw.runner.shipment.services.dto.request.npm.UpdateContractRequest;
 import com.dpw.runner.shipment.services.dto.response.npm.NPMFetchLangChargeCodeResponse;
 import com.dpw.runner.shipment.services.exception.exceptions.RunnerException;
 import org.springframework.http.ResponseEntity;
-import org.testcontainers.shaded.org.bouncycastle.jcajce.provider.asymmetric.ec.KeyFactorySpi;
 
 public interface INPMServiceAdapter {
     ResponseEntity<IRunnerResponse> fetchContracts(CommonRequestModel commonRequestModel) throws RunnerException;
