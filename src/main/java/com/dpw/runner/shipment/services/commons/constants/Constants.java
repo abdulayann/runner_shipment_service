e90cc7663c34c733e086a43990ffaaa8bc03a6f1
mayankgupta <mayank.gupta@dpworld.com>
2024-03-12 12:23:57 +0530

    service detail mandatory

--- src/main/java/com/dpw/runner/shipment/services/commons/constants/Constants.java
+++ src/main/java/com/dpw/runner/shipment/services/commons/constants/Constants.java
@@ -42,6 +42,7 @@ public class Constants {
     public static final String CONTAINER = "CONTAINER";
     public static final String CARRIAGE = "CARRIAGE";
     public static final String PreCarriage = "PreCarriage";
+    public static final String SERVICE_DETAILS = "SERVICE_DETAILS";
     public static final String Main = "Main";
     public static final String EVENTS = "EVENTS";
     public static final String JOBS = "JOBS";
