4e8cf002b890a7d65d32ed8b30808768beb84afa
mayankgupta <mayank.gupta@dpworld.com>
2024-03-14 16:27:50 +0530

    xss

---
+++ src/main/java/com/dpw/runner/shipment/services/config/MvcConfig.java
@@ -0,0 +1,15 @@
+package com.dpw.runner.shipment.services.config;
+
+import com.dpw.runner.shipment.services.validator.XSSInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new XSSInterceptor()).addPathPatterns("/**");
+    }
+
+}
---
+++ src/main/java/com/dpw/runner/shipment/services/validator/XSSInterceptor.java
@@ -0,0 +1,58 @@
+package com.dpw.runner.shipment.services.validator;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class XSSInterceptor implements HandlerInterceptor {
+
+//    private final PolicyFactory policy = Sanitizers.FORMATTING.and(Sanitizers.LINKS);
+//
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        ContentCachingRequestWrapper wrappedRequest = new ContentCachingRequestWrapper(request);
+//        String requestBody = getRequestBody(wrappedRequest);
+//        if (!requestBody.isEmpty()) {
+//            try {
+//                validateXSS(requestBody);
+//            } catch (Exception e) {
+//                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+//                response.getWriter().write(e.toString());
+//                response.getWriter().flush();
+//                response.getWriter().close();
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//
+//    private String getRequestBody(ContentCachingRequestWrapper request) throws IOException {
+//        StringBuilder requestBody = new StringBuilder();
+//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()))) {
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//                requestBody.append(line);
+//            }
+//        }
+//        return requestBody.toString();
+//    }
+//
+//    public void validateXSS(String jsonString) throws Exception {
+//        ObjectMapper mapper = new ObjectMapper();
+//        JsonNode jsonNode = mapper.readTree(jsonString);
+//        Iterator<Map.Entry<String, JsonNode>> fieldsIterator = jsonNode.fields();
+//        while (fieldsIterator.hasNext()) {
+//            Map.Entry<String, JsonNode> fieldEntry = fieldsIterator.next();
+//            String fieldName = fieldEntry.getKey();
+//            JsonNode fieldValue = fieldEntry.getValue();
+//            if (fieldValue.isTextual() && containsXSS(fieldValue.asText())) {
+//                throw new RuntimeException("Use of Invalid characters. Please ensure no special characters used such as '<', '>' and '&'. Error occurred in field: " + fieldName);
+//            }
+//        }
+//    }
+//
+//    private boolean containsXSS(String input) {
+//        String sanitizedInput = policy.sanitize(input);
+//        return !input.equals(sanitizedInput);
+//    }
+}
