02fa21cc895ddb1b8aec0022362914688085ed68
Pardeep Malik <Pardeep.Malik@dpworld.com>
2024-04-02 15:28:41 +0530

    watermark added for Hawb EAW case

--- src/main/java/com/dpw/runner/shipment/services/commons/constants/Constants.java
+++ src/main/java/com/dpw/runner/shipment/services/commons/constants/Constants.java
@@ -400,4 +400,5 @@ public class Constants {
     public static final String EVENT = "event";
     public static final String TYPE = "Type";
     public static final String DESCARTES = "Descartes";
+    public static final String XFNM = "XFNM";
 }
