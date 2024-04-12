32bc52a60aa468f49356c8fb1fc86fc9224e6a01
mayankgupta <mayank.gupta@dpworld.com>
2024-04-12 16:03:46 +0530

    changes

--- src/main/java/com/dpw/runner/shipment/services/ReportingService/CommonUtils/ReportHelper.java
+++ src/main/java/com/dpw/runner/shipment/services/ReportingService/CommonUtils/ReportHelper.java
@@ -41,12 +41,12 @@ public class ReportHelper {
         String temp = getCommaSeparatedAddress(partiesModel.getAddressData(), ReportConstants.CITY, null);
         temp = getCommaSeparatedAddress(partiesModel.getAddressData(), ReportConstants.STATE, temp);
         temp = getCommaSeparatedAddress(partiesModel.getAddressData(), ReportConstants.COUNTRY, temp);
-        temp = getCommaSeparatedAddress(partiesModel.getAddressData(), ReportConstants.ZIPCODE, temp);
+        temp = getCommaSeparatedAddress(partiesModel.getAddressData(), ReportConstants.ZIP_POST_CODE, temp);
         if(!CommonUtils.IsStringNullOrEmpty(temp)) {
             if(response == null)
                 response = temp;
             else
-                response = response + "\n" + temp;
+                response = response + ",\n" + temp;
         }
         return response;
     }
@@ -57,7 +57,7 @@ public class ReportHelper {
             if(response == null)
                 response = x;
             else
-                response = response + "\n" + x;
+                response = response + ",\n" + x;
         }
         return response;
     }
