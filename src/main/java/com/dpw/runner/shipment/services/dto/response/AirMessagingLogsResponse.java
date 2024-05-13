3d10d317a262eae3de3621dbf81602f378047bae
Pardeep Malik <Pardeep.Malik@dpworld.com>
2024-05-13 15:23:15 +0530

    date time format fix

--- src/main/java/com/dpw/runner/shipment/services/dto/response/AirMessagingLogsResponse.java
+++ src/main/java/com/dpw/runner/shipment/services/dto/response/AirMessagingLogsResponse.java
@@ -1,6 +1,8 @@
 package com.dpw.runner.shipment.services.dto.response;
 
 import com.dpw.runner.shipment.services.commons.responses.IRunnerResponse;
+import com.dpw.runner.shipment.services.config.CustomLocalDateTimeSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,5 +23,6 @@ public class AirMessagingLogsResponse implements IRunnerResponse {
     private String messageType;
     private String xmlPayload;
     private String status;
+    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
     private LocalDateTime createdAt;
 }
