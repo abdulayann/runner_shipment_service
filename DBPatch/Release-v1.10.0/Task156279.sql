432dedeeca5f7b7fc67629668f1120df2caf3c91
Anupam Gupta <anupam.rtp@gmail.com>
2024-05-22 18:15:26 +0530

    Fixed shipment_id value

--- DBPatch/Release-v1.10.0/Task156279.sql
+++ DBPatch/Release-v1.10.0/Task156279.sql
@@ -1,1 +1,1 @@
-update shipment_details set status = 3, updated_at = now() where shipment_id = 'BOMS24Added050212' and tenant_id = 536;
+update shipment_details set status = 3, updated_at = now() where shipment_id = 'BOMS24050212' and tenant_id = 536;
