2c8d41a168653e60d870c01db9695ad6bb7dd123
ChiragBansal <chiragbansal@Macs-MacBook-Pro.local>
2024-05-29 21:37:26 +0530

    complete report test

--- src/main/java/com/dpw/runner/shipment/services/ReportingService/Reports/MawbReport.java
+++ src/main/java/com/dpw/runner/shipment/services/ReportingService/Reports/MawbReport.java
@@ -2,11 +2,7 @@ package com.dpw.runner.shipment.services.ReportingService.Reports;
 
 import com.dpw.runner.shipment.services.ReportingService.Models.HawbModel;
 import com.dpw.runner.shipment.services.ReportingService.Models.IDocumentModel;
-import com.dpw.runner.shipment.services.ReportingService.Models.MawbModel;
 import com.dpw.runner.shipment.services.aspects.MultitenancyAspect.UserContext;
-import com.dpw.runner.shipment.services.commons.constants.Constants;
-import com.dpw.runner.shipment.services.entity.Parties;
-import com.dpw.runner.shipment.services.service.impl.AwbService;
 import com.dpw.runner.shipment.services.service.v1.util.V1ServiceUtil;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
