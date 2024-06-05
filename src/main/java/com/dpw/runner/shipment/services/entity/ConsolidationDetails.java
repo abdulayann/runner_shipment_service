6c203818b9ff712c80a74597df658b4ac538befa
mayankgupta <mayank.gupta@dpworld.com>
2024-06-05 13:51:24 +0530

    dg fields migration

--- src/main/java/com/dpw/runner/shipment/services/entity/ConsolidationDetails.java
+++ src/main/java/com/dpw/runner/shipment/services/entity/ConsolidationDetails.java
@@ -401,11 +401,11 @@ public class ConsolidationDetails extends MultiTenancy {
     @Column(name = "hazardous")
     private Boolean hazardous;
 
-//    @Column(name = "emergency_contact_number")
-//    @Size(max=31, message = "max size is 31 for emergency_contact_number")
-//    private String emergencyContactNumber;
-//
-//    @Column(name = "emergency_contact_number_code")
-//    @Size(max=31, message = "max size is 31 for emergency_contact_number_code")
-//    private String emergencyContactNumberCode;
+    @Column(name = "emergency_contact_number")
+    @Size(max=31, message = "max size is 31 for emergency_contact_number")
+    private String emergencyContactNumber;
+
+    @Column(name = "emergency_contact_number_code")
+    @Size(max=31, message = "max size is 31 for emergency_contact_number_code")
+    private String emergencyContactNumberCode;
 }
