d5464a61e8dc5084e846710a499980910169aa9c
Tanishq <tanishq.malhotra@dpworld.com>
2024-06-12 13:33:12 +0530

    bugfix #160149

--- src/main/java/com/dpw/runner/shipment/services/ReportingService/Reports/IReport.java
+++ src/main/java/com/dpw/runner/shipment/services/ReportingService/Reports/IReport.java
@@ -518,7 +518,7 @@ public abstract class IReport {
         dictionary.put(ReportConstants.DESTINATION_NAME_, destination != null ? destination.getName() : null);
         dictionary.put(ReportConstants.DESTINATION, destination != null ? destination.getName() : null);
         dictionary.put(ReportConstants.DESTINATION_COUNTRY, destination != null ? destination.getCountry() : null);
-        dictionary.put(ReportConstants.DESTINATION_COUNTRY, destination != null ? destination.getCountryName() : null);
+        dictionary.put(DESTINATION_COUNTRY_NAME, destination != null ? destination.getCountryThreeDigitCode() : null);
 
         dictionary.put(ReportConstants.PRINT_DATE, ConvertToDPWDateFormat(LocalDateTime.now(), tsDateTimeFormat));
         if(destination != null) {
@@ -644,7 +644,7 @@ public abstract class IReport {
             PartiesModel notifyParty1 = null;
             List<PartiesModel> shipmentAddresses = shipment.getShipmentAddresses();
             for (PartiesModel party : shipmentAddresses) {
-                if(Objects.equals(party.getType(), "Notify Party 1"))
+                if(Objects.equals(party.getType(), "NP1"))
                 {
                     notifyParty1 = party;
                 }
