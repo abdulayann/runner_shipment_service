14ec3cee1032406078ed15bdca09f74e56bb8bf1
mayankgupta <mayank.gupta@dpworld.com>
2024-06-14 14:45:09 +0530

    awb packs dg keys

--- src/main/java/com/dpw/runner/shipment/services/dto/request/awb/AwbPackingInfo.java
+++ src/main/java/com/dpw/runner/shipment/services/dto/request/awb/AwbPackingInfo.java
@@ -73,6 +73,8 @@ public class AwbPackingInfo implements Serializable {
     private String awbNumber;
     private UUID mawbGoodsDescGuid;
     private UUID awbGoodsDescriptionInfoGuid;
-
     private Long mawbGoodsDescId;
+    private String unNumberAir;
+    private String dgClassAir;
+    private String dgClassAirDescription;
 }
