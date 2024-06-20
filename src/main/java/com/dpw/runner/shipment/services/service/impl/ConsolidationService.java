79b7da414e03050430251f1278d9613ee768058f
peeyushpandey <peeyush.pandey@dpworld.com>
2024-06-20 13:47:29 +0530

    RaKc Consol

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ConsolidationService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ConsolidationService.java
@@ -30,6 +30,7 @@ import com.dpw.runner.shipment.services.dto.request.*;
 import com.dpw.runner.shipment.services.dto.response.*;
 import com.dpw.runner.shipment.services.dto.v1.request.ConsoleBookingIdFilterRequest;
 import com.dpw.runner.shipment.services.dto.v1.response.GuidsListResponse;
+import com.dpw.runner.shipment.services.dto.v1.response.OrgAddressResponse;
 import com.dpw.runner.shipment.services.dto.v1.response.V1DataResponse;
 import com.dpw.runner.shipment.services.dto.v1.response.V1TenantSettingsResponse;
 import com.dpw.runner.shipment.services.entity.*;
@@ -56,6 +57,7 @@ import com.dpw.runner.shipment.services.service.interfaces.IConsolidationService
 import com.dpw.runner.shipment.services.service.interfaces.IContainerService;
 import com.dpw.runner.shipment.services.service.interfaces.IPackingService;
 import com.dpw.runner.shipment.services.service.v1.IV1Service;
+import com.dpw.runner.shipment.services.service.v1.util.V1ServiceUtil;
 import com.dpw.runner.shipment.services.service_bus.AzureServiceBusTopic;
 import com.dpw.runner.shipment.services.service_bus.ISBProperties;
 import com.dpw.runner.shipment.services.service_bus.ISBUtils;
@@ -241,6 +243,9 @@ public class ConsolidationService implements IConsolidationService {
 
     @Autowired @Lazy
     private BookingIntegrationsUtility bookingIntegrationsUtility;
+
+    @Autowired
+    private V1ServiceUtil v1ServiceUtil;
 
     @Value("${consolidationsKafka.queue}")
     private String senderQueue;
@@ -2887,6 +2892,61 @@ public class ConsolidationService implements IConsolidationService {
                 awbDao.save(awb);
             }
         }
+
+        Parties sendingAgent = consolidationDetails.getSendingAgent();
+        V1TenantSettingsResponse tenantSettingsResponse = TenantSettingsDetailsContext.getCurrentTenantSettings();
+        if(Boolean.TRUE.equals(tenantSettingsResponse.getEnableAirMessaging()) && Objects.equals(consolidationDetails.getTransportMode(), Constants.TRANSPORT_MODE_AIR)) {
+            List<Parties> orgList = new ArrayList<>();
+            if(sendingAgent != null) {
+                if(sendingAgent != null && StringUtility.isNotEmpty(sendingAgent.getAddressCode())) {
+                    orgList.add(sendingAgent);
+                }
+            }
+
+            if(orgList.size() > 0) {
+                OrgAddressResponse orgAddressResponse = v1ServiceUtil.fetchOrgInfoFromV1(orgList);
+                if (orgAddressResponse != null) {
+                    Map<String, Map<String, Object>> addressMap = orgAddressResponse.getAddresses();
+                    if(sendingAgent != null) {
+                        if (addressMap.containsKey(sendingAgent.getOrgCode() + "#" + sendingAgent.getAddressCode())) {
+                            Map<String, Object> addressConsignorAgent = addressMap.get(sendingAgent.getOrgCode() + "#" + sendingAgent.getAddressCode());
+                            if (addressConsignorAgent.containsKey(Constants.RA_KC_TYPE)) {
+                                var rakcType = addressConsignorAgent.get(Constants.RA_KC_TYPE);
+                                if (rakcType != null && (Integer) rakcType == RAKCType.KNOWN_CONSIGNOR.getId() && (consolidationDetails.getScreeningStatus() == null ||
+                                        consolidationDetails.getScreeningStatus().isEmpty() ||
+                                        consolidationDetails.getSecurityStatus() == null)) {
+                                    throw new RunnerException("Screening Status and Security Status is mandatory for KC consginor.");
+                                }
+                            }
+                        }
+                    }
+
+                    if(consolidationDetails.getId() == null ) {
+                        if (consolidationDetails.getSendingAgent() != null && StringUtility.isNotEmpty(consolidationDetails.getSendingAgent().getAddressCode())) {
+                            if (!checkRaStatusFields(consolidationDetails, orgAddressResponse, consolidationDetails.getSendingAgent())) {
+                                throw new RunnerException("Screening Status and Security Status is mandatory for RA Origin Agent.");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean checkRaStatusFields(ConsolidationDetails consolidationDetails, OrgAddressResponse orgAddressResponse, Parties parties) {
+        Map<String, Map<String, Object>> addressMap = orgAddressResponse.getAddresses();
+        if (addressMap.containsKey(parties.getOrgCode() + "#" + parties.getAddressCode())) {
+            Map<String, Object> addressConsignorAgent = addressMap.get(parties.getOrgCode() + "#" + parties.getAddressCode());
+            if (addressConsignorAgent.containsKey(Constants.RA_KC_TYPE)) {
+                var rakcType = addressConsignorAgent.get(Constants.RA_KC_TYPE);
+                if (rakcType != null && (Integer)rakcType == RAKCType.REGULATED_AGENT.getId() && (consolidationDetails.getScreeningStatus() == null ||
+                        consolidationDetails.getScreeningStatus().isEmpty() ||
+                        consolidationDetails.getSecurityStatus() == null)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     private boolean checkDisableFetchConditionForAwb(ConsolidationDetails consolidationDetails, ConsolidationDetails oldEntity,ShipmentSettingsDetails shipmentSettingsDetails){
