62a77ed899e6f4c2baa37b2b942d5bc739e1b58e
abhishekgoyal <abhishek.goyal@dpworld.com>
2024-06-20 18:06:08 +0530

    booking date

--- src/main/java/com/dpw/runner/shipment/services/service/impl/CustomerBookingService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/CustomerBookingService.java
@@ -1292,7 +1292,7 @@ public class CustomerBookingService implements ICustomerBookingService {
             customerBookingResponse.setSource(null);
             customerBookingResponse.setCreatedBy(null);
             customerBookingResponse.setSourceGuid(customerBooking.get().getGuid());
-            customerBookingResponse.setBookingDate(null);
+            customerBookingResponse.setBookingDate(LocalDateTime.now());
             if(customerBookingResponse.getCustomer() != null)
             {
                 customerBookingResponse.getCustomer().setId(null);
