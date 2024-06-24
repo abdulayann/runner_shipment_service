420343d5bb6e29d74e604eb832598758c03349d1
mayankgupta <mayank.gupta@dpworld.com>
2024-06-24 15:41:31 +0530

    tests fix

--- src/test/java/com/dpw/runner/shipment/services/ReportingService/Reports/HblReportTest.java
+++ src/test/java/com/dpw/runner/shipment/services/ReportingService/Reports/HblReportTest.java
@@ -142,7 +142,6 @@ class HblReportTest extends CommonMocks {
         shipmentContainers.setContainerCount(1L);
         shipmentContainers.setContainerCode("20GP");
         shipmentContainers.setNetWeight(BigDecimal.TEN);
-        shipmentContainers.setNoOfPackages(10L);
         shipmentContainers.setContainerNumber("CONT000283");
         shipmentContainers.setGrossVolume(BigDecimal.TEN);
         shipmentContainers.setGrossVolumeUnit("M3");
@@ -155,7 +154,6 @@ class HblReportTest extends CommonMocks {
         shipmentContainers.setContainerCount(1L);
         shipmentContainers.setContainerCode("20GP");
         shipmentContainers.setNetWeight(BigDecimal.TEN);
-        shipmentContainers.setNoOfPackages(10L);
         shipmentContainers.setContainerNumber("CONT000284");
         shipmentContainers.setGrossVolume(BigDecimal.TEN);
         shipmentContainers.setGrossVolumeUnit("M3");
