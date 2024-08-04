45298de761d3182befa1626863bf488f6c874a2c
mayankgupta <mayank.gupta@dpworld.com>
2024-08-04 19:57:08 +0530

    interbranch tenantid

--- src/main/java/com/dpw/runner/shipment/services/syncing/Entity/CustomShipmentSyncRequest.java
+++ src/main/java/com/dpw/runner/shipment/services/syncing/Entity/CustomShipmentSyncRequest.java
@@ -437,8 +437,6 @@ public class CustomShipmentSyncRequest implements IRunnerRequest, IRunnerRespons
     private LocalDateTime ShipmentCompletedOn;
     @JsonProperty("SourceTenantId")
     private Integer SourceTenantId;
-    @JsonProperty("TenantId")
-    private Integer tenantId;
     @JsonProperty("TriangulationPartner")
     private Integer TriangulationPartner;
 
