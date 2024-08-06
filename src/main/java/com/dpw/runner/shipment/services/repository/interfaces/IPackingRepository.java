3834f1d57d01f65f64001e630c6184229c0a36d4
Pardeep Malik <Pardeep.Malik@dpworld.com>
2024-08-06 16:31:47 +0530

    Update scenario fixes

--- src/main/java/com/dpw/runner/shipment/services/repository/interfaces/IPackingRepository.java
+++ src/main/java/com/dpw/runner/shipment/services/repository/interfaces/IPackingRepository.java
@@ -3,6 +3,7 @@ package com.dpw.runner.shipment.services.repository.interfaces;
 import com.dpw.runner.shipment.services.aspects.MultitenancyAspect.MultiTenancyRepository;
 import com.dpw.runner.shipment.services.entity.Packing;
 import com.dpw.runner.shipment.services.utils.Generated;
+import com.dpw.runner.shipment.services.utils.InterBranchEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -10,7 +11,7 @@ import org.springframework.data.jpa.domain.Specification;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-@Generated
+@Generated @InterBranchEntity
 public interface IPackingRepository extends MultiTenancyRepository<Packing> {
 
     List<Packing> findByShipmentId(Long shipmentId);
