f4612ef4ce481694338f54d94ea7743dcd7a3328
Himanshu.Chhikara <himanshu.chhikara@dpworld.com>
2024-09-03 15:05:37 +0530

    dg User flow changes

--- src/main/java/com/dpw/runner/shipment/services/service/v1/IV1Service.java
+++ src/main/java/com/dpw/runner/shipment/services/service/v1/IV1Service.java
@@ -177,7 +177,6 @@ public interface IV1Service {
     V1DataResponse listTask(Object request);
     List<UsersRoleListResponse> getUserEmailsByRoleId(V1UsersEmailRequest request);
     List<UsersRoleListResponse> getUserEmailsByRoleId(com.dpw.runner.shipment.services.dto.v1.request.V1UsersEmailRequest request);
-    List<String> getRoleIdsByRoleName(Object request);
-
+    Integer getRoleIdsByRoleName(V1RoleIdRequest roleIdRequest);
     V1DataResponse createTask(Object request);
 }
