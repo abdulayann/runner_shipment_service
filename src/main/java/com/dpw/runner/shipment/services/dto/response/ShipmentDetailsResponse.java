d01c39cb1e081b8ad574495b0a3304f8bbd7b29d
mayankgupta <mayank.gupta@dpworld.com>
2024-09-04 14:17:58 +0530

    ocean dg console attach/detach and listing changes

--- src/main/java/com/dpw/runner/shipment/services/dto/response/ShipmentDetailsResponse.java
+++ src/main/java/com/dpw/runner/shipment/services/dto/response/ShipmentDetailsResponse.java
@@ -210,4 +210,5 @@ public class ShipmentDetailsResponse implements IRunnerResponse {
     private Integer pendingActionCount;
     private FileStatus fileStatus;
     private Boolean isReceivingBranchAdded;
+    private OceanDGStatus oceanDGStatus;
 }
