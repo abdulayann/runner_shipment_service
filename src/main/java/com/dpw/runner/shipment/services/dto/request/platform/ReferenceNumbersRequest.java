8deb22cc9d57a723a66a9981a490318dfd167847
abhishekgoyal <abhishek.goyal@dpworld.com>
2024-09-04 20:12:27 +0530

    Code Smells Fix

--- src/main/java/com/dpw/runner/shipment/services/dto/request/platform/ReferenceNumbersRequest.java
+++ src/main/java/com/dpw/runner/shipment/services/dto/request/platform/ReferenceNumbersRequest.java
@@ -6,10 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
-import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 @Data
 @AllArgsConstructor
--- src/main/java/com/dpw/runner/shipment/services/dto/request/platform/RouteLegRequest.java
+++ src/main/java/com/dpw/runner/shipment/services/dto/request/platform/RouteLegRequest.java
@@ -1,5 +1,6 @@
 package com.dpw.runner.shipment.services.dto.request.platform;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,18 +16,26 @@ import java.time.LocalDateTime;
 public class RouteLegRequest implements Serializable {
     private String order;
     private String origin_code;
-    private String vessel_name;
+    @JsonProperty(value = "vessel_name")
+    private String vesselName;
     private String destination_code;
-    private String voyage_number;
+    @JsonProperty(value = "voyage_number")
+    private String voyageNumber;
     private String transport_mode;
-    private String vessel_imo;
-    private String vessel_mmsi;
-    private String carrier_scac_code;
+    @JsonProperty(value = "vessel_imo")
+    private String vesselImo;
+    @JsonProperty(value = "vessel_mmsi")
+    private String vesselMmsi;
+    @JsonProperty(value = "carrier_scac_code")
+    private String carrierScacCode;
     private LocalDateTime eta;
     private LocalDateTime ets;
     private LocalDateTime ata;
     private LocalDateTime ats;
-    private String airline_iata_code;
-    private String airline_prefix;
-    private String flight_number;
+    @JsonProperty(value = "airline_iata_code")
+    private String airlineIataCode;
+    @JsonProperty(value = "airline_prefix")
+    private String airlinePrefix;
+    @JsonProperty(value = "flight_number")
+    private String flightNumber;
 }
