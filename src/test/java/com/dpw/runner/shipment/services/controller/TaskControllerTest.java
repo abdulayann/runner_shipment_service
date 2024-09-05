4ff191410dbb8665d343f87caa67594d60801c67
Pardeep Malik <Pardeep.Malik@dpworld.com>
2024-09-05 19:06:58 +0530

    Task controller uts

--- src/test/java/com/dpw/runner/shipment/services/controller/TaskControllerTest.java
+++ src/test/java/com/dpw/runner/shipment/services/controller/TaskControllerTest.java
@@ -3,6 +3,7 @@ package com.dpw.runner.shipment.services.controller;
 import com.dpw.runner.shipment.services.entitytransfer.dto.request.SendShipmentRequest;
 import com.dpw.runner.shipment.services.helpers.ResponseHelper;
 import com.dpw.runner.shipment.services.service.interfaces.ITasksService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.parallel.Execution;
@@ -55,6 +56,36 @@ class TaskControllerTest {
         var responseEntity = taskController.create(SendShipmentRequest.builder().build());
         // Assert
         assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    }
+
+    @Test
+    void update() {
+        // Mock
+        when(tasksService.retrieveTask(any())).thenReturn(ResponseHelper.buildSuccessResponse());
+        // Test
+        var responseEntity = taskController.retrieve(1L);
+        // Assert
+        Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
+
+    @Test
+    void update2() {
+        // Mock
+        when(tasksService.retrieveTask(any())).thenThrow(new RuntimeException());
+        // Test
+        var responseEntity = taskController.retrieve(1L);
+        // Assert
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    }
+
+    @Test
+    void update3() {
+        // Mock
+        when(tasksService.retrieveTask(any())).thenThrow(new RuntimeException("RuntimeException"));
+        // Test
+        var responseEntity = taskController.retrieve(1L);
+        // Assert
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
     }
 
 
