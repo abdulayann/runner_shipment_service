aaa7ca658616f499109075b0f8c6f0425a94e0fe
Tanishq <tanishq.malhotra@dpworld.com>
2024-09-12 15:16:41 +0530

    save pack utilisation check for AIR transport mode only

--- src/test/java/com/dpw/runner/shipment/services/service/impl/PackingServiceTest.java
+++ src/test/java/com/dpw/runner/shipment/services/service/impl/PackingServiceTest.java
@@ -1923,6 +1923,45 @@ class PackingServiceTest extends CommonMocks {
         }
     }
 
+    @Test
+    void savePackUtilisationCalculationSkipsInCaseOfNonAirTransportMode() {
+        var spyService = Mockito.spy(packingService);
+        var packingList = jsonTestUtility.getTestPackingList();
+        Long consolidationId = 1L;
+        ConsolidationDetails consolidationDetails = jsonTestUtility.getTestConsolidationAir();
+        consolidationDetails.setPackingList(packingList);
+        consolidationDetails.setTransportMode("SEA");
+        ShipmentRequest shipmentRequest = ShipmentRequest.builder().id(1L).build();
+        shipmentRequest.setPackingList(jsonTestUtility.convertValueToList(packingList, PackingRequest.class));
+        Allocations allocations = jsonTestUtility.getJson("CONSOLIDATION_ALLOCATION", Allocations.class);
+        consolidationDetails.setAllocations(allocations);
+        setFeatureFlagForInterBranch();
+
+        CalculatePackUtilizationRequest request = new CalculatePackUtilizationRequest();
+        request.setSaveConsol(true);
+        request.setShipmentRequest(shipmentRequest);
+        request.setConsolidationId(consolidationId);
+
+        AchievedQuantities achievedQuantities = new AchievedQuantities();
+        BigDecimal consolidatedWeight = BigDecimal.valueOf(packingList.stream().mapToDouble(i -> i.getWeight().doubleValue()).sum());
+        BigDecimal consolidatedVolume = BigDecimal.valueOf(packingList.stream().mapToDouble(i -> i.getVolume().doubleValue()).sum());
+        achievedQuantities.setConsolidatedWeight(consolidatedWeight);
+        achievedQuantities.setConsolidatedVolume(consolidatedVolume);
+        achievedQuantities.setWeightUtilization(String.valueOf(consolidatedWeight.divide(allocations.getWeight())));
+        achievedQuantities.setVolumeUtilization(String.valueOf(consolidatedVolume.divide(allocations.getVolume())));
+
+        // Mocking
+        when(consolidationDao.findById(consolidationId)).thenThrow(new RuntimeException());
+
+        try{
+            spyService.savePackUtilisationCalculationInConsole(request);
+            verify(consolidationDao, times(0)).save(any(), anyBoolean());
+        }
+        catch (Exception e) {
+            fail(e);
+        }
+    }
+
 
 
     void setFeatureFlagForInterBranch() {
