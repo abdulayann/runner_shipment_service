56ab26b4a89fa691a1ab88feff8096f413fdccb9
Himanshu.Chhikara <himanshu.chhikara@dpworld.com>
2024-09-13 13:56:55 +0530

    handling rejection as required for sending approval

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ShipmentService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ShipmentService.java
@@ -6157,7 +6157,7 @@ public class ShipmentService implements IShipmentService {
     }
 
     private DBOperationType determineOperationTypeAfterApproval(OceanDGStatus dgStatus, OceanDGRequest request){
-        DBOperationType operationType = null;
+        DBOperationType operationType = DG_REQUEST;
         if(dgStatus == OCEAN_DG_REQUESTED){
             if(request.getStatus() == TaskStatus.APPROVED){
                 operationType = DG_APPROVE;
@@ -6175,10 +6175,10 @@ public class ShipmentService implements IShipmentService {
     }
 
     private OceanDGStatus determineDgStatusAfterApproval(OceanDGStatus dgStatus, boolean isOceanDgUser, ShipmentDetails shipmentDetails) {
-        if (dgStatus == OCEAN_DG_APPROVAL_REQUIRED && isOceanDgUser) {
+        if ((dgStatus == OCEAN_DG_APPROVAL_REQUIRED || dgStatus == OCEAN_DG_REJECTED) && isOceanDgUser) {
             return checkForClass1(shipmentDetails) ? OCEAN_DG_COMMERCIAL_APPROVAL_REQUIRED : OCEAN_DG_ACCEPTED;
         } else {
-            return (dgStatus == OCEAN_DG_COMMERCIAL_APPROVAL_REQUIRED) ? OCEAN_DG_COMMERCIAL_REQUESTED : OCEAN_DG_REQUESTED;
+            return ((dgStatus == OCEAN_DG_COMMERCIAL_APPROVAL_REQUIRED || dgStatus == OCEAN_DG_COMMERCIAL_REJECTED)) ? OCEAN_DG_COMMERCIAL_REQUESTED : OCEAN_DG_REQUESTED;
         }
     }
 
