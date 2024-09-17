1ce353ae6411071191047590f95e93bb1c60bf69
Pardeep Malik <Pardeep.Malik@dpworld.com>
2024-09-17 17:34:43 +0530

    Email Bug fix

--- src/main/java/com/dpw/runner/shipment/services/entitytransfer/service/impl/EntityTransferService.java
+++ src/main/java/com/dpw/runner/shipment/services/entitytransfer/service/impl/EntityTransferService.java
@@ -507,7 +507,8 @@ public class EntityTransferService implements IEntityTransferService {
         // Send consolidated shipments email
         if(!shipmentGuids.isEmpty()) {
             try {
-                CompletableFuture.runAsync(masterDataUtils.withMdc(() -> sendGroupedEmailForShipmentImport(shipmentGuids, entityTransferConsolidationDetails.getSourceBranchTenantName())));
+                var shipmentDetailsList = shipmentDao.findShipmentsByGuids(new HashSet<>(shipmentGuids));
+                CompletableFuture.runAsync(masterDataUtils.withMdc(() -> sendGroupedEmailForShipmentImport(shipmentDetailsList, entityTransferConsolidationDetails.getSourceBranchTenantName())));
             } catch (Exception ex) {
                 log.error(String.format(ErrorConstants.ERROR_WHILE_EMAIL, ex.getMessage()));
             }
@@ -1587,12 +1588,11 @@ public class EntityTransferService implements IEntityTransferService {
     }
 
 
-    public void sendGroupedEmailForShipmentImport(List<UUID> shipmentGuids, String consoleSourceBranchTenantName) {
+    public void sendGroupedEmailForShipmentImport(List<ShipmentDetails> shipmentDetailsList, String consoleSourceBranchTenantName) {
         commonUtils.setInterBranchContextForHub();
         Set<Integer> tenantIds = new HashSet<>();
         Set<Integer> sourceTenantIds = new HashSet<>();
         Map<Integer, List<ShipmentDetails>> tenantShipmentMapping = new HashMap<>();
-        var shipmentDetailsList = shipmentDao.findShipmentsByGuids(new HashSet<>(shipmentGuids));
 
         for(ShipmentDetails shipmentDetails: shipmentDetailsList) {
             tenantShipmentMapping.computeIfAbsent(shipmentDetails.getTenantId(), shipmentDetail -> new ArrayList<>()).add(shipmentDetails);
--- src/test/java/com/dpw/runner/shipment/services/entitytransfer/service/impl/EntityTransferServiceTest.java
+++ src/test/java/com/dpw/runner/shipment/services/entitytransfer/service/impl/EntityTransferServiceTest.java
@@ -1452,7 +1452,6 @@ class EntityTransferServiceTest {
     @Test
     void testSendGroupedEmailForShipmentImport_Success() {
         // Given
-        List<UUID> shipmentGuids = List.of(UUID.randomUUID(), UUID.randomUUID());
         String consoleSourceBranchTenantName = "consoleBranch";
         when(shipmentDetails.getTenantId()).thenReturn(100);
         when(shipmentDetails.getSourceTenantId()).thenReturn(123L);
@@ -1467,8 +1466,6 @@ class EntityTransferServiceTest {
         );
 
         // Mocks
-        when(shipmentDao.findShipmentsByGuids(anySet()))
-                .thenReturn(shipmentDetailsList);
         doNothing().when(commonUtils).setInterBranchContextForHub();
         doNothing().when(commonUtils).getToAndCCEmailIdsFromTenantSettings(anySet(), anyMap());
         EmailTemplatesRequest emailTemplatesRequest = mock(EmailTemplatesRequest.class);
@@ -1488,7 +1485,7 @@ class EntityTransferServiceTest {
         when(v1TenantResponse.getTenantName()).thenReturn("abcd");
 
         // When
-        entityTransferService.sendGroupedEmailForShipmentImport(shipmentGuids, consoleSourceBranchTenantName);
+        entityTransferService.sendGroupedEmailForShipmentImport(shipmentDetailsList, consoleSourceBranchTenantName);
 
         // Then
         verify(commonUtils, times(1)).setInterBranchContextForHub();
