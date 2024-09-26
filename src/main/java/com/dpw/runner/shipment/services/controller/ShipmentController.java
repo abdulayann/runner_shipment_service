eb8ed832fd9b0b44fca06f5f32158855694a8a71
Himanshu.Chhikara <himanshu.chhikara@dpworld.com>
2024-09-26 17:18:59 +0530

    return type corrected
    Signed-off-by: Himanshu.Chhikara <himanshu.chhikara@dpworld.com>

--- src/main/java/com/dpw/runner/shipment/services/controller/ShipmentController.java
+++ src/main/java/com/dpw/runner/shipment/services/controller/ShipmentController.java
@@ -39,11 +39,9 @@ import com.dpw.runner.shipment.services.dto.response.HblCheckResponse;
 import com.dpw.runner.shipment.services.dto.response.UpstreamDateUpdateResponse;
 import com.dpw.runner.shipment.services.dto.response.billing.InvoicePostingValidationResponse;
 import com.dpw.runner.shipment.services.dto.response.notification.PendingNotificationResponse;
-import com.dpw.runner.shipment.services.dto.v1.request.AddressTranslationRequest.OrgAddressCode;
 import com.dpw.runner.shipment.services.dto.v1.request.PartiesOrgAddressRequest;
 import com.dpw.runner.shipment.services.dto.v1.request.TIContainerListRequest;
 import com.dpw.runner.shipment.services.dto.v1.request.TIListRequest;
-import com.dpw.runner.shipment.services.dto.v1.response.OrgAddressResponse;
 import com.dpw.runner.shipment.services.entity.ShipmentDetails;
 import com.dpw.runner.shipment.services.exception.exceptions.RunnerException;
 import com.dpw.runner.shipment.services.helpers.JsonHelper;
@@ -247,7 +245,7 @@ public class ShipmentController {
     }
 
     @ApiResponses(value = {
-        @ApiResponse(code = 200, message = ShipmentConstants.FETCH_ORG_INFO, response = OrgAddressResponse.class),
+        @ApiResponse(code = 200, message = ShipmentConstants.FETCH_ORG_INFO, response = PartiesRequest.class),
         @ApiResponse(code = 500, message = DaoConstants.DAO_GENERIC_RETRIEVE_EXCEPTION_MSG)
     })
     @GetMapping(ShipmentConstants.FETCH_ORG_INFO)
