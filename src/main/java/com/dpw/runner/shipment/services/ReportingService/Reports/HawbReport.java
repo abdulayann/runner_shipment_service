6c046cb021a7ba0710a86e16788bf439721936d8
Mylavarapu Vamsi <mylavarapu.vamsi@CTIPL-MAC-584.local>
2024-10-04 12:58:49 +0530

    Refractored code to remove code smells

--- src/main/java/com/dpw/runner/shipment/services/ReportingService/Reports/HawbReport.java
+++ src/main/java/com/dpw/runner/shipment/services/ReportingService/Reports/HawbReport.java
@@ -379,12 +379,10 @@ public class HawbReport extends IReport{
                     }
                     if(value.get(PIECES_NO) != null)
                         value.put(PIECES_NO, GetDPWWeightVolumeFormat(new BigDecimal(value.get(PIECES_NO).toString()), 0, v1TenantSettingsResponse));
-                    if(value.get(ReportConstants.HS_CODE_KEY) != null){
-                        String hsCode = value.get(ReportConstants.HS_CODE_KEY).toString();
-                        if (hsCode != null && !hsCode.isEmpty()) {
-                            hsCodeList.add(hsCode);
-                            dictionary.put(ReportConstants.HAS_HS_CODE,true);
-                        }
+                    String hsCode = (String) value.get(ReportConstants.HS_CODE_KEY);
+                    if (hsCode != null && !hsCode.isEmpty()) {
+                        hsCodeList.add(hsCode);
+                        dictionary.put(ReportConstants.HAS_HS_CODE, true);
                     }
                 });
                 dictionary.put(ReportConstants.HS_CODE, String.join(",", hsCodeList));
