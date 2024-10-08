c0e2ca88bd368f434ef10a18a6c53a1cd84f3ed2
Anirudh.Kanojia@dpworld.com <Anirudh.Kanojia@dpworld.com>
2024-10-08 21:54:27 +0530

    task 182112: mawb,bol and other data update

---
+++ DBPatch/Release-v1.19.2/update_mawb_hbl_number.sql
@@ -0,0 +1,5 @@
+update mawb_stocks_link set entity_id = 10661, entity_type = 'CONSOLIDATION' where id = 1410;
+
+update mawb_stocks_link set entity_id = 10070, entity_type = 'CONSOLIDATION' where id = 1395;
+
+update consolidation_details set mawb = '055-23099893', bol = '055-23099893' where id = 106007;
