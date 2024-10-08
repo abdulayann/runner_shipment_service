5365a1312cffdfca2c0b7a36f9202fb1f2623bd3
Anirudh.Kanojia@dpworld.com <Anirudh.Kanojia@dpworld.com>
2024-10-08 23:44:23 +0530

    comment resolved

--- DBPatch/Release-v1.19.2/update_mawb_hbl_number.sql
+++ DBPatch/Release-v1.19.2/update_mawb_hbl_number.sql
@@ -2,4 +2,4 @@ update mawb_stocks_link set entity_id = 10661, entity_type = 'CONSOLIDATION' whe
 
 update mawb_stocks_link set entity_id = 10070, entity_type = 'CONSOLIDATION' where id = 1395;
 
-update consolidation_details set mawb = '055-23099893', bol = '055-23099893' where id = 106007;
+update consolidation_details set mawb = '055-23099893', bol = '055-23099893' where id = 10070;
