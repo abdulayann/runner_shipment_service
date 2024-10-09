fb7b5e95c4a3a023a72b379da55d430fed5f5b2a
Anirudh.Kanojia@dpworld.com <Anirudh.Kanojia@dpworld.com>
2024-10-09 16:39:08 +0530

    task 182590: update mawb number of consol

---
+++ DBPatch/Release-v1.19.2/update_mawb_consolidation.sql
@@ -0,0 +1,3 @@
+update mawb_stocks_link set entity_id = 10592, entity_type = 'CONSOLIDATION' where id = 1468;
+
+update consolidation_details set mawb = '074-65710735', bol = '074-65710735' where id = 10592;
