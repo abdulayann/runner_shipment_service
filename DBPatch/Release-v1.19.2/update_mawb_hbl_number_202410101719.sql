30e6bf3fb1cb885111637c3b0c28147482aa7728
mayankgupta <mayank.gupta@dpworld.com>
2024-10-15 12:38:01 +0530

    mawb stock update

--- DBPatch/Release-v1.19.2/update_mawb_hbl_number_202410101719.sql
+++ DBPatch/Release-v1.19.2/update_mawb_hbl_number_202410101719.sql
@@ -1,1 +1,2 @@
 update consolidation_details set mawb = '618-35331715', bol = '618-35331715' where id = 10890;
+update mawb_stocks_link set entity_id = 10890, entity_type = 'CONSOLIDATION' where id = 1484;
