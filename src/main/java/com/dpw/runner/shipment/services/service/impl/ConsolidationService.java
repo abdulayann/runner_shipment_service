3b48fc64e0a4d247c29b3008801791f09fcc5c17
Aditya.Thakur <aditya.thakur@dpworld.com>
2024-10-15 18:54:33 +0530

    email changes for shipment pull

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ConsolidationService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ConsolidationService.java
@@ -934,62 +934,26 @@ public class ConsolidationService implements IConsolidationService {
         var emailTemplatesRequests = getEmailTemplates(IMPORT_SHIPMENT_ATTACHMENT_EMAIL);
         var emailTemplateModel = emailTemplatesRequests.stream().findFirst().orElse(new EmailTemplatesRequest());
 
-        // No CC emails are used
-        List<String> ccEmails = new ArrayList<>();
-//        // Fetching role ids corresponding to console destination branches
-//        var shipDestinationBranchIds = new ArrayList<>(destinationBranches);
-//        if(shipmentGuidSendToBranch == null)
-//            shipmentGuidSendToBranch = new HashMap<>();
-//        for (var keySet : shipmentGuidSendToBranch.entrySet())
-//            shipDestinationBranchIds.addAll(keySet.getValue());
-//        shipDestinationBranchIds.add(TenantContext.getCurrentTenant());
-//        var branchIdVsTenantModelMap = convertToTenantModel(v1ServiceUtil.getTenantDetails(shipDestinationBranchIds));
+        List<String> toEmailList = new ArrayList<>();
+        toEmailList.add(shipmentDetails.getCreatedBy() != null ? shipmentDetails.getCreatedBy() : "");
+        toEmailList.add(shipmentDetails.getAssignedTo() != null ? shipmentDetails.getAssignedTo() : "");
 
-        for (int i = 0; i < destinationBranches.size(); i++) {
-            List<String> toEmailList = new ArrayList<>();
-            toEmailList.add(shipmentDetails.getCreatedBy() != null ? shipmentDetails.getCreatedBy() : "");
-            toEmailList.add(shipmentDetails.getAssignedTo() != null ? shipmentDetails.getAssignedTo() : "");
+        Set<String> toEmailIds = new HashSet<>();
+        Set<String> ccEmailIds = new HashSet<>();
+        Map<Integer, V1TenantSettingsResponse> v1TenantSettingsMap = new HashMap<>();
+        Set<Integer> tenantIds = new HashSet<>();
+        tenantIds.add(consolidationDetails.getTenantId());
+        commonUtils.getToAndCCEmailIdsFromTenantSettings(tenantIds, v1TenantSettingsMap);
 
-            Set<String> toEmailIds = new HashSet<>();
-            Set<String> ccEmailIds = new HashSet<>();
-            Map<Integer, V1TenantSettingsResponse> v1TenantSettingsMap = new HashMap<>();
-            Set<Integer> tenantIds = new HashSet<>();
-            tenantIds.add(shipmentDetails.getTenantId());
-            commonUtils.getToAndCCEmailIdsFromTenantSettings(tenantIds, v1TenantSettingsMap);
-
-            if(toEmailList.isEmpty()) {
-               commonUtils.getToAndCcEmailMasterLists(toEmailIds, ccEmailIds, v1TenantSettingsMap, shipmentDetails.getTenantId(), true);
-
-            }
-
-
-            populateShipmentImportAttachmentTemplate(shipmentDetails, emailTemplateModel);
-            sendEmailNotification(emailTemplateModel, toEmailList, ccEmails);
+        if(toEmailList.isEmpty()) {
+           commonUtils.getToAndCcEmailMasterLists(toEmailIds, ccEmailIds, v1TenantSettingsMap, consolidationDetails.getTenantId(), true);
+           toEmailIds.addAll(new ArrayList<>(toEmailIds));
         }
-    }
 
-    public void populateShipmentImportAttachmentTemplate(ShipmentDetails shipmentDetails, EmailTemplatesRequest template) {
-        UsersDto user = UserContext.getUser();
-
-        var branchIdVsTenantModelMap = convertToTenantModel(v1ServiceUtil.getTenantDetails(List.of(TenantContext.getCurrentTenant())));
-        // Subject
-        String subject = (template.getSubject() == null) ?
-                Constants.DEFAULT_SHIPMENT_RECEIVED_SUBJECT : template.getSubject();
-        subject = subject.replace(SOURCE_BRANCH_PLACEHOLDER, StringUtility.convertToString(branchIdVsTenantModelMap.get(TenantContext.getCurrentTenant()).tenantName));
-        subject = subject.replace(SHIPMENT_NUMBER_PLACEHOLDER, String.valueOf(shipmentDetails.getShipmentId()));
-
-        // Body
-        String body = (template.getBody() == null) ?
-                Constants.DEFAULT_SHIPMENT_RECEIVED_BODY : template.getBody();
-        body = body.replace(SOURCE_BRANCH_PLACEHOLDER, StringUtility.convertToString(branchIdVsTenantModelMap.get(TenantContext.getCurrentTenant()).tenantName));
-        body = body.replace(SENDER_USER_NAME_PLACEHOLDER, StringUtility.convertToString(user.getDisplayName()));
-        body = body.replace(BL_NUMBER_PLACEHOLDER, StringUtility.convertToString(shipmentDetails.getHouseBill()));
-        body = body.replace(MBL_NUMBER_PLACEHOLDER, StringUtility.convertToString(shipmentDetails.getMasterBill()));
-        body = body.replace(SENT_DATE_PLACEHOLDER, LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-        body = body.replace(SHIPMENT_NUMBER_PLACEHOLDER, String.valueOf(shipmentDetails.getShipmentId()));
-
-        template.setSubject(subject);
-        template.setBody(body);
+        Map<String, Object> dictionary = new HashMap<>();
+        commonUtils.populateShipmentImportAttachmentTemplate(dictionary, shipmentDetails, consolidationDetails, emailTemplateModel);
+        sendEmailNotification(emailTemplateModel, toEmailList, new ArrayList<>(ccEmailIds));
+        }
     }
 
     private void sendEmailNotification(EmailTemplatesRequest emailTemplateModel, List<String> to, List<String> cc) {
