cc9b6bdbbad6e43b27b8f0f3d6efe016fea9fde7
Subham Mallick <subham.mallick@dpworld.com>
2024-11-14 22:10:07 +0530

    188176

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ShipmentService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ShipmentService.java
@@ -2674,14 +2674,7 @@ public class ShipmentService implements IShipmentService {
             createTrackingEvents(newUpdatedEvents, shipmentDetails);
         }
 
-        // update events with consolidation id with condition
-        List<ConsolidationDetails> consolidationList = shipmentDetails.getConsolidationList();
-        if(ObjectUtils.isNotEmpty(consolidationList)) {
-            Long consolidationId = consolidationList.get(0).getId();
-            newUpdatedEvents.stream()
-                    .filter(event -> eventDao.shouldSendEventFromShipmentToConsolidation(event, shipmentDetails.getTransportMode()))
-                    .forEach(event -> event.setConsolidationId(consolidationId));
-        }
+        newUpdatedEvents.forEach(event -> eventDao.updateEventDetails(event));
 
         return newUpdatedEvents;
     }
