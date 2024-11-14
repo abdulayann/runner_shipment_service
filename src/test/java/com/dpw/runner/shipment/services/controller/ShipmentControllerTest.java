96a37fccc2c830cc7ba1235aaa2aaf8051665684
Aditya.Thakur <aditya.thakur@dpworld.com>
2024-11-14 15:14:11 +0530

    Refactored code

--- src/test/java/com/dpw/runner/shipment/services/controller/ShipmentControllerTest.java
+++ src/test/java/com/dpw/runner/shipment/services/controller/ShipmentControllerTest.java
@@ -1364,7 +1364,7 @@ class ShipmentControllerTest {
     }
 
     @Test
-    void testListExternal1() throws InterruptedException {
+    void testListExternal1() throws RunnerException, InterruptedException {
         // Mock
         when(jsonHelper.convertToJson(any())).thenReturn(StringUtility.getRandomString(10));
         when(shipmentService.fullShipmentsExternalList(any())).thenThrow(new RuntimeException());
