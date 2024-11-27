9a475fc5c11f929b8deac289b022dc5f971f43ea
Subham Mallick <subham.mallick@dpworld.com>
2024-11-27 15:57:38 +0530

    184258 | Merge 1.25

--- src/test/java/com/dpw/runner/shipment/services/dao/impl/ShipmentDaoTest.java
+++ src/test/java/com/dpw/runner/shipment/services/dao/impl/ShipmentDaoTest.java
@@ -10,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
