7ec9760dd5d4b26ef87a06ff0dc472f07777a1f6
Anupam Gupta <anupam.rtp@gmail.com>
2024-12-05 19:22:41 +0530

    Added NotificationRequestType values in the Enum List API

--- src/main/java/com/dpw/runner/shipment/services/commons/constants/Constants.java
+++ src/main/java/com/dpw/runner/shipment/services/commons/constants/Constants.java
@@ -589,6 +589,7 @@ public class Constants {
     public static final String NETWORK_TRANSFER_ENTITY_TYPES = "NetworkTransferEntityTypes";
     public static final String UAE_TWO_DIGIT_IATA_CODE = "AE";
     public static final String NOTIFICATION_ENTITY = "Notification";
+    public static final String NOTIFICATION_REQUEST_TYPES = "NotificationRequestTypes";
     private Constants() {
     }
 
