d65fd5d88ba678f6f2b1b7f80e315b79bfec941f
Pardeep Malik <Pardeep.Malik@dpworld.com>
2024-12-05 13:06:09 +0530

    Source guid added in map

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ShipmentService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ShipmentService.java
@@ -684,7 +684,8 @@ public class ShipmentService implements IShipmentService {
             Map.entry(Constants.TENANT_ID, RunnerEntityMapping.builder().tableName(Constants.SHIPMENT_DETAILS).dataType(Integer.class).fieldName(Constants.TENANT_ID).build()),
             Map.entry("cargoReadyDate", RunnerEntityMapping.builder().tableName(Constants.SHIPMENT_DETAILS).dataType(LocalDateTime.class).fieldName("cargoReadyDate").build()),
             Map.entry("cargoDeliveryDate", RunnerEntityMapping.builder().tableName(Constants.SHIPMENT_DETAILS).dataType(LocalDateTime.class).fieldName("cargoDeliveryDate").build()),
-            Map.entry("requestedOn", RunnerEntityMapping.builder().tableName("consoleShipmentMappings").dataType(LocalDateTime.class).fieldName(CREATED_AT).build())
+            Map.entry("requestedOn", RunnerEntityMapping.builder().tableName("consoleShipmentMappings").dataType(LocalDateTime.class).fieldName(CREATED_AT).build()),
+            Map.entry("sourceGuid", RunnerEntityMapping.builder().tableName(Constants.SHIPMENT_DETAILS).dataType(UUID.class).fieldName("sourceGuid").build())
     );
 
     @Override
