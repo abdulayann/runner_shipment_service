273ac047a587e65c6ad99a4a4bdbd9c6aba7dffc
Wasim Jafar <wasim.jafar@dpworld.com>
2024-12-10 13:04:50 +0530

    As Agreed on the AWB - for SumOfTotalAmount

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ReportService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ReportService.java
@@ -105,6 +105,7 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.protocol.types.Field;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -470,7 +471,7 @@ public class ReportService implements IReportService {
             if (reportRequest.getDisplayFreightAmount() != null && !reportRequest.getDisplayFreightAmount())
             {
                 dataRetrived.put(ReportConstants.PACKING_LIST, dataRetrived.get(ReportConstants.PACKING_LIST_FAT));
-                dataRetrived.put(ReportConstants.SUM_OF_TOTAL_AMOUNT, dataRetrived.get(ReportConstants.FREIGHT_AMOUNT_TEXT));
+                dataRetrived.put(ReportConstants.SUM_OF_TOTAL_AMOUNT, StringUtility.getEmptyString());
                 dataRetrived.put(ReportConstants.WT_CHARGE_P, dataRetrived.get(ReportConstants.FREIGHT_AMOUNT_TEXT_P));
                 dataRetrived.put(ReportConstants.WT_CHARGE_C, dataRetrived.get(ReportConstants.FREIGHT_AMOUNT_TEXT_C));
                 dataRetrived.put(ReportConstants.TOTAL_PREPAID, dataRetrived.get(ReportConstants.TOTAL_OTHERS_P));
