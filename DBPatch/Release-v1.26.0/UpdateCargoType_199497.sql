d45cc83bd05b33719f90b258fba3f946b9f7b46a
Deepshikha.Tripathi <deepshikha.tripathi@dpworld.com>
2025-01-28 09:51:37 +0530

    Updated Cargo Type to LCL

--- DBPatch/Release-v1.26.0/UpdateCargoType_199497.sql
+++ DBPatch/Release-v1.26.0/UpdateCargoType_199497.sql
@@ -1,2 +1,2 @@
-Update customer_booking set cargo_type = 'LCL' where id in ('23041', '23047');
-Update shipment_details set shipment_type = 'LCL' where id in ('31082', '31074');
+Update customer_booking set cargo_type = 'LCL', updated_at = NOW() where id in ('23041', '23047');
+Update shipment_details set shipment_type = 'LCL', updated_at = NOW() where id in ('31082', '31074');
