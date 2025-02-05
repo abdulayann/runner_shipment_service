23a39fbdf457ab49c9b4cebe7cc5a02cfee5bd9b
Subham Mallick <subham.mallick@dpworld.com>
2025-02-05 14:05:38 +0530

    188243

--- src/main/resources/db/migration/V1.233__New_Shipment_details_fields.sql
+++ src/main/resources/db/migration/V1.233__New_Shipment_details_fields.sql
@@ -1,5 +1,5 @@
 ALTER TABLE IF EXISTS shipment_details
-    ALTER COLUMN is_co_load_enabled SET DEFAULT FALSE,
+    ADD COLUMN IF NOT EXISTS is_co_load_enabled BOOLEAN DEFAULT FALSE,
     ADD COLUMN IF NOT EXISTS co_load_carrier_name VARCHAR,
     ADD COLUMN IF NOT EXISTS co_load_bl_number VARCHAR,
     ADD COLUMN IF NOT EXISTS issuing_carrier_name VARCHAR,
