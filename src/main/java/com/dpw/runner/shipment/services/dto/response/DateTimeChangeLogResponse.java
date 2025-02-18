d01d4873efde5eb2abc151678352fe924948dd20
Pardeep Malik <Pardeep.Malik@dpworld.com>
2025-02-18 13:17:44 +0530

    time zone issue fix

--- src/main/java/com/dpw/runner/shipment/services/dto/response/DateTimeChangeLogResponse.java
+++ src/main/java/com/dpw/runner/shipment/services/dto/response/DateTimeChangeLogResponse.java
@@ -2,12 +2,14 @@ package com.dpw.runner.shipment.services.dto.response;
 
 import com.dpw.runner.shipment.services.commons.responses.IRunnerResponse;
 import com.dpw.runner.shipment.services.entity.enums.DateType;
+import com.dpw.runner.shipment.services.utils.ExcludeTimeZone;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class DateTimeChangeLogResponse implements IRunnerResponse {
+    @ExcludeTimeZone
     private LocalDateTime currentValue;
     private DateType dateType;
     private Long shipmentId;
