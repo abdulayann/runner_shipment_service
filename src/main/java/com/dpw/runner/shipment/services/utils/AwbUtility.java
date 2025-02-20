cdc84d1687830f26ccc4186f0dfa458fe8ad6e21
Wasim Jafar <wasim.jafar@dpworld.com>
2025-02-20 11:28:25 +0530

    Air Messaging Events, Overarching Console Usecase

--- src/main/java/com/dpw/runner/shipment/services/utils/AwbUtility.java
+++ src/main/java/com/dpw/runner/shipment/services/utils/AwbUtility.java
@@ -950,10 +950,11 @@ public class AwbUtility {
         List<ConsoleShipmentMapping> consoleShipmentMappings = null;
         if(awb.get().getConsolidationId() != null) {
             consoleShipmentMappings = consoleShipmentMappingDao.findByConsolidationIdByQuery(awb.get().getConsolidationId());
-
             eventDao.createEventForAirMessagingEvent(prepareEventPayload(airMessageEvent, awb.get().getConsolidationId(), Constants.CONSOLIDATION, tenantId));
             if(consoleShipmentMappings != null && !consoleShipmentMappings.isEmpty()){
-                consoleShipmentMappings.forEach(x -> eventDao.createEventForAirMessagingEvent(prepareEventPayload(airMessageEvent, x.getShipmentId(), Constants.SHIPMENT, tenantId)));
+                var shipmentList = shipmentDao.findShipmentsByIds(consoleShipmentMappings.stream().map(ConsoleShipmentMapping::getShipmentId).collect(Collectors.toSet()));
+                shipmentList.forEach(shipment -> eventDao.createEventForAirMessagingEvent(prepareEventPayload(airMessageEvent, shipment.getId(), Constants.SHIPMENT, shipment.getTenantId())));
+//                consoleShipmentMappings.forEach(x -> eventDao.createEventForAirMessagingEvent(prepareEventPayload(airMessageEvent, x.getShipmentId(), Constants.SHIPMENT, tenantId)));
             }
         } else if (awb.get().getShipmentId() != null) {
             eventDao.createEventForAirMessagingEvent(prepareEventPayload(airMessageEvent, awb.get().getShipmentId(), Constants.SHIPMENT, tenantId));
