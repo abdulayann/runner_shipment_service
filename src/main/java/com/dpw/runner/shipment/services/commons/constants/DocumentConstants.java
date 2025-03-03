ef758c80d27ae94d32205d319e1c2f80afa6bb78
Wasim Jafar <wasim.jafar@dpworld.com>
2025-03-03 12:40:46 +0530

    Document History & Download API

--- src/main/java/com/dpw/runner/shipment/services/commons/constants/DocumentConstants.java
+++ src/main/java/com/dpw/runner/shipment/services/commons/constants/DocumentConstants.java
@@ -8,8 +8,12 @@ public class DocumentConstants {
 
     private DocumentConstants(){}
     public static final String DOCUMENT_API_HANDLE = "/document/manager";
-    public static final String DELETE_SUCCESSFUL = "Successful Shipment Delete";
+    public static final String DELETE_SUCCESSFUL = "Successful Document Delete";
+    public static final String FETCH_SUCCESSFUL = "Successful Document Fetched";
     public static final String TEMPORARY_UPLOAD = "/temp/upload";
     public static final String SAVE_FILE = "/save/file";
 
+    public static final String FILE_HISTORY = "/history";
+    public static final String FILE_DOWNLOAD = "/download";
+    public static final String DOCUMENT_ID = "Document Id";
 }
