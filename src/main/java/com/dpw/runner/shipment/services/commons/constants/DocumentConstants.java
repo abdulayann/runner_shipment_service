e3f6017dde015d08e15d5f32e66e8d44ead40aab
Wasim Jafar <wasim.jafar@dpworld.com>
2025-03-04 05:28:28 +0530

    Document Bulk Save

--- src/main/java/com/dpw/runner/shipment/services/commons/constants/DocumentConstants.java
+++ src/main/java/com/dpw/runner/shipment/services/commons/constants/DocumentConstants.java
@@ -10,8 +10,11 @@ public class DocumentConstants {
     public static final String DOCUMENT_API_HANDLE = "/document/manager";
     public static final String DELETE_SUCCESSFUL = "Successful Document Delete";
     public static final String FETCH_SUCCESSFUL = "Successful Document Fetched";
+    public static final String ADDED_SUCCESSFUL = "Successful Document ADDED";
+    // API handler
     public static final String TEMPORARY_UPLOAD = "/temp/upload";
     public static final String SAVE_FILE = "/save/file";
+    public static final String BULK_SAVE_FILES = "/bulk-save";
 
     public static final String FILE_HISTORY = "/history";
     public static final String FILE_DOWNLOAD = "/download";
