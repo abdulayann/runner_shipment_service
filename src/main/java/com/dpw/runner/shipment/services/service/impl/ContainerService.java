de77741c8963c2472027e27653b4bf5fd75d082a
lalchand.mali <lalchand@Administrators-MacBook-Pro.local>
2025-03-04 13:17:33 +0530

    perf changes

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ContainerService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ContainerService.java
@@ -1308,7 +1308,7 @@ public class ContainerService implements IContainerService {
             if(response.getSummary() == null)
                 response.setSummary("");
             try {
-                response.setSummary(calculateContainerSummary(jsonHelper.convertValueToList(containersList, Containers.class)));
+                response.setSummary(calculateContainerSummary(containersList));
             }
             catch (Exception e) {
                 log.error("Error calculating summary");
