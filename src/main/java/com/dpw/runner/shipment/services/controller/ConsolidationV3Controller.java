19433e9571edb577fcd29d636bd7f4670419c2a0
mayankgupta <mayank.gupta@dpworld.com>
2025-03-24 19:49:37 +0530

    console create update adde

--- src/main/java/com/dpw/runner/shipment/services/controller/ConsolidationV3Controller.java
+++ src/main/java/com/dpw/runner/shipment/services/controller/ConsolidationV3Controller.java
@@ -2,15 +2,19 @@ package com.dpw.runner.shipment.services.controller;
 
 import com.dpw.runner.shipment.services.commons.constants.ApiConstants;
 import com.dpw.runner.shipment.services.commons.constants.ConsolidationConstants;
+import com.dpw.runner.shipment.services.commons.constants.Constants;
 import com.dpw.runner.shipment.services.commons.responses.IRunnerResponse;
 import com.dpw.runner.shipment.services.commons.responses.RunnerResponse;
+import com.dpw.runner.shipment.services.dto.request.ConsolidationDetailsRequest;
 import com.dpw.runner.shipment.services.dto.request.ShipmentAttachDetachV3Request;
+import com.dpw.runner.shipment.services.dto.response.ConsolidationDetailsResponse;
 import com.dpw.runner.shipment.services.exception.exceptions.RunnerException;
+import com.dpw.runner.shipment.services.helpers.JsonHelper;
+import com.dpw.runner.shipment.services.helpers.LoggerHelper;
 import com.dpw.runner.shipment.services.helpers.ResponseHelper;
 import com.dpw.runner.shipment.services.service.interfaces.IConsolidationV3Service;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,16 +23,33 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping(ConsolidationConstants.CONSOLIDATION_V3_API_HANDLE)
 @Slf4j
 public class ConsolidationV3Controller {
 
     private final IConsolidationV3Service consolidationV3Service;
+    private final JsonHelper jsonHelper;
+
+    private static class MyResponseClass extends RunnerResponse<ConsolidationDetailsResponse> {}
 
     @Autowired
-    public ConsolidationV3Controller(IConsolidationV3Service consolidationV3Service) {
+    public ConsolidationV3Controller(IConsolidationV3Service consolidationV3Service,
+                                     JsonHelper jsonHelper) {
         this.consolidationV3Service = consolidationV3Service;
+        this.jsonHelper = jsonHelper;
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = ConsolidationConstants.CREATE_SUCCESSFUL, response = ConsolidationV3Controller.MyResponseClass.class),
+            @ApiResponse(code = 404, message = Constants.NO_DATA, response = RunnerResponse.class)
+    })
+    @PostMapping(ApiConstants.API_CREATE)
+    public ResponseEntity<IRunnerResponse> create(@RequestBody @Valid ConsolidationDetailsRequest request) {
+        log.info("Received Consolidation create request with RequestId: {} and payload: {}", LoggerHelper.getRequestIdFromMDC(), jsonHelper.convertToJson(request));
+        return ResponseHelper.buildSuccessResponse(consolidationV3Service.create(request));
     }
 
     /**
