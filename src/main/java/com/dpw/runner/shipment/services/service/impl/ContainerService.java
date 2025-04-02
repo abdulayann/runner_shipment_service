8f6be05d56a058eebb348390b10b2385ba4ba652
lalchand.mali <lalchand@Administrators-MacBook-Pro.local>
2025-04-02 15:15:19 +0530

    added code for containers/log history entity transfer

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ContainerService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ContainerService.java
@@ -1666,7 +1666,7 @@ public class ContainerService implements IContainerService {
 
 
     private IRunnerResponse convertEntityToDto(Containers container) {
-        return jsonHelper.convertValue(container, ContainerResponse.class);
+        return (IRunnerResponse) commonUtils.setIncludedFieldsToResponse(container,new HashSet<>(defaultIncludeColumns), new ContainerResponse());
     }
 
     private Containers convertRequestToEntity(ContainerRequest request) {
