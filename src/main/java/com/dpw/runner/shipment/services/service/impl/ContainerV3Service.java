7e2bd2c1885e8111a37d7993ac5eca6e75f24c17
Himanshu.Chhikara <himanshu.chhikara@dpworld.com>
2025-04-06 23:52:03 +0530

    create ContainerV3Service
    Signed-off-by: Himanshu.Chhikara <himanshu.chhikara@dpworld.com>

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ContainerV3Service.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ContainerV3Service.java
@@ -2,17 +2,29 @@ package com.dpw.runner.shipment.services.service.impl;
 
 import com.dpw.runner.shipment.services.dto.request.ContainerRequest;
 import com.dpw.runner.shipment.services.dto.response.ContainerResponse;
+import com.dpw.runner.shipment.services.entity.Containers;
+import com.dpw.runner.shipment.services.helpers.JsonHelper;
+import com.dpw.runner.shipment.services.repository.interfaces.IContainerRepository;
 import com.dpw.runner.shipment.services.service.interfaces.IContainerV3Service;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class ContainerV3Service implements IContainerV3Service {
 
+    @Autowired
+    private IContainerRepository containerRepository;
+
+    @Autowired
+    private JsonHelper jsonHelper;
+
     @Override
     public ContainerResponse create(ContainerRequest containerRequest) {
-        return null;
+        Containers container = jsonHelper.convertValue(containerRequest, Containers.class);
+        Containers savedContainer = containerRepository.save(container);
+        return jsonHelper.convertValue(savedContainer, ContainerResponse.class);
     }
     
 }
