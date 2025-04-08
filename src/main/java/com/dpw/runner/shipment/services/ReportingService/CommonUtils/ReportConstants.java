fc72ab67785e47ce3cfc05ec4bad8b35b0c48c68
Tanishq <tanishq.malhotra@dpworld.com>
2025-04-08 16:17:53 +0530

    pre alert email address formatting fix

--- src/main/java/com/dpw/runner/shipment/services/ReportingService/CommonUtils/ReportConstants.java
+++ src/main/java/com/dpw/runner/shipment/services/ReportingService/CommonUtils/ReportConstants.java
@@ -1364,4 +1364,5 @@ public class ReportConstants {
     public static final String TIME_TAKE_TO_GET_SHIPMENT_CONSOLE_DATA = "Time taken to get Shipment Console Data for shipmentId :{} is  {}";
 
     public static final String USA_LOC_CODE_PREFIX = "US";
+    public static final String HTML_BREAK = "<br/>";
 }
