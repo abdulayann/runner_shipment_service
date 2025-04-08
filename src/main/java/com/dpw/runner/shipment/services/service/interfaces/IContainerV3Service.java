86d3e3b855beedc58d31d92bd52c71678def1cb4
mayankgupta <mayank.gupta@dpworld.com>
2025-04-08 17:44:51 +0530

    container summary

--- src/main/java/com/dpw/runner/shipment/services/service/interfaces/IContainerV3Service.java
+++ src/main/java/com/dpw/runner/shipment/services/service/interfaces/IContainerV3Service.java
@@ -1,12 +1,17 @@
 package com.dpw.runner.shipment.services.service.interfaces;
 
+import com.dpw.runner.shipment.services.dto.CalculationAPIsDto.ContainerSummaryResponse;
 import com.dpw.runner.shipment.services.dto.request.ContainerRequest;
 import com.dpw.runner.shipment.services.dto.response.BulkContainerResponse;
 import com.dpw.runner.shipment.services.dto.response.ContainerResponse;
+import com.dpw.runner.shipment.services.entity.Containers;
+import com.dpw.runner.shipment.services.exception.exceptions.RunnerException;
+
 import java.util.List;
 
 public interface IContainerV3Service {
     ContainerResponse create(ContainerRequest containerRequest);
     BulkContainerResponse updateBulk(List<ContainerRequest> request);
     BulkContainerResponse deleteBulk(List<ContainerRequest> request);
+    ContainerSummaryResponse calculateContainerSummary(List<Containers> containersList, String transportMode, String containerCategory) throws RunnerException;
 }
