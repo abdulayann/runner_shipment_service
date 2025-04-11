e88da2e8a534235a62faaacbe782784d63538897
Subham Mallick <subham.mallick@dpworld.com>
2025-04-11 11:57:25 +0530

    trim-place-desc

---
+++ src/main/resources/db/migration/V1.268__alter_table_booking_add_rejection_remarks.sql
@@ -0,0 +1,7 @@
+CREATE INDEX IF NOT EXISTS idx_event_entity_id ON events(entity_id);
+CREATE INDEX IF NOT EXISTS idx_event_entity_type ON events(entity_type);
+CREATE INDEX IF NOT EXISTS idx_event_entity_id_type ON events(entity_id, entity_type);
+
+CREATE INDEX IF NOT EXISTS idx_events_dump_entity_id ON events_dump(entity_id);
+CREATE INDEX IF NOT EXISTS idx_events_dump_entity_type ON events_dump(entity_type);
+CREATE INDEX IF NOT EXISTS idx_events_dump_entity_id_type ON events_dump(entity_id, entity_type);
