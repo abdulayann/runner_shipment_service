0cba0b12333ba7202e9da5e8f8fa82ac119e16a0
Mylavarapu Vamsi <mylavarapu.vamsi@CTIPL-MAC-584.local>
2025-04-22 19:26:55 +0530

    Removed unused import

--- src/main/java/com/dpw/runner/shipment/services/dto/request/GetMatchingRulesRequest.java
+++ src/main/java/com/dpw/runner/shipment/services/dto/request/GetMatchingRulesRequest.java
@@ -1,7 +1,6 @@
 package com.dpw.runner.shipment.services.dto.request;
 
 import com.dpw.runner.shipment.services.entity.enums.DpsExecutionStatus;
-import com.dpw.runner.shipment.services.entity.enums.DpsWorkflowState;
 import lombok.*;
 
 import java.util.List;
