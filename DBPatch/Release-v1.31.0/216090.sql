8a89d39332fd2ee3a56dc0e73ddbb587866f9cbb
Rakesh Pradhan <rakesh.pradhan@dpworld.com>
2025-04-24 13:54:56 +0530

    prod support #216090 #216370

--- DBPatch/Release-v1.31.0/216090.sql
+++ DBPatch/Release-v1.31.0/216090.sql
@@ -1,18 +1,20 @@
 Update shipment_details
     SET client_id = 126165
-where shipmentId IN ('MILS24110421','HANS24120299','MILS25010983','MILS25011045','MILS25021266','MILS25021366')
+where shipment_id IN ('MILS24110421','HANS24120299','MILS25010983','MILS25011045','MILS25021266','MILS25021366')
 and tenant_id = 459
 and client_id = 135677;
 
 Update shipment_details
     SET consignee_id = 126165
-where shipmentId IN ('MILS24110421','HANS24120299','MILS25010983','MILS25011045','MILS25021266','MILS25021366')
+where shipment_id IN ('MILS24110421','HANS24120299','MILS25010983','MILS25011045','MILS25021266','MILS25021366')
 and tenant_id = 459
 and consignee_id = 135677;
 
 
-Update shipment_details
-    SET client_id = 126165
-where shipmentId IN ('MILS24110421','HANS24120299','MILS25010983','MILS25011045','MILS25021266','MILS25021366')
-and tenant_id = 459
-and client_id = 135677;
+Update shipment_additional_details
+    SET notify_party_id = 126165
+where shipment_id in
+    (Select id from shipment_details
+        where shipment_id in ('MILS24110421','HANS24120299','MILS25010983','MILS25011045','MILS25021266','MILS25021366')
+    and tenant_id = 459)
+and notify_party_id = 135677;
--- DBPatch/Release-v1.31.0/216370.sql
+++ DBPatch/Release-v1.31.0/216370.sql
@@ -1,5 +1,5 @@
 Update shipment_details
     SET client_id = 135677
-where shipmentId IN ('SNZA25042678')
+where shipment_id IN ('SNZA25042678')
 and tenant_id = 583
 and client_id = 148723;
