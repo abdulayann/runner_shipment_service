dffe181b8ae58152bbdbadc5fba273e6280c17c9
Aditya.Thakur <aditya.thakur@dpworld.com>
2025-05-09 16:23:24 +0530

    removed httpheaders change

--- src/main/java/com/dpw/runner/shipment/services/adapters/impl/PlatformServiceAdapter.java
+++ src/main/java/com/dpw/runner/shipment/services/adapters/impl/PlatformServiceAdapter.java
@@ -14,10 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 
 @Service
 @Slf4j
@@ -41,11 +43,9 @@ public class PlatformServiceAdapter implements IPlatformServiceAdapter {
         String url = baseUrl + "/booking/external";
         log.info("Platform Create Request for booking reference {}: {}", request.getBooking_ref_code(), request);
         log.info("Payload sent for event: {} with request payload: {}", IntegrationType.PLATFORM_CREATE_BOOKING, jsonHelper.convertToJsonWithNulls(request));
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        log.info("Headers: {}", headers);
-        ResponseEntity<?> responseEntity = restTemplate.exchange(RequestEntity.post(URI.create(url)).headers(headers).body(jsonHelper.convertToJsonWithNulls(request)), Object.class);
-        log.info("Response: {}", jsonHelper.convertToJson(responseEntity.getBody()));
+        restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+        ResponseEntity<?> responseEntity = restTemplate.exchange(RequestEntity.post(URI.create(url)).body(jsonHelper.convertToJsonWithNulls(request)), Object.class);
+        log.info("Response for endpoint url: {}, : {}", url, jsonHelper.convertToJson(responseEntity.getBody()));
         return ResponseHelper.buildDependentServiceResponse(responseEntity.getBody(),0,0);
     }
 
