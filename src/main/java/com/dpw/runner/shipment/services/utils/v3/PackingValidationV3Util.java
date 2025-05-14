e9fc6c0c39923af27a769485345085ff588a6ab1
Wasim Jafar <wasim.jafar@dpworld.com>
2025-05-14 11:25:33 +0530

    Revert "Lazy"
    This reverts commit b4a1bbbea3e40550fe6f78197487beba84f17c3b.

--- src/main/java/com/dpw/runner/shipment/services/utils/v3/PackingValidationV3Util.java
+++ src/main/java/com/dpw/runner/shipment/services/utils/v3/PackingValidationV3Util.java
@@ -16,7 +16,6 @@ import com.dpw.runner.shipment.services.service.interfaces.IShipmentServiceV3;
 import com.dpw.runner.shipment.services.utils.CommonUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.dao.DataRetrievalFailureException;
 import org.springframework.stereotype.Component;
 
@@ -31,7 +30,6 @@ import java.util.stream.Collectors;
 @Component
 public class PackingValidationV3Util {
     @Autowired
-    @Lazy
     private IShipmentServiceV3 shipmentService;
 
     @Autowired
