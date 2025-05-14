83784b7fcac28b7d4f94bbe94c8815fa4e04c126
Wasim Jafar <wasim.jafar@dpworld.com>
2025-05-14 11:25:15 +0530

    Revert "Lazy"
    This reverts commit 858c4902a7ff26e85bb53d9c74e1ff1d88341d5e.

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ConsolidationV3Service.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ConsolidationV3Service.java
@@ -274,7 +274,6 @@ public class ConsolidationV3Service implements IConsolidationV3Service {
     private IEventsV3Service eventV3Service;
 
     @Autowired
-    @Lazy
     private IShipmentServiceV3 shipmentV3Service;
 
     @Autowired
