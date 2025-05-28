5c7da9d0836b9e2b507991313bc400871ca0bae4
Pardeep Malik <Pardeep.Malik@dpworld.com>
2025-05-28 13:37:11 +0530

    Doc Changes added for EWL

--- src/main/java/com/dpw/runner/shipment/services/entitytransfer/dto/request/SendFileToExternalRequest.java
+++ src/main/java/com/dpw/runner/shipment/services/entitytransfer/dto/request/SendFileToExternalRequest.java
@@ -3,6 +3,9 @@ package com.dpw.runner.shipment.services.entitytransfer.dto.request;
 import com.dpw.runner.shipment.services.commons.requests.IRunnerRequest;
 import lombok.*;
 
+import java.util.List;
+import java.util.Map;
+
 @Data
 @Builder
 @ToString
@@ -12,4 +15,6 @@ public class SendFileToExternalRequest implements IRunnerRequest {
     private Long entityId;
     private String entityType;
     private String sendToBranch;
+    private List<String> additionalDocs;
+    private Map<String, List<String>> shipAdditionalDocs;
 }
