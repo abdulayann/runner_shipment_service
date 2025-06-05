2c82baa6210e43528a04ebe3e9c13780a8a66077
Pardeep Malik <Pardeep.Malik@dpworld.com>
2025-06-05 12:14:46 +0530

    Loc code added

--- src/main/java/com/dpw/runner/shipment/services/service/impl/RoutingsV3Service.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/RoutingsV3Service.java
@@ -330,17 +330,19 @@ public class RoutingsV3Service implements IRoutingsV3Service {
         carrierDetails.setEtd(firstLeg.getEtd());
         carrierDetails.setAtd(firstLeg.getAtd());
         carrierDetails.setOriginPort(firstLeg.getPol());
-        carrierDetails.setOriginLocCode(firstLeg.getOriginPortLocCode());
-        if(Boolean.TRUE.equals(carrierDetails.getIsSameAsOriginPort())){
+        carrierDetails.setOriginPortLocCode(firstLeg.getOriginPortLocCode());
+        if(Boolean.TRUE.equals(carrierDetails.getIsSameAsOriginPort())) {
             carrierDetails.setOrigin(carrierDetails.getOriginPort());
+            carrierDetails.setOriginLocCode(carrierDetails.getOriginPortLocCode());
         }
 
         carrierDetails.setEta(lastLeg.getEta());
         carrierDetails.setAta(lastLeg.getAta());
         carrierDetails.setDestinationPort(lastLeg.getPod());
         carrierDetails.setDestinationPortLocCode(lastLeg.getDestinationPortLocCode());
-        if(Boolean.TRUE.equals(carrierDetails.getIsSameAsDestinationPort())){
+        if(Boolean.TRUE.equals(carrierDetails.getIsSameAsDestinationPort())) {
             carrierDetails.setDestination(carrierDetails.getDestinationPort());
+            carrierDetails.setDestinationLocCode(carrierDetails.getDestinationPortLocCode());
         }
         ShipmentSettingsDetails shipmentSettingsDetails = commonUtils.getShipmentSettingFromContext();
         if (shipmentSettingsDetails != null && Boolean.TRUE.equals(shipmentSettingsDetails.getIsAutomaticTransferEnabled()) && isValidDateChange(carrierDetails, existingCarrierDetails))
