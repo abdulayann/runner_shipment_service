22e4dd377012b7efa9765ca2c868d1fa2f828989
mayankgupta <mayank.gupta@dpworld.com>
2025-06-05 15:39:26 +0530

    removed existing achieved calc

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ConsolidationV3Service.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ConsolidationV3Service.java
@@ -527,8 +527,7 @@ public class ConsolidationV3Service implements IConsolidationV3Service {
 
         if(!Boolean.TRUE.equals(isCreate)){
             // This method will only work for non air transport modes , validation check moved inside the method
-            calculateAchievedValues(consolidationDetails, new ShipmentGridChangeV3Response(), oldEntity.getShipmentsList());
-            shipmentDetails = updateLinkedShipmentData(consolidationDetails, oldEntity, false);
+            updateLinkedShipmentData(consolidationDetails, oldEntity, false);
         }
 
         if(consolidationDetails.getDocumentationPartner() != null && consolidationDetails.getDocumentationPartner() == 0)
@@ -2492,7 +2491,6 @@ public class ConsolidationV3Service implements IConsolidationV3Service {
         }
         ConsolidationDetails consoleDetails = consolidationDetails.get();
         log.info(ConsolidationConstants.CONSOLIDATION_DETAILS_FETCHED_SUCCESSFULLY, id, LoggerHelper.getRequestIdFromMDC());
-        calculateAchievedValuesForRetrieve(consoleDetails);
         ConsolidationDetailsV3Response response = jsonHelper.convertValue(consoleDetails, ConsolidationDetailsV3Response.class);
         List<Routings> routingsList = consoleDetails.getRoutingsList();
         if (!CollectionUtils.isEmpty(routingsList)) {
@@ -2512,14 +2510,6 @@ public class ConsolidationV3Service implements IConsolidationV3Service {
         var notificationMap = notificationDao.pendingNotificationCountBasedOnEntityIdsAndEntityType(Arrays.asList(consolidationDetails.getId()), CONSOLIDATION);
         int pendingCount = map.getOrDefault(consolidationDetails.getId(), 0) + notificationMap.getOrDefault(consolidationDetails.getId(), 0);
         response.setPendingActionCount((pendingCount == 0) ? null : pendingCount);
-    }
-
-    private void calculateAchievedValuesForRetrieve(ConsolidationDetails consolidationDetails) {
-        try {
-            calculateAchievedValues(consolidationDetails, new ShipmentGridChangeV3Response(), consolidationDetails.getShipmentsList());
-        } catch (Exception e) {
-            log.error("Error while calculating achieved values for Consolidation with Id " + consolidationDetails.getId());
-        }
     }
 
     public void createConsolidationPayload(ConsolidationDetails consolidationDetails, ConsolidationDetailsV3Response consolidationDetailsV3Response) {
@@ -3210,9 +3200,6 @@ public class ConsolidationV3Service implements IConsolidationV3Service {
             warning = "Mail Template not found, please inform the region users individually";
         }
         processInterConsoleDetachShipment(consol, shipmentIdList);
-        CalculateAchievedValueRequest request = CalculateAchievedValueRequest.builder()
-                .consolidationId(consolidationId).build();
-        calculateAchievedValues(request);
 
         return ResponseHelper.buildSuccessResponseWithWarning(warning);
     }
