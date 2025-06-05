7f37c4f956d9dd9c676d382d43fdf6566a2f6b41
Pardeep Malik <Pardeep.Malik@dpworld.com>
2025-06-05 12:14:46 +0530

    Loc code added

--- src/main/java/com/dpw/runner/shipment/services/service/impl/RoutingsV3Service.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/RoutingsV3Service.java
@@ -340,8 +340,9 @@ public class RoutingsV3Service implements IRoutingsV3Service {
         carrierDetails.setAta(lastLeg.getAta());
         carrierDetails.setDestinationPort(lastLeg.getPod());
         carrierDetails.setDestinationPortLocCode(lastLeg.getDestinationPortLocCode());
-        if(Boolean.TRUE.equals(carrierDetails.getIsSameAsDestinationPort())){
+        if(Boolean.TRUE.equals(carrierDetails.getIsSameAsDestinationPort())) {
             carrierDetails.setDestination(carrierDetails.getDestinationPort());
+            carrierDetails.setDestinationLocCode(carrierDetails.getDestinationPortLocCode());
         }
         ShipmentSettingsDetails shipmentSettingsDetails = commonUtils.getShipmentSettingFromContext();
         if (shipmentSettingsDetails != null && Boolean.TRUE.equals(shipmentSettingsDetails.getIsAutomaticTransferEnabled()) && isValidDateChange(carrierDetails, existingCarrierDetails))
