2b38331c52d5560094fce6a109358be0609973e1
Aditya.Thakur <aditya.thakur@dpworld.com>
2025-06-05 14:09:01 +0530

    cb->shipment fix

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ShipmentServiceImplV3.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ShipmentServiceImplV3.java
@@ -1875,6 +1875,8 @@ public class ShipmentServiceImplV3 implements IShipmentServiceV3 {
         if (isConsoleCreationNeededV3(customerBookingRequest)) {
             ConsolidationDetailsV3Request consolidationDetailsV3Request = ConsolidationDetailsV3Request.builder().
                     carrierDetails(CarrierDetailRequest.builder()
+                            .origin(customerBookingRequest.getCarrierDetails().getOrigin())
+                            .destination(customerBookingRequest.getCarrierDetails().getDestination())
                             .shippingLine(customerBookingRequest.getCarrierDetails().getShippingLine())
                             .originPort(customerBookingRequest.getCarrierDetails().getOriginPort())
                             .destinationPort(customerBookingRequest.getCarrierDetails().getDestinationPort())
@@ -1884,6 +1886,7 @@ public class ShipmentServiceImplV3 implements IShipmentServiceV3 {
                             .ata(customerBookingRequest.getCarrierDetails().getAta())
                             .atd(customerBookingRequest.getCarrierDetails().getAtd())
                             .build()).
+                    consolidationType("STD").
                     transportMode(customerBookingRequest.getTransportType()).
                     containerCategory(customerBookingRequest.getCargoType()).
                     shipmentType(customerBookingRequest.getDirection()).
