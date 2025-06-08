edf56f7eae061084ed3b0565f8466fcdb8076117
Subham Mallick <subham.mallick@dpworld.com>
2025-06-08 11:36:46 +0530

    221868

--- src/test/java/com/dpw/runner/shipment/services/adapters/TrackingServiceAdapterTest.java
+++ src/test/java/com/dpw/runner/shipment/services/adapters/TrackingServiceAdapterTest.java
@@ -506,7 +506,7 @@ class TrackingServiceAdapterTest {
         String result = trackingServiceAdapter.convertTrackingEventCodeToShortCode(event, container);
 
         // Expect EMCR to be returned
-        assertEquals(EventConstants.EMCR, result);
+        assertEquals(EventConstants.GATE_IN_WITH_CONTAINER_EMPTY, result);
     }
 
     @Test
