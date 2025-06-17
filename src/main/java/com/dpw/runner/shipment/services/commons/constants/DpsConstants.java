451a0fe9e59565576a58552dcd59ba73e64ad4c4
Mylavarapu Vamsi <mylavarapu.vamsi@CTIPL-MAC-584.local>
2025-06-17 17:14:11 +0530

    Changes in DPS alert message

--- src/main/java/com/dpw/runner/shipment/services/commons/constants/DpsConstants.java
+++ src/main/java/com/dpw/runner/shipment/services/commons/constants/DpsConstants.java
@@ -4,7 +4,7 @@ public class DpsConstants {
 
     public static final String DOPR = "DOPR";
     public static final String DPS_ERROR_1 = "This shipment is currently on Compliance Hold. You are not permitted to proceed with this action until the hold is lifted";
-    public static final String DPS_ERROR_2 = "The Shipment cannot be linked to the consolidation due to Country/Goods Screening Hold/Warning";
+    public static final String DPS_ERROR_2 = "This shipment is currently on Compliance Hold. You are not permitted to proceed with this action until the hold is lifted";
     public static final String HAWBPR = "HAWBPR";
     public static final String HBLPR = "HBLPR";
     public static final String CONCR = "CONCR";
