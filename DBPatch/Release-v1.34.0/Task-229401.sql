66554acbdab370796fe1b9859fdfb32f48360609
Priyansh Shukla <Priyansh.Shukla@dpworld.com>
2025-06-19 10:01:26 +0000

    Added Task-229401.sql

---
+++ DBPatch/Release-v1.34.0/Task-229401.sql
@@ -0,0 +1,6 @@
+UPDATE parties
+SET
+  org_id = '258622',
+  org_code = 'FRC00037889',
+  org_data = '{"Id":258622,"Guid":"f6fa7010-194b-4bb4-a6b7-fac7c3a318fc","AddressShortCode":"FRO00039542","CompanyName":"TATA STEEL IJMUIDEN BV","AddressType":2,"Address1":"MAGAZIJN IJZER EN STAAL 3E. 16","Address2":"","Country":"NLD","City":"Velsen-Noord","ZipPostCode":"1951 MC","Email":"","OrgGuid":"b728c59d-d1fb-43f9-bf34-20950e1a6fae","OrgOrganizationCode":"FRC00037889","OrgSource":"CRP","OrgFullName":"TATA STEEL IJMUIDEN BV","OrgActiveClient":true,"OrgReceivables":false,"OrgPayables":false,"RegulatedAgent":false,"KnownConsignor":false}'
+WHERE id = '1016264';
