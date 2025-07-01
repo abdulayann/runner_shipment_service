041871bcf656b62a672d71db0a2ce4fe0ae03a1e
Wasim Jafar <wasim.jafar@dpworld.com>
2025-07-01 16:14:16 +0530

    AWB

--- src/main/java/com/dpw/runner/shipment/services/service/impl/AwbService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/AwbService.java
@@ -2071,6 +2071,7 @@ public class AwbService implements IAwbService {
         awb.setAwbOtherChargesInfo(resetAwb.getAwbOtherChargesInfo());
         awb.setAwbPaymentInfo(resetAwb.getAwbPaymentInfo());
         awb.setAwbSpecialHandlingCodesMappings(resetAwb.getAwbSpecialHandlingCodesMappings());
+        awb.setAirMessagingAdditionalFields(resetAwb.getAirMessagingAdditionalFields());
         // Link
         linkHawbMawb(awb, awbList, consolidationDetails.getInterBranchConsole());
         getMawnLinkPacks(awb, true, awbList);
