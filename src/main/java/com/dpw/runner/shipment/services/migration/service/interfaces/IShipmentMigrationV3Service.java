3b19ba7c89f183060e6ab1b019f26d445982eabc
Himanshu.Chhikara <himanshu.chhikara@dpworld.com>
2025-07-14 11:35:44 +0530

    Interface syntax corrected

--- src/main/java/com/dpw/runner/shipment/services/migration/service/interfaces/IShipmentMigrationV3Service.java
+++ src/main/java/com/dpw/runner/shipment/services/migration/service/interfaces/IShipmentMigrationV3Service.java
@@ -15,7 +15,7 @@ public interface IShipmentMigrationV3Service {
 
     ShipmentDetails migrateShipmentV3ToV2(ShipmentDetails shipmentDetails) throws RunnerException;
     ShipmentDetails migrateShipmentV3ToV2(ShipmentDetails shipmentDetails, Map<String, EntityTransferContainerType> containerTypeMap) throws RunnerException;
-    public void setContainerUtilisation(Set<Containers> containers, Map<String, EntityTransferContainerType> containerTypeMap, boolean isFCL) throws RunnerException;
+    void setContainerUtilisation(Set<Containers> containers, Map<String, EntityTransferContainerType> containerTypeMap, boolean isFCL) throws RunnerException;
 
     Map<String, EntityTransferContainerType> fetchContainerTypeDetails(List<Containers> containers);
 }
