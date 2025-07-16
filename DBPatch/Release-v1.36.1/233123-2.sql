8fa9d8bfe391cda3dae9f3cd7a630c108ffb35c4
isha <isha.mittal@dpworld.com>
2025-07-16 21:19:17 +0530

    fix for ntr

---
+++ DBPatch/Release-v1.36.1/233123-2.sql
@@ -0,0 +1,36 @@
+UPDATE consolidation_details c
+SET is_transferred_to_receiving_branch = TRUE
+WHERE EXISTS (
+    SELECT 1
+    FROM consolidation_details cd2
+    JOIN network_transfer nt
+        ON nt.entity_id = c.id
+        AND nt.entity_guid = c.guid
+        AND nt.entity_type = 'CONSOLIDATION'
+        AND nt.job_type = 'IMP'
+        AND nt.status = 'ACCEPTED'
+    WHERE cd2.source_guid = c.guid
+      AND cd2.source_tenant_id = c.tenant_id
+      AND cd2.tenant_id = c.receiving_branch
+      AND c.receiving_branch IS NOT NULL
+      AND c.receiving_branch != c.tenant_id
+);
+
+UPDATE triangulation_partner_consolidation tpc
+SET is_accepted = TRUE
+WHERE EXISTS (
+    SELECT 1
+    FROM consolidation_details c
+    LEFT JOIN consolidation_details cd2
+        ON cd2.source_guid = c.guid
+        AND cd2.source_tenant_id = c.tenant_id
+        AND cd2.tenant_id = tpc.partner_id
+        AND nt.status = 'ACCEPTED'
+    JOIN network_transfer nt
+        ON nt.entity_id = c.id
+        AND nt.entity_guid = c.guid
+        AND nt.tenant_id = tpc.partner_id  -- extra condition
+        AND nt.job_type = 'CTS'
+    WHERE c.id = tpc.consolidation_id
+      AND cd2.id IS NOT NULL
+);
---
+++ DBPatch/Release-v1.36.1/233123-3.sql
@@ -0,0 +1,87 @@
+INSERT INTO network_transfer (
+    created_at,
+    created_by ,
+    updated_at,
+    updated_by,
+    tenant_id,
+    entity_type,
+    entity_number,
+    entity_id,
+    transport_mode,
+    source_branch_id,
+    status,
+    job_type,
+    entity_guid,
+    created_entity_id
+)
+SELECT
+    now() AT TIME ZONE 'UTC' AS created_at,
+    shp.created_by,
+    now() AT TIME ZONE 'UTC' AS updated_at,
+    shp.updated_by,
+    shp.receiving_branch,
+    'SHIPMENT' AS entity_type,
+    shp.shipment_id,
+    shp.id,
+    shp.transport_mode,
+    shp.tenant_id,
+    'ACCEPTED',
+    'IMP',
+    shp.guid,
+    shp2.id -- <== this is the child consolidation that references c.guid
+FROM shipment_details shp
+LEFT JOIN network_transfer nt
+    ON nt.entity_id = shp.id  AND nt.entity_guid = shp.guid
+LEFT JOIN shipment_details shp2
+    ON shp2.source_guid = shp.guid AND shp2.tenant_id = shp.receiving_branch and shp2.source_tenant_id =shp.tenant_id
+WHERE shp.receiving_branch IS NOT NULL
+  AND shp.receiving_branch != shp.tenant_id
+  AND shp2.id IS NOT NULL
+  AND nt.entity_id IS NULL
+  and shp.id!=8768;
+
+
+
+INSERT INTO network_transfer (
+    created_at,
+    created_by ,
+    updated_at,
+    updated_by,
+    tenant_id,
+    entity_type,
+    entity_number,
+    entity_id,
+    transport_mode,
+    source_branch_id,
+    status,
+    job_type,
+    entity_guid,
+    created_entity_id
+)
+SELECT
+    now() AT TIME ZONE 'UTC' AS created_at,
+    c.created_by,
+    now() AT TIME ZONE 'UTC' AS updated_at,
+    c.updated_by,
+    tpc.partner_id AS triangulation_branch,
+    'SHIPMENT' AS entity_type,
+    c.shipment_id,
+    c.id AS parent_id,
+    c.transport_mode,
+    c.tenant_id,
+    'ACCEPTED',
+    'CTS',
+    c.guid,
+    cd2.id
+FROM shipment_details c
+JOIN triangulation_partner_shipment tpc
+    ON tpc.shipment_id = c.id
+LEFT JOIN shipment_details cd2
+    ON cd2.source_guid = c.guid
+    AND cd2.source_tenant_id = c.tenant_id
+    AND cd2.tenant_id = tpc.partner_id
+LEFT JOIN network_transfer nt
+    ON nt.entity_id = c.id AND nt.entity_guid = c.guid AND nt.job_type ='CTS'
+WHERE cd2.id IS NOT NULL
+  AND nt.entity_id IS NULL;
+
