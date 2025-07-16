e0d136f482b78b9968f2fcec5569b823cf408e52
Wasim Jafar <wasim.jafar@dpworld.com>
2025-07-16 09:43:02 +0530

    Release Number

--- src/main/java/com/dpw/runner/shipment/services/RunnerShipmentServicesApplication.java
+++ src/main/java/com/dpw/runner/shipment/services/RunnerShipmentServicesApplication.java
@@ -29,7 +29,7 @@ public class RunnerShipmentServicesApplication {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(RunnerShipmentServicesApplication.class, args);
         log.info("--------==========Shipment Service Started==========--------");
-
+        log.info("Release-v3.1.0");
     }
 
 
