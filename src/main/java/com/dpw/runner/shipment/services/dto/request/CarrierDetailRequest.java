ae0d2a409632e9b65e86e2a8d380652fb03e5031
lalchand.mali <lalchand@nsg-corporate-210.87.186.122.airtel.in>
2025-07-28 15:06:50 +0530

    moved validations from db level to request

--- src/main/java/com/dpw/runner/shipment/services/dto/request/CarrierDetailRequest.java
+++ src/main/java/com/dpw/runner/shipment/services/dto/request/CarrierDetailRequest.java
@@ -5,6 +5,7 @@ import com.dpw.runner.shipment.services.utils.ExcludeTimeZone;
 import io.swagger.annotations.ApiModel;
 import lombok.*;
 
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -18,16 +19,24 @@ import java.util.UUID;
 public class CarrierDetailRequest implements IRunnerRequest {
     private Long id;
     private UUID guid;
+    @Size(max=64, message = "max size is 64 for shipping line" )
     private String shippingLine;
     private String vessel;
+    @Size(max = 20, message = "max size is 20 for voyage")
     private String voyage;
     private String flightNumber;
+    @Size(max=20, message = "max size is 20 for aircraft type")
     private String aircraftType;
+    @Size(max=20, message = "max size is 20 for aircraft registration")
     private String aircraftRegistration;
     private String truckRefNumber;
+    @Size(max=10, message = "max size is 10 for journey number")
     private String journeyNumber;
+    @Size(max=10, message = "max size is 10 for journey ref number")
     private String journeyRefNumber;
+    @Size(max=100, message = "max size is 100 for origin")
     private String origin;
+    @Size(max=100, message = "max size is 100 for destination")
     private String destination;
     @ExcludeTimeZone
     private LocalDateTime eta;
@@ -40,16 +49,24 @@ public class CarrierDetailRequest implements IRunnerRequest {
     private Long shipmentId;
     private String originPort;
     private String destinationPort;
+    @Size(max=100, message = "max size is 100 for cfs")
     private String cfs;
     private LocalDateTime vesselBerthingDate;
+    @Size(max = 32, message = "max size is 32 for carrier country")
     private String carrierCountry;
+    @Size(max = 15, message = "max size is 15 for min transit hours")
     private String minTransitHours;
+    @Size(max = 15, message = "max size is 15 for max transit hours")
     private String maxTransitHours;
     private Boolean carrierAddedFromNpm;
     private Boolean isCarrierChanged;
+    @Size(max=64, message = "max size is 64 for origin loc code")
     private String originLocCode;
+    @Size(max=64, message = "max size is 64 for destination loc code")
     private String destinationLocCode;
+    @Size(max=64, message = "max size is 64 for origin port loc code")
     private String originPortLocCode;
+    @Size(max=64, message = "max size is 64 for destination port loc code")
     private String destinationPortLocCode;
     private Boolean isSameAsOriginPort;
     private Boolean isSameAsDestinationPort;
