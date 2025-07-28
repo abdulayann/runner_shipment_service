0279ff4fb1e11517e6eba5bc620dc1217c44482d
Anupam Gupta <anupam.rtp@gmail.com>
2025-07-28 18:11:58 +0530

    Shipment Cargo Summary changes for SEA FCL

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ConsolidationV3Service.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ConsolidationV3Service.java
@@ -2898,7 +2898,7 @@ public class ConsolidationV3Service implements IConsolidationV3Service {
         }
     }
 
-    private String determineWeightChargeableUnit(ShipmentSettingsDetails shipmentSettingsDetails){
+    public String determineWeightChargeableUnit(ShipmentSettingsDetails shipmentSettingsDetails){
         String weightChargeableUnit = Constants.WEIGHT_UNIT_KG;
         if(!isStringNullOrEmpty(shipmentSettingsDetails.getWeightChargeableUnit()))
             weightChargeableUnit = shipmentSettingsDetails.getWeightChargeableUnit();
