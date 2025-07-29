d734e64f1c13778e9e134d74256109963cc3e2d6
neerajkumar <neeraj.kumar@dpworld.com>
2025-07-29 18:20:20 +0530

    production issue fix : #240047

---
+++ DBPatch/Release-v1.36.2/task_240047.sql
@@ -0,0 +1,101 @@
+update
+	parties
+set
+	org_code = 'FRC00039841',
+	address_code = 'FRO00041585',
+	org_id = '157275',
+	address_id = '262371',
+	updated_at = NOW(),
+	org_data = '{
+        "Id": 157275,
+        "City": "Dottingen",
+        "Guid": "52634fc2-8c10-4015-8e66-dfd8a62f2b3f",
+        "Email": "Nal@sara-transporte.ch",
+        "IsGSA": false,
+        "Phone": "0041562819884",
+        "State": "Aargau",
+        "label": "Sara Transporte AG",
+        "value": "FRC00039841",
+        "Broker": false,
+        "Source": "CRP",
+        "Carrier": false,
+        "Country": "CHE",
+        "Address1": "Vorhard 15",
+        "FullName": "Sara Transporte AG",
+        "IsActive": 1,
+        "Payables": false,
+        "Services": false,
+        "TenantId": 442,
+        "CompanyId": 372,
+        "Consignee": true,
+        "Consigner": true,
+        "IsParnter": false,
+        "PANNumber": "CHE-264.842.468",
+        "TaxVendor": false,
+        "WareHouse": false,
+        "AirCarrier": false,
+        "ExtraParam": "",
+        "InsertDate": "2025-07-06T15:11:39.000",
+        "SeaCarrier": false,
+        "UpdateDate": "2025-07-06T17:10:16.000",
+        "IsSuspended": false,
+        "RailCarrier": false,
+        "Receivables": true,
+        "RoadCarrier": false,
+        "ZipPostCode": "5312",
+        "ActiveClient": true,
+        "CurrencyCode": "CHF",
+        "EmailInvoice": false,
+        "InsertUserId": 1712,
+        "UpdateUserId": 1712,
+        "VatRegNumber": "CHE264842468",
+        "ForworderAgent": false,
+        "IsCreditEnabled": false,
+        "IsV2PaymentTerm": false,
+        "TransportClient": false,
+        "IsWalkInCustomer": false,
+        "OrganizationCode": "FRC00039841",
+        "EnableBulkInvoice": false,
+        "CustomerIdentifier": "1937295_001",
+        "ReceivableTermsDate": 2,
+        "InsertUserIdUsername": "p100serviceaccountegypt@dpworld.com",
+        "UpdateUserIdUsername": "p100serviceaccountegypt@dpworld.com",
+        "IsClientEInvoiceEnabled": false,
+        "ImportExportClearanceLock": false,
+        "FusionCreditlimitOverrideApproved": false
+    }',
+	address_data = '{
+        "Id": 262371,
+        "City": "Dottingen",
+        "Email": "Nal@sara-transporte.ch",
+        "State": "Aargau",
+        "label": "FRO00041585",
+        "value": "FRO00041585",
+        "Country": "CHE",
+        "OrgGuid": "52634fc2-8c10-4015-8e66-dfd8a62f2b3f",
+        "Address1": "Vorhard 15",
+        "OrgSource": "CRP",
+        "AddressType": 1,
+        "CompanyName": "Sara Transporte AG",
+        "OrgFullName": "Sara Transporte AG",
+        "OrgPayables": false,
+        "ZipPostCode": "5312",
+        "TaxRegNumber": "CHE264842468",
+        "OrgReceivables": true,
+        "SiteIdentifier": "1937295_001_CHTWL_001_B",
+        "OrgActiveClient": true,
+        "AddressShortCode": "FRO00041585",
+        "OrgOrganizationCode": "FRC00039841"
+    }'
+where
+	id = 1252894;
+
+
+
+update
+	shipment_details
+set
+	client_dps_address_id = 98202,
+	updated_at = NOW()
+where
+	id = 80432;
