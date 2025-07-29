2d1005052266c6c9999c6c449be9ba30d196e47b
neerajkumar <neeraj.kumar@dpworld.com>
2025-07-29 18:31:22 +0530

    production issue fix : #240048

--- DBPatch/Release-v1.36.2/task_239720.sql
+++ DBPatch/Release-v1.36.2/task_239720.sql
@@ -1,3 +1,8 @@
-UPDATE public.consolidation_details
-SET receiving_branch = 583
-WHERE id = 58643;
+UPDATE awb
+SET air_message_status = 'AWB_GENERATED'
+WHERE shipment_id = 80958;
+
+UPDATE awb
+SET air_message_status             = 'AWB_GENERATED',
+    linked_hawb_air_message_status = 'AWB_GENERATED'
+WHERE consolidation_id = 61388;
---
+++ DBPatch/Release-v1.36.2/task_240048.sql
@@ -0,0 +1,3 @@
+UPDATE public.consolidation_details
+SET receiving_branch = 583
+WHERE id = 58643;
