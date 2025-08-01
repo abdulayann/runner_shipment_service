ea40615d91ce450f94bf2ec452c44ad29e7e26ed
Raksha Jain <raksha.jain@CTIPL-MAC-MA22.local>
2025-08-01 17:22:23 +0530

    Added Date validations for Routing Legs

--- src/main/java/com/dpw/runner/shipment/services/service/impl/RoutingsV3Service.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/RoutingsV3Service.java
@@ -668,6 +668,9 @@ public class RoutingsV3Service implements IRoutingsV3Service {
     public BulkRoutingResponse updateBulk(BulkUpdateRoutingsRequest request, String module) throws RunnerException {
         routingValidationUtil.validateBulkUpdateRoutingRequest(request, module);
         List<RoutingsRequest> incomingRoutings = request.getRoutings();
+        routingValidationUtil.validateRoutingLegs(incomingRoutings);
+        routingValidationUtil.validateMainCarriageRoutingLegs(incomingRoutings);
+
         setFlightNumberInCaseAir(incomingRoutings);
         // Separate IDs and determine existing routing
         List<Long> incomingIds = getIncomingRoutingsIds(incomingRoutings);
@@ -683,7 +686,6 @@ public class RoutingsV3Service implements IRoutingsV3Service {
 
         List<Routings> routingsList = reOrderRoutings(jsonHelper.convertValueToList(incomingRoutings, Routings.class), existingRoutings);
         // Separate into create and update requests
-
         List<Routings> allSavedRouting = routingsDao.saveAll(routingsList);
 
         ParentResult parentResult = getParentDetails(allSavedRouting, request.getEntityId(), module);
