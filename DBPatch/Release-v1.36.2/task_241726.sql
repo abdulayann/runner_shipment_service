7a009b580b051eee0b5d199b3b628a71e9117541
Sai Kumar Reddy <Saikumar.Reddy@dpworld.com>
2025-08-05 09:30:36 +0000

    task 241726

---
+++ DBPatch/Release-v1.36.2/task_241726.sql
@@ -0,0 +1,23 @@
+UPDATE awb
+SET awb_shipment_info = jsonb_set(awb_shipment_info, '{consigneeName}', '"CARGO SERVICES AIRFREIGHT (SHANGHAI) LTD. (CSL)"', true)
+WHERE consolidation_id = 63901;
+
+
+UPDATE awb
+SET awb_shipment_info = jsonb_set(awb_shipment_info, '{consigneeAddress}', '"2735,FujinRoad,Baoshan District,Shanghai, China 201901"', true)
+WHERE consolidation_id = 63901;
+
+
+UPDATE awb
+SET awb_shipment_info = jsonb_set(awb_shipment_info, '{consigneeAddress2}', '"USCI+91310115717856770B"', true)
+WHERE consolidation_id = 63901;
+
+
+UPDATE awb
+SET awb_shipment_info = jsonb_set(awb_shipment_info, '{consigneeContactName}', '"Ivy"', true)
+WHERE consolidation_id = 63901;
+
+
+UPDATE awb
+SET awb_shipment_info = jsonb_set(awb_shipment_info, '{consigneePhone}', '"00862136632709"', true)
+WHERE consolidation_id = 63901;
