2ff772e7e6bbd00dd98feec5496a41179cf0900d
Deepshikha.Tripathi <deepshikha.tripathi@dpworld.com>
2025-08-06 12:42:59 +0530

    updated task - 241209

--- DBPatch/Release-v1.36.2/task_241209.sql
+++ DBPatch/Release-v1.36.2/task_241209.sql
@@ -7,4 +7,19 @@ set
 	entity_id = null,
 	ship_cons_number = null
 where
+	mawb_number = '157-05955935'
+
+update
+	consolidation_details
+set
+	bol = '157-05955935'
+where
 	id = 63931;
+
+
+update
+	awb
+set
+	awb_number = '157-05955935'
+where
+	consolidation_id = 63931
