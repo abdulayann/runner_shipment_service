1c2db1b02a50df0c8eb8705c1923a53cc8ae5292
mayankgupta <mayank.gupta@dpworld.com>
2025-08-13 20:02:17 +0530

    RAIL supported

--- src/main/java/com/dpw/runner/shipment/services/service/impl/PackingV3Service.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/PackingV3Service.java
@@ -1549,7 +1549,8 @@ public class PackingV3Service implements IPackingV3Service {
     private void updateAttachedContainersData(List<Packing> packings, ShipmentDetails shipmentDetails) throws RunnerException {
         if (shipmentDetails == null ||
                 !(TRANSPORT_MODE_SEA.equals(shipmentDetails.getTransportMode()) ||
-                        TRANSPORT_MODE_ROA.equalsIgnoreCase(shipmentDetails.getTransportMode())))
+                        TRANSPORT_MODE_ROA.equalsIgnoreCase(shipmentDetails.getTransportMode()) ||
+                        TRANSPORT_MODE_RAI.equalsIgnoreCase(shipmentDetails.getTransportMode())))
             return;
         Set<Long> containerIdsToUpdate = new HashSet<>();
         packings.forEach(e -> {
