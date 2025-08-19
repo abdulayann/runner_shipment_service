df8e5ad358a7a9d7174fe5821cd0236252fb57f5
Dinanath.Gupta <Dinanath.Gupta@dpworld.com>
2025-08-19 11:56:03 +0530

    changes for prod support ticket # 246193

--- DBPatch/Release-v1.36.4/task_246193.sql
+++ DBPatch/Release-v1.36.4/task_246193.sql
@@ -1,6 +1,1 @@
-update consolidation_details
-set receiving_branch=713
-where id in (
-    select id from consolidation_details
-    where consolidation_number = 'BCNS25062296' and source_guid  is null and tenant_id =520
-);
+Delete from network_transfer where entity_id=50282 and tenant_id=583;
