d763b610df6f57a140adf7b45049606a8959b697
lalchand.mali <lalchand@CTIPL-MAC-736.local>
2025-08-24 19:15:11 +0530

    refactored code

--- src/main/java/com/dpw/runner/shipment/services/commons/constants/Constants.java
+++ src/main/java/com/dpw/runner/shipment/services/commons/constants/Constants.java
@@ -721,6 +721,8 @@ public class Constants {
     public static final String PACKING_GROUP = "packingGroup";
     public static final String ORG_DATA = "orgData";
     public static final String ADDRESS_DATA = "addressData";
+    public static final String SHIPMENT_ROOT_KEY_NAME = "shipmentDetails";
+    public static final String DOT = ".";
 
     private Constants() {
     }
