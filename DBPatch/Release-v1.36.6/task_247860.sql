42310dfecd8a3d66dd4c6eb83d921c68c24139f8
Prajwal Pant <prajwal.pant@dpworld.com>
2025-08-26 16:16:04 +0530

    added air msg status for consol

--- DBPatch/Release-v1.36.6/task_247860.sql
+++ DBPatch/Release-v1.36.6/task_247860.sql
@@ -3,5 +3,5 @@ SET air_message_status = 'AWB_GENERATED'
 WHERE shipment_id = 90729;
 
 UPDATE awb
-SET linked_hawb_air_message_status = 'AWB_GENERATED'
+SET linked_hawb_air_message_status = 'AWB_GENERATED', air_message_status = 'AWB_GENERATED'
 WHERE consolidation_id = 69741;
