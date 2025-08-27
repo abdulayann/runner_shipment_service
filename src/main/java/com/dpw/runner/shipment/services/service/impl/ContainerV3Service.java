ed135cda3e98de85fe39ac1bea9f2d389f27a74a
Raksha Jain <raksha.jain@CTIPL-MAC-MA22.local>
2025-08-27 15:41:20 +0530

    Adding Reassign Functionality For Shipment/Consol Screens

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ContainerV3Service.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ContainerV3Service.java
@@ -128,7 +128,6 @@ import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
-import org.junit.runner.Runner;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
