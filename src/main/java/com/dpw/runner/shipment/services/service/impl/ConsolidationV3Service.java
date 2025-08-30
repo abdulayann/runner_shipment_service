8ff92a9460604cf321bc116588650e3ced8836ca
Pardeep Malik <Pardeep.Malik@dpworld.com>
2025-08-30 20:44:59 +0530

    Flight number bug fix

--- src/main/java/com/dpw/runner/shipment/services/service/impl/ConsolidationV3Service.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/ConsolidationV3Service.java
@@ -2754,6 +2754,8 @@ public class ConsolidationV3Service implements IConsolidationV3Service {
                 .forEach(consolRoute -> {
                     // Deep copy of the routing object
                     var syncedRoute = jsonHelper.convertCreateValue(consolRoute, Routings.class);
+                    if(Constants.TRANSPORT_MODE_AIR.equals(syncedRoute.getMode()))
+                        syncedRoute.setVoyage(syncedRoute.getFlightNumber());
                     syncedRoute.setConsolidationId(null);
                     syncedRoute.setShipmentId(shipmentDetails.getId());
                     syncedRoute.setBookingId(null);
