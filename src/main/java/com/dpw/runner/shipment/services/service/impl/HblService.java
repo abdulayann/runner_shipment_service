23f850cefa17cf094bb546587405c706b50dacc8
isha <isha.mittal@dpworld.com>
2025-09-05 18:31:15 +0530

    removed extra import

--- src/main/java/com/dpw/runner/shipment/services/service/impl/HblService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/impl/HblService.java
@@ -82,7 +82,6 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.ObjectUtils;
 
 
 @Slf4j
