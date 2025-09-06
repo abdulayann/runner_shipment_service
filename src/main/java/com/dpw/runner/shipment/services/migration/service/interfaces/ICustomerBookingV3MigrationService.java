9267e822315c919407719333e671f7203bb4a58f
Mylavarapu Vamsi <mylavarapu.vamsi@CTIPL-MAC-584.local>
2025-09-06 12:26:12 +0530

    Changes for fixing decimal places of containers and packings in booking

--- src/main/java/com/dpw/runner/shipment/services/migration/service/interfaces/ICustomerBookingV3MigrationService.java
+++ src/main/java/com/dpw/runner/shipment/services/migration/service/interfaces/ICustomerBookingV3MigrationService.java
@@ -7,12 +7,10 @@ import java.math.BigDecimal;
 import java.util.Map;
 
 public interface ICustomerBookingV3MigrationService {
-    CustomerBooking migrateBookingV2ToV3(Long bookingId, Map<String, BigDecimal> codeTeuMap) throws RunnerException;
-    CustomerBooking mapBookingV2ToV3(CustomerBooking customerBooking, Map<String, BigDecimal> codeTeuMap) throws RunnerException;
+    CustomerBooking migrateBookingV2ToV3(Long bookingId, Map<String, BigDecimal> codeTeuMap, Integer weightDecimal, Integer volumeDecimal) throws RunnerException;
 
-    CustomerBooking migrateBookingV3ToV2(Long bookingId, Map<String, BigDecimal> codeTeuMap) throws RunnerException;
-    CustomerBooking mapBookingV3ToV2(CustomerBooking customerBooking, Map<String, BigDecimal> codeTeuMap) throws RunnerException;
+    CustomerBooking migrateBookingV3ToV2(Long bookingId, Map<String, BigDecimal> codeTeuMap, Integer weightDecimal, Integer volumeDecimal) throws RunnerException;
 
-    Map<String, Integer> migrateBookingV2ToV3ForTenant(Integer tenantId);
-    Map<String, Integer> migrateBookingV3ToV2ForTenant(Integer tenantId);
+    Map<String, Integer> migrateBookingV2ToV3ForTenant(Integer tenantId, Map<String, BigDecimal> codeTeuMap, Integer weightDecimal, Integer volumeDecimal);
+    Map<String, Integer> migrateBookingV3ToV2ForTenant(Integer tenantId, Integer weightDecimal, Integer volumeDecimal);
 }
