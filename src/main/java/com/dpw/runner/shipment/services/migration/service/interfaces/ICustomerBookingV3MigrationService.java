3e44303cb97c411786fa57bf727cbddc47e6dc7b
Mylavarapu Vamsi <mylavarapu.vamsi@CTIPL-MAC-584.local>
2025-09-06 12:47:25 +0530

    Removed code smells

--- src/main/java/com/dpw/runner/shipment/services/migration/service/interfaces/ICustomerBookingV3MigrationService.java
+++ src/main/java/com/dpw/runner/shipment/services/migration/service/interfaces/ICustomerBookingV3MigrationService.java
@@ -8,8 +8,7 @@ import java.util.Map;
 
 public interface ICustomerBookingV3MigrationService {
     CustomerBooking migrateBookingV2ToV3(Long bookingId, Map<String, BigDecimal> codeTeuMap, Integer weightDecimal, Integer volumeDecimal) throws RunnerException;
-
-    CustomerBooking migrateBookingV3ToV2(Long bookingId, Map<String, BigDecimal> codeTeuMap, Integer weightDecimal, Integer volumeDecimal) throws RunnerException;
+    CustomerBooking migrateBookingV3ToV2(Long bookingId, Integer weightDecimal, Integer volumeDecimal) throws RunnerException;
 
     Map<String, Integer> migrateBookingV2ToV3ForTenant(Integer tenantId, Map<String, BigDecimal> codeTeuMap, Integer weightDecimal, Integer volumeDecimal);
     Map<String, Integer> migrateBookingV3ToV2ForTenant(Integer tenantId, Integer weightDecimal, Integer volumeDecimal);
