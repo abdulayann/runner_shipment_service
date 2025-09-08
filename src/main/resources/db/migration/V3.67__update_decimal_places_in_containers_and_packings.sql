035cf55242fc0d9c97724c051989cb45ae40f5d3
Mylavarapu Vamsi <mylavarapu.vamsi@CTIPL-MAC-584.local>
2025-09-08 10:56:12 +0530

    Updated DB migration version number

--- src/main/resources/db/migration/V3.67__update_decimal_places_in_containers_and_packings.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE IF EXISTS packing
-    ALTER COLUMN IF EXISTS volume_per_pack TYPE NUMERIC,
-    ALTER COLUMN IF EXISTS cargo_weight_per_pack TYPE NUMERIC;
-
-ALTER TABLE IF EXISTS containers
-    ALTER COLUMN IF EXISTS cargo_weight_per_container NUMERIC;
