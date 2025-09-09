c2033b4a7182d3c4fb7e2d1608a396da29252e72
Mathavan <mathavan.g@ext.dpworld.com>
2025-09-09 12:02:32 +0530

    Added validation for flags

--- src/main/java/com/dpw/runner/shipment/services/dto/request/CloneRequest.java
+++ src/main/java/com/dpw/runner/shipment/services/dto/request/CloneRequest.java
@@ -3,7 +3,11 @@ package com.dpw.runner.shipment.services.dto.request;
 import com.dpw.runner.shipment.services.commons.requests.CommonRequest;
 import com.dpw.runner.shipment.services.commons.requests.IRunnerRequest;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 /**
  * Request model to clone.
@@ -16,5 +20,8 @@ import lombok.*;
 public class CloneRequest extends CommonRequest implements IRunnerRequest {
     private Long shipmentId;
     private Long bookingId;
+    @NotNull(message = "Flags cannot be null")
+    @Valid
+    @ApiModelProperty(value = "Clone configuration flags", required = true)
     private CloneFlagsRequest flags;
 }
