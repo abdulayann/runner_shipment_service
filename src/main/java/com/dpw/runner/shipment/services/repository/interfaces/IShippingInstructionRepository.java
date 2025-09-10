73e2998aaabe9631cd153b85abf15b6cd88b2dfc
Ashish Tiwari <ashish.t@CTIPL-MAC-MA03.local>
2025-09-10 15:15:15 +0530

    kafka sender and other changes

--- src/main/java/com/dpw/runner/shipment/services/repository/interfaces/IShippingInstructionRepository.java
+++ src/main/java/com/dpw/runner/shipment/services/repository/interfaces/IShippingInstructionRepository.java
@@ -2,6 +2,13 @@ package com.dpw.runner.shipment.services.repository.interfaces;
 
 import com.dpw.runner.shipment.services.aspects.MultitenancyAspect.MultiTenancyRepository;
 import com.dpw.runner.shipment.services.entity.ShippingInstruction;
+import com.dpw.runner.shipment.services.projection.CarrierBookingInfoProjection;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface IShippingInstructionRepository extends MultiTenancyRepository<ShippingInstruction> {
+    @Query(value = "select status as bookingStatus, booking_no as bookingNumber " +
+            "from carrier_booking where id = ?1 and is_deleted is false;", nativeQuery = true)
+    List<CarrierBookingInfoProjection> findCarrierBookingInfoById(Long id);
 }
---
+++ src/main/java/com/dpw/runner/shipment/services/utils/IntraCommonKafkaHelper.java
@@ -0,0 +1,31 @@
+package com.dpw.runner.shipment.services.utils;
+
+import com.dpw.runner.shipment.services.entity.enums.GenericKafkaMsgType;
+import com.dpw.runner.shipment.services.kafka.dto.GenericKafkaPayload;
+import com.dpw.runner.shipment.services.kafka.producer.KafkaProducer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class IntraCommonKafkaHelper {
+
+    @Autowired
+    private KafkaProducer producer;
+
+    private void sendDataToKafka(String payload, GenericKafkaMsgType msgType, String topic) {
+        try {
+            // Wrap it inside your generic payload with the provided msgType
+            GenericKafkaPayload genericKafkaMsg =
+                    new GenericKafkaPayload(msgType, payload);
+
+            log.debug("Payload sent to kafka with id {} and type {}", genericKafkaMsg.getId(), msgType);
+
+            producer.produceToKafka(genericKafkaMsg, topic, genericKafkaMsg.getId());
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error serializing payload for Kafka", e);
+        }
+    }
+}
