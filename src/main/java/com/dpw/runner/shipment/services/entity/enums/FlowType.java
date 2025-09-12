3154bef44345cbc9124d63d70f19b2c10dc42336
Raksha Jain <raksha.jain@CTIPL-MAC-1019.local>
2025-09-12 12:27:28 +0530

    Modified Enums

--- src/main/java/com/dpw/runner/shipment/services/entity/enums/FlowType.java
+++ src/main/java/com/dpw/runner/shipment/services/entity/enums/FlowType.java
@@ -3,10 +3,11 @@ package com.dpw.runner.shipment.services.entity.enums;
 import lombok.Getter;
 
 @Getter
+@SuppressWarnings("java:S115") //Suppressing Rename this constant name to match the regular expression
 public enum FlowType {
 
-    INBOUND(0, "Inbound"),
-    OUTBOUND(1, "Outbound");
+    Inbound(0, "Inbound"),
+    Outbound(1, "Outbound");
 
     private final int value;
     private final String description;
