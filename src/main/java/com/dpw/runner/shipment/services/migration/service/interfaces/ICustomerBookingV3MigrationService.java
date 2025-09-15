47ee53194dca2ac7ea3878c728e631cd1b5e2b19
Mylavarapu Vamsi <mylavarapu.vamsi@CTIPL-MAC-584.local>
2025-09-15 21:00:57 +0530

    Changes for updating parentContractId during v2 to v3 migration

--- src/main/java/com/dpw/runner/shipment/services/migration/service/interfaces/ICustomerBookingV3MigrationService.java
+++ src/main/java/com/dpw/runner/shipment/services/migration/service/interfaces/ICustomerBookingV3MigrationService.java
@@ -3,11 +3,12 @@ package com.dpw.runner.shipment.services.migration.service.interfaces;
 import com.dpw.runner.shipment.services.entity.CustomerBooking;
 import com.dpw.runner.shipment.services.exception.exceptions.RunnerException;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Map;
 
 public interface ICustomerBookingV3MigrationService {
-    CustomerBooking migrateBookingV2ToV3(Long bookingId, Map<String, BigDecimal> codeTeuMap, Integer weightDecimal, Integer volumeDecimal) throws RunnerException;
+    CustomerBooking migrateBookingV2ToV3(Long bookingId, Map<String, BigDecimal> codeTeuMap, Integer weightDecimal, Integer volumeDecimal) throws RunnerException, IOException;
     CustomerBooking migrateBookingV3ToV2(Long bookingId, Integer weightDecimal, Integer volumeDecimal) throws RunnerException;
 
     Map<String, Object> migrateBookingV2ToV3ForTenant(Integer tenantId, Map<String, BigDecimal> codeTeuMap, Integer weightDecimal, Integer volumeDecimal);
