cc7629d26cbcc560a9c86fb8d205c18855452fa9
Arpit.Shrivastava <Arpit.Shrivastava@dpworld.com>
2025-09-16 19:43:12 +0530

    tenant id = 459

--- DBPatch/Release-v3.3.0/ShipmentTransferMigration_252891.sql
+++ DBPatch/Release-v3.3.0/ShipmentTransferMigration_252891.sql
@@ -1,5 +1,5 @@
 --shipment no HR2A25090362
-DELETE FROM network_transfer WHERE entity_id = 99156 AND entity_type = 'SHIPMENT' AND tenant_id = 663;
+DELETE FROM network_transfer WHERE entity_id = 99156 AND entity_type = 'SHIPMENT' AND tenant_id = 459;
 
 UPDATE shipment_details
 SET receiving_branch = 477
