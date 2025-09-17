44afeacc1463f28c486f020b0fd8c8d80a6eedb2
Pardeep Malik <Pardeep.Malik@dpworld.com>
2025-09-17 15:26:37 +0530

    Logging added for request and response

---
+++ src/main/java/com/dpw/runner/shipment/services/filters/JsonLoggingFilter.java
@@ -0,0 +1,78 @@
+package com.dpw.runner.shipment.services.filters;
+
+import com.dpw.runner.shipment.services.helpers.LoggerHelper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Slf4j
+public class JsonLoggingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+        String method = request.getMethod();
+        String url = request.getRequestURI();
+        String query = request.getQueryString();
+
+        Map<String, String> headers = new HashMap<>();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            String value = name.equalsIgnoreCase("authorization") ? "*****" : request.getHeader(name);
+            headers.put(name, value);
+        }
+
+        long start = System.currentTimeMillis();
+
+        String requestBody = "";
+        if (isJsonContent(request)) {
+            requestBody = new String(requestWrapper.getContentAsByteArray(), requestWrapper.getCharacterEncoding());
+        }
+        log.info("{} | REQUEST RECEIVED [HTTP Method={}] [URL={}] [QUERY={}] [HEADERS={}] [BODY={}]", LoggerHelper.getRequestIdFromMDC(),
+                method,
+                url,
+                query != null ? query : "",
+                headers,
+                requestBody);
+
+        try {
+            filterChain.doFilter(requestWrapper, responseWrapper);
+        } finally {
+            long duration = System.currentTimeMillis() - start;
+
+            String responseBody = "";
+            if (isJsonResponse(responseWrapper)) {
+                responseBody = new String(responseWrapper.getContentAsByteArray(), responseWrapper.getCharacterEncoding());
+            }
+            log.info("{} | RESPONSE RETURNED [URL={}], [DURATION = {} ms] [RESPONSE={}]", LoggerHelper.getRequestIdFromMDC(), url, duration, responseBody);
+
+            responseWrapper.copyBodyToResponse();
+        }
+    }
+
+    private boolean isJsonContent(HttpServletRequest request) {
+        String contentType = request.getContentType();
+        return contentType != null && contentType.toLowerCase().contains("application/json");
+    }
+
+    private boolean isJsonResponse(HttpServletResponse response) {
+        String contentType = response.getContentType();
+        return contentType != null && contentType.toLowerCase().contains("application/json");
+    }
+}
