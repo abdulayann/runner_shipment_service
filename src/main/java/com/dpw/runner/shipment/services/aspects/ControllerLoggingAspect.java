1e7e2ea8b33b965555d367e3730ea8850e321818
Himanshu.Chhikara <himanshu.chhikara@dpworld.com>
2025-09-18 16:22:58 +0530

    removed duplicate imports

--- src/main/java/com/dpw/runner/shipment/services/aspects/ControllerLoggingAspect.java
+++ src/main/java/com/dpw/runner/shipment/services/aspects/ControllerLoggingAspect.java
@@ -47,7 +47,9 @@ public class ControllerLoggingAspect {
     @Around("controllerMethods()")
     public Object logSingleLineRequest(ProceedingJoinPoint joinPoint) throws Throwable {
         HttpServletRequest request =
-                ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+                ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes()))
+                        .getRequest();
+
 
         CompletableFuture.runAsync(withMdc(() -> this.logRequest(joinPoint, request)), executorServiceForLogging);
 
