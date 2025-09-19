d4639b1a512cad054060544c8f5e46ef112d13f0
Himanshu.Chhikara <himanshu.chhikara@dpworld.com>
2025-09-19 17:08:26 +0530

    CarrierBooking Bridge Request

---
+++ src/main/java/com/dpw/runner/shipment/services/dto/request/carrierbooking/CarrierBookingBridgeRequest.java
@@ -0,0 +1,7 @@
+package com.dpw.runner.shipment.services.dto.request.carrierbooking;
+
+import com.dpw.runner.shipment.services.commons.requests.IRunnerRequest;
+import com.dpw.runner.shipment.services.dto.response.carrierbooking.CarrierBookingResponse;
+
+public class CarrierBookingBridgeRequest extends CarrierBookingResponse implements IRunnerRequest {
+}
--- src/main/java/com/dpw/runner/shipment/services/dto/response/carrierbooking/SailingInformationResponse.java
+++ src/main/java/com/dpw/runner/shipment/services/dto/response/carrierbooking/SailingInformationResponse.java
@@ -25,6 +25,7 @@ public class SailingInformationResponse implements IRunnerResponse {
     private String voyageNo;
     private LocalDateTime eta;
     private LocalDateTime etd;
+    private String SCACCode;
     private LocalDateTime earliestDepartureDate;
     private LocalDateTime latestDeliveryDate;
     private LocalDateTime terminalCutoff;
