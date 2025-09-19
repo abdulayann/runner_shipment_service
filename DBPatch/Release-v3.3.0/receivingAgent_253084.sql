0b7c7504a94e6546f4434a2fe7a1eae0dab531b1
Arpit.Shrivastava <Arpit.Shrivastava@dpworld.com>
2025-09-19 12:00:22 +0530

    receiving agent update 253512

---
+++ DBPatch/Release-v3.3.0/receivingAgent_253084.sql
@@ -0,0 +1,6 @@
+--shipment no HR2A25090362
+DELETE FROM network_transfer WHERE entity_id = 99156 AND entity_type = 'SHIPMENT' AND source_branch_id = 663;
+
+UPDATE shipment_details
+SET receiving_branch = 477
+WHERE id = 99156;
