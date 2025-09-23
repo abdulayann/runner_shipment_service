b5a5c4fd928dc98090eb7143705f683b31b26c6a
Arpit.Shrivastava <Arpit.Shrivastava@dpworld.com>
2025-09-23 16:14:14 +0530

    shipment details need not to be updated

--- DBPatch/Release-v3.3.0/ReceivingAgentUpdate_253512.sql
+++ DBPatch/Release-v3.3.0/ReceivingAgentUpdate_253512.sql
@@ -5,8 +5,8 @@ UPDATE consolidation_details
 SET receiving_branch = 705
 WHERE id = 61699;
 
-DELETE FROM network_transfer WHERE entity_id = 68880 AND entity_type = 'SHIPMENT' AND tenant_id = 713;
-
-UPDATE shipment_details
-SET receiving_branch = 705
-WHERE id = 68880;
+--DELETE FROM network_transfer WHERE entity_id = 68880 AND entity_type = 'SHIPMENT' AND tenant_id = 713;
+--
+--UPDATE shipment_details
+--SET receiving_branch = 705
+--WHERE id = 68880;
