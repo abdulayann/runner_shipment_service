27ed34e3eb75f52850b114a11ca59d394ceaa155
mayankgupta <mayank.gupta@dpworld.com>
2025-09-23 12:22:22 +0530

    changes

--- src/main/resources/db/migration/V3.75__add_shipment_attach_filter_flag.sql
+++ src/main/resources/db/migration/V3.75__add_shipment_attach_filter_flag.sql
@@ -1,2 +1,2 @@
 ALTER TABLE IF EXISTS shipment_setting
-    ADD COLUMN IF NOT EXISTS include_ports_in_ship_console_attach_filter BOOLEAN DEFAULT false;
+    ADD COLUMN IF NOT EXISTS include_ports_in_ship_console_attach_filter BOOLEAN DEFAULT true;
