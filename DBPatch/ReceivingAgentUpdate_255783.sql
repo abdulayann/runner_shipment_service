eb0cb39f790938076f612551e8d0278b5b833d26
Sumit Bansal <Sumit.Bansal@dpworld.com>
2025-09-26 11:37:09 +0000

    Updated ReceivingAgentUpdate_255783.sql

--- DBPatch/ReceivingAgentUpdate_255783.sql
+++ DBPatch/ReceivingAgentUpdate_255783.sql
@@ -1,5 +1,5 @@
 
-DELETE FROM network_transfer WHERE entity_id = 78589 AND entity_type = 'CONSOLIDATION' AND tenant_id = 728;
+DELETE FROM network_transfer WHERE entity_id = 78598 AND entity_type = 'CONSOLIDATION' AND tenant_id = 728;
 UPDATE consolidation_details
 SET receiving_branch = 459
-WHERE id = 78589;
+WHERE id = 78598;
