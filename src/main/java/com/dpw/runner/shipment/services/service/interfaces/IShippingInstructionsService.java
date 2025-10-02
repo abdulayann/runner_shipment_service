6e0216e47638bd9a1a15d315284115672514ba4c
Ashish Tiwari <ashish.t@CTIPL-MAC-MA03.local>
2025-10-03 00:38:33 +0530

    conflict resolve

--- src/main/java/com/dpw/runner/shipment/services/service/interfaces/IShippingInstructionsService.java
+++ src/main/java/com/dpw/runner/shipment/services/service/interfaces/IShippingInstructionsService.java
@@ -5,6 +5,7 @@ import com.dpw.runner.shipment.services.commons.responses.IRunnerResponse;
 import com.dpw.runner.shipment.services.dto.request.carrierbooking.ShippingInstructionRequest;
 import com.dpw.runner.shipment.services.dto.response.carrierbooking.ShippingInstructionResponse;
 import com.dpw.runner.shipment.services.entity.enums.EntityType;
+import com.dpw.runner.shipment.services.kafka.dto.inttra.ShippingInstructionEventDto;
 import org.springframework.http.ResponseEntity;
 
 public interface IShippingInstructionsService {
