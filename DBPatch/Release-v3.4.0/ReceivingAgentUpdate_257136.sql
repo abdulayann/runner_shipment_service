a1e2ef7972d2c76dd101d4cb46968b4eee57d0c4
karan.nehra <karana.nehra@dpworld.com>
2025-10-06 10:53:51 +0530

    updated query

--- DBPatch/Release-v3.4.0/ReceivingAgentUpdate_257136.sql
+++ DBPatch/Release-v3.4.0/ReceivingAgentUpdate_257136.sql
@@ -1,4 +1,4 @@
 DELETE FROM network_transfer WHERE entity_id = 73917 AND entity_type = 'CONSOLIDATION' AND tenant_id = 474;
 UPDATE consolidation_details
 SET receiving_branch = 481
-WHERE id = 73917;
+WHERE id = 73917 and tenant_id = 474;
