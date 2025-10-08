2d11b397dd27ee0177d41149cab4e9f3dda0bf1e
Himanshu.Chhikara <himanshu.chhikara@dpworld.com>
2025-10-08 19:40:55 +0530

    shipment Entity Refactoring

--- src/main/java/com/dpw/runner/shipment/services/dto/response/ShipmentDetailsResponse.java
+++ src/main/java/com/dpw/runner/shipment/services/dto/response/ShipmentDetailsResponse.java
@@ -1,108 +1,58 @@
 package com.dpw.runner.shipment.services.dto.response;
 
-import com.dpw.runner.shipment.services.commons.responses.IRunnerResponse;
 import com.dpw.runner.shipment.services.config.CustomLocalDateTimeSerializer;
-import com.dpw.runner.shipment.services.config.CustomVolumeValueSerializer;
 import com.dpw.runner.shipment.services.config.CustomWeightValueSerializer;
-import com.dpw.runner.shipment.services.config.DecimalPlaceValueSerializer;
 import com.dpw.runner.shipment.services.dto.CalculationAPIsDto.ContainerSummaryResponse;
 import com.dpw.runner.shipment.services.dto.CalculationAPIsDto.PackSummaryResponse;
-import com.dpw.runner.shipment.services.entity.enums.*;
+import com.dpw.runner.shipment.services.entity.enums.CustomerCategoryRates;
+import com.dpw.runner.shipment.services.entity.enums.DateBehaviorType;
+import com.dpw.runner.shipment.services.entity.enums.MigrationStatus;
+import com.dpw.runner.shipment.services.entity.enums.OceanDGStatus;
+import com.dpw.runner.shipment.services.entity.enums.ShipmentPackStatus;
 import com.dpw.runner.shipment.services.utils.ExcludeTimeZone;
 import com.dpw.runner.shipment.services.utils.Generated;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 
-@Builder
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Generated
 @SuppressWarnings("java:S1948")
-public class ShipmentDetailsResponse implements IRunnerResponse {
-    private Long id;
-    private Integer tenantId;
-    private UUID guid;
-    private String houseBill;
-    private String transportMode;
-    private String direction;
-    private String shipmentType;
-    private Integer status;
-    private String shipmentStatus;
-    private String source;
-    private String jobType;
-    private String serviceType;
-    private String masterBill;
-    private String bookingReference;
-    private String consolRef;
-    private Long salesAgent;
-    private String paymentTerms;
-    private String incoterms;
-    private String shipmentId;
+public class ShipmentDetailsResponse extends BaseShipmentResponse {
     private Boolean isDomestic;
-    private String assignedTo;
-    private String additionalTerms;
-    private String goodsDescription;
-    @JsonSerialize(using = CustomWeightValueSerializer.class)
-    private BigDecimal weight;
-    private String weightUnit;
-    @JsonSerialize(using = CustomVolumeValueSerializer.class)
-    private BigDecimal volume;
-    private String volumeUnit;
-    @JsonSerialize(using = CustomWeightValueSerializer.class)
-    private BigDecimal volumetricWeight;
-    private String volumetricWeightUnit;
-    @JsonSerialize(using = DecimalPlaceValueSerializer.class)
-    private BigDecimal chargable;
-    private String chargeableUnit;
     @JsonSerialize(using = CustomWeightValueSerializer.class)
     private BigDecimal netWeight;
     private String netWeightUnit;
-    private Integer noOfPacks;
-    private String packsUnit;
     private Integer innerPacks;
     private String innerPackUnit;
     @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
     private LocalDateTime cargoReadyDate;
-    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
-    private LocalDateTime cargoDeliveryDate;
-    private BigDecimal freightLocal;
-    private String freightLocalCurrency;
-    private BigDecimal freightOverseas;
-    private String freightOverseasCurrency;
     private Boolean autoUpdateWtVol;
     private Boolean containerAutoWeightVolumeUpdate;
-    private String marksNum;
     private String entryDetail;
-    private Boolean isLocked;
-    private String lockedBy;
-    private Boolean isNotifyConsigneeEqual;
-    private String bookingType;
     private Boolean cargoFinanceBooking = Boolean.FALSE;
-    private String bookingNumber;
     private String route;
-    private Long sourceTenantId;
     private Long documentationPartner;
-    private List<TriangulationPartnerResponse> triangulationPartnerList;
     private Long triangulationPartner;
-    private Long receivingBranch;
-    private Long originBranch;
     private Boolean intraBranch = Boolean.FALSE;
     private Integer prevShipmentStatus;
     @JsonProperty("isShipmentReadOnly")
     private Boolean isShipmentReadOnly = Boolean.FALSE;
-    private String shipmentCompletedBy;
-    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
-    private LocalDateTime shipmentCompletedOn;
     private String financeClosedBy;
     @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
     private LocalDateTime financeClosedOn;
@@ -110,7 +60,6 @@ public class ShipmentDetailsResponse implements IRunnerResponse {
     private PartiesResponse consigner;
     private PartiesResponse consignee;
     private AdditionalDetailResponse additionalDetails;
-    private CarrierDetailResponse carrierDetails;
     private PickupDeliveryDetailsResponse pickupDetails;
     private PickupDeliveryDetailsResponse deliveryDetails;
     private List<NotesResponse> notesList;
@@ -133,127 +82,44 @@ public class ShipmentDetailsResponse implements IRunnerResponse {
     private Long container20RECount;
     private Long container40GPCount;
     private Long container40RECount;
-    private String jobStatus;
     private Map<String, String> masterData;
     private Map<String, String> unlocationData;
     private Map<String, String> currenciesMasterData;
     private Map<String, String> tenantIdsData;
-    private BigDecimal goodsValue;
-    private String goodsValueCurrency;
-    private BigDecimal insuranceValue;
-    private String InsuranceValueCurrency;
-    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
-    private LocalDateTime shipmentCreatedOn;
     private String entryRefNo;
     private List<PartiesResponse> shipmentAddresses;
     private List<AuditLogResponse> logsList;
     private String flightStatus;
-    private Boolean containsHazardous;
     private String fmcTlcId;
     private String commodity;
     private Long orderNumber;
     private String orderManagementId;
     private String orderManagementNumber;
     private List<ShipmentOrderResponse> shipmentOrders;
-    private String createdBy;
     private ContainerSummaryResponse containerSummary;
     private PackSummaryResponse packSummary;
     private Map<String, String> textData;
     private List<NotesResponse> customerBookingNotesList;
     private Map<String, Long> containerData;
     private Map<String, String> organizationsMasterData;
-    private Long containerCount;
-    private BigDecimal teuCount;
-    private Integer packCount;
-    private String department;
-
-    public void addTextData(Map<String, String> dataMap) {
-        if(textData == null) {
-            textData = new HashMap<>();
-        }
-        textData.putAll(dataMap);
-    }
-    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
-    private LocalDateTime createdAt;
-    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
-    private LocalDateTime updatedAt;
-
     private CustomerCategoryRates customerCategory;
-    private String contractId;
-    private String parentContractId;
-    private String contractType;
-    private String clientCountry;
-    private String consignorCountry;
-    private String consigneeCountry;
-    private String notifyPartyCountry;
-    private String salesBranch;
-    private String primarySalesAgentEmail;
-    private String secondarySalesAgentEmail;
     private UUID sourceGuid;
     private UUID clonedGuid;
-    private Long consigneeDpsAddressId;
-    private Long clientDpsAddressId;
-    private Long consignorDpsAddressId;
-    private Long notifyPartyDpsAddressId;
     private List<String> implicationList;
-    private Long shipmentCount;
-    private LocalDateTime bookingCreatedDate;
-    private String securityStatus;
-    private AwbStatus awbStatus;
-
-    private String currentPartyForQuote;
     private Map<String, Object> masterDataMap;
-    private Boolean entityTransfer;
-    private String destinationSalesBranch;
-    private String destinationPrimarySalesAgentEmail;
-    private String destinationSecondarySalesAgentEmail;
-    private String destinationCurrentPartyForQuote;
-    private String destinationContractId;
-    private String destinationParentContractId;
-    private String destinationContractType;
-    private String updatedBy;
     private DateBehaviorType dateType;
     @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
     private LocalDateTime shipmentGateInDate;
     private ShipmentPackStatus shipmentPackStatus;
-    private Integer pendingActionCount;
-    private FileStatus fileStatus;
-    private Boolean isReceivingBranchAdded;
     private OceanDGStatus oceanDGStatus;
     private Boolean syncRoutingFromConsolidation;
     private Boolean isNetworkFile;
-    private String transferStatus;
-    private Boolean isReceivingBranchManually;
-    private Boolean isTransferredToReceivingBranch;
     private Boolean b2b;
-
     private Boolean isCoLoadEnabled;
-    private String coLoadCarrierName;
-    private String coLoadBlNumber;
-
     private String issuingCarrierName;
     private String oceanBlNumber;
-    private UUID customerBookingGuid;
-    private Boolean isFrob;
-    private String consolidationNumber;
-    private Long containerAssignedToShipmentCargo;
-    private Boolean isReefer;
-    private String incotermsLocation;
     @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
     private LocalDateTime cargoReadinessDate;
-    private Boolean controlled;
-    private String controlledReferenceNumber;
-    private String partner;
-    private Long bookingAgent;
-    private String coLoadBkgNumber;
-    private String pickupAtOriginType;
-    private String deliveryAtDestinationType;
-    private String brokerageAtOriginType;
-    private String brokerageAtDestinationType;
-    private Long pickupAtOrigin;
-    private Long deliveryAtDestination;
-    private Long brokerageAtOrigin;
-    private Long brokerageAtDestination;
     @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
     @ExcludeTimeZone
     private LocalDateTime brokerageAtOriginDate;
@@ -266,29 +132,15 @@ public class ShipmentDetailsResponse implements IRunnerResponse {
     @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
     @ExcludeTimeZone
     private LocalDateTime estimatedBrokerageAtDestinationDate;
-    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
-    private LocalDateTime terminalCutoff;
-    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
-    private LocalDateTime verifiedGrossMassCutoff;
-    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
-    private LocalDateTime shippingInstructionCutoff;
-    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
-    private LocalDateTime dgCutoff;
-    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
-    private LocalDateTime reeferCutoff;
-    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
-    private LocalDateTime earliestEmptyEquipmentPickUp;
-    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
-    private LocalDateTime latestFullEquipmentDeliveredToCarrier;
-    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
-    private LocalDateTime earliestDropOffFullEquipmentToCarrier;
-    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
-    private LocalDateTime latestArrivalTime;
-    private Boolean isBorrowed;
     private Integer dgPacksCount;
     private String dgPacksUnit;
-
-    private Integer slac;
     private MigrationStatus migrationStatus;
     private Boolean triggerMigrationWarning;
+
+    public void addTextData(Map<String, String> dataMap) {
+        if (textData == null) {
+            textData = new HashMap<>();
+        }
+        textData.putAll(dataMap);
+    }
 }
