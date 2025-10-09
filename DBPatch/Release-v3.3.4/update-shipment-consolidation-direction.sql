45b5c0f6920f47953cc40433041fbd6a2c4cd5a5
Isha Mittal <isha.mittal@dpworld.com>
2025-10-09 05:54:19 +0000

    Merged PR 375302: DB patch for direction update
    DB patch for direction update
    
    Related work items: #257654

---
+++ DBPatch/Release-v3.3.4/update-shipment-consolidation-direction.sql
@@ -0,0 +1,7 @@
+Update consolidation_details
+set shipment_type = 'IMP'
+where id = (76068,81857,65656,78349,73746,65657)
+
+Update shipment_details
+set direction = 'IMP'
+where id = (102138,86200,96140,86201,100060,99392,106549)
