144117165eacbaad59ca0557363730189092bd7a
saikrishna mundrathi <saikrishna.mundrathi@dpworld.com>
2025-10-17 16:11:06 +0530

    #260511 changes for Receiving Agent

--- DBPatch/Release-v3.3.7/DBPatch_260511.sql
+++ DBPatch/Release-v3.3.7/DBPatch_260511.sql
@@ -1,4 +1,4 @@
-DELETE FROM network_transfer WHERE entity_id = 83617 AND entity_type = 'SHIPMENT' AND tenant_id = 465;
+DELETE FROM network_transfer WHERE entity_id = 83617 AND entity_type = 'SHIPMENT' AND tenant_id = 453;
 UPDATE shipment_details
 SET receiving_branch = 465
 WHERE id = 83617;
