93025d89c4e81d11e444f6aa8b1fe78c9e49f300
Himanshu.Chhikara <himanshu.chhikara@dpworld.com>
2025-10-21 21:33:26 +0530

    Migration renaming

--- src/main/resources/db/migration/V1.277__add_security_columns_to_consolidation_details.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE consolidation_details
-    ADD COLUMN security_status_received_from VARCHAR(100),
-    ADD COLUMN expiry_date TIMESTAMP,
-    ADD COLUMN regulated_entity_category VARCHAR(255);
